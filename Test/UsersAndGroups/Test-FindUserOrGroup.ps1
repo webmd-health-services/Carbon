f279b4c0eaac0016c4957979e2c80323d78f6c6e
Aaron Jensen <ajensen@webmd.net>
2012-03-26 11:34:41 -0700

    Updating test for Find-UserOrGroup to work on any computer.

--- Test/UsersAndGroups/Test-FindUserOrGroup.ps1
+++ Test/UsersAndGroups/Test-FindUserOrGroup.ps1
@@ -16,9 +16,12 @@ function Invoke-FindUserOrGroup($name)
 
 function Test-ShouldFindLocalUser
 {
-    $user = Invoke-FindUserOrGroup 'Administrator'
-    Assert-IsNotNull $user 'Unable to find Administrator.'
-    Assert-Equal 'Administrator' $user.Name
+    Get-WmiObject Win32_UserAccount -Filter "Domain='$($env:ComputerName)'" | ForEach-Object {
+        $username = $_.Name
+        $user = Invoke-FindUserOrGroup $username
+        Assert-IsNotNull $user "Unable to find local user $username."
+        Assert-Equal $username $user.Name
+    }
 }
 
 function Test-ShouldFindDomainUser
@@ -30,9 +33,12 @@ function Test-ShouldFindDomainUser
 
 function Test-ShouldFindLocalGroup
 {
-    $group = Invoke-FindUserOrGroup 'Administrators'
-    Assert-IsNotNull $group 'Unable to find administrators'
-    Assert-Equal 'Administrators' $group.Name
+    Get-WmiObject Win32_Group -Filter "Domain='$($env:ComputerName)'" | ForEach-Object {
+        $groupName = $_.Name
+        $group = Invoke-FindUserOrGroup $groupName
+        Assert-IsNotNull $group "Unable to find '$groupName'."
+        Assert-Equal $groupName $group.Name
+    }
 }
 
 function Test-ShouldFindDomainGroup
