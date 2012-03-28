bb6963bd46d5f9909f83245c2d8435d3df47160c
Aaron Jensen <ajensen@webmd.net>
2012-03-27 19:10:31 -0700

    Adding Apache license notice to all source files.

--- Test/MSMQ/Test-InstallMSMQ.ps1
+++ Test/MSMQ/Test-InstallMSMQ.ps1
@@ -1,3 +1,16 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # These tests should only run if MSMQ is not installed and we're on Windows 7/2008 R2
 if( -not (Get-Service -Name MSMQ -ErrorAction SilentlyContinue) -and (Get-WmiObject Win32_OperatingSystem).Version -like '6.1*' )
