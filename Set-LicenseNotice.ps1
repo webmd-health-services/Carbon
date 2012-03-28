eb431d0219305f4b0e1194e85964bc292132339d
Aaron Jensen <ajensen@webmd.net>
2012-03-27 19:18:48 -0700

    Adding documentation to the Set-LicenseNotices script.

--- Set-LicenseNotice.ps1
+++ Set-LicenseNotice.ps1
@@ -1,22 +1,48 @@
 <#
 .SYNOPSIS
 Adds the license notice to a set of files.
+
+.DESCRIPTION
+Given a path, or a pipeline of files, will add or update the Apache license notice to the top of it.  The notice is pulled from a LICENSE file, which is expected to live next to this file.
+
+.EXAMPLE
+>dir .\Carbon,.\Test *ps*1 -Recurse | .\Set-LicenseNotice.ps1 -Copyright '2012 Aaron Jensen' 
+
+Adds or updates the license notice on all the *.ps*1 files under the .\Carbon and .\Test directories.
+
+.EXAMPLE
+> .\Set-LicenseNotice.ps1 -Path .\Test\Test-LicenseNotices.ps1 -Copyright '2012 Aaron Jensen'
+
+Adds or updates the license notice in the .\Test\Test-LicenseNotices.ps1 file.
 #>
 
-# License
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 [CmdletBinding(SupportsShouldProcess=$true)]
 param(
     [Parameter(Mandatory=$true,ParameterSetName='FromUser')]
+    # Path to the file whose license to set.
     $Path,
     
     [Parameter(Mandatory=$true,ParameterSetName='FromPipeline',ValueFromPipeline=$true)]
-    # The files to add 
+    # The files whose licenses should be set.
     $InputObject,
     
     [Parameter(Mandatory=$true)]
     [string]
-    # The copyright string.
+    # The copyright string, i.e. everything after the word "Copyright".
     $Copyright
 )
 
