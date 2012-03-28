9baa24fc9d3bb3c7b25ff472ea635b8a2eb7ad64
Aaron Jensen <ajensen@webmd.net>
2012-03-28 00:24:28 -0700

    Moving Complete-Jobs and Test-ProcessIs*Bit function to PowerShell file.

--- Carbon/PowerShell.ps1
+++ Carbon/PowerShell.ps1
@@ -12,6 +12,91 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+function Complete-Jobs
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [Management.Automation.Job[]]
+        # The jobs to complete.
+        $Jobs,
+        
+        [Parameter()]
+        [int]
+        # The number of seconds to sleep between job status checks.  Default is 1 second.
+        $IntervalSeconds = 1
+    )
+    
+    trap { Write-Warning "Unhandled error found: $_" }
+    $numFailed = 0
+    do
+    {
+        Start-Sleep -Seconds $IntervalSeconds
+        
+        $jobsStillRunning = $false
+        foreach( $pendingJob in $Jobs )
+        {
+            $job = Get-Job $pendingJob.Id -ErrorAction SilentlyContinue
+            if( -not $job )
+            {
+                Write-Verbose "Job with ID $($pendingJob.Id) doesn't exist."
+                continue
+            }
+            
+            try
+            {
+                Write-Verbose "Job $($job.Name) is in the $($job.State) state."
+                
+                $jobHeader = "# $($job.Name): $($job.State)"
+                if( $job.State -eq 'Blocked' -or $job.State -eq 'Stopped')
+                {
+                    Write-Host $jobHeader
+
+                    Write-Verbose "Stopping job $($job.Name)."
+                    Stop-Job -Job $job
+
+                    Write-Verbose "Receiving job $($job.Name)."
+                    Receive-Job -Job $job -ErrorAction Continue | Write-Host
+
+                    Write-Verbose "Removing job $($job.Name)."
+                    Remove-Job -Job $job
+                    $numFailed += 1
+                }
+                elseif( $job.State -eq 'Completed' -or $job.State -eq 'Failed' )
+                {
+                    Write-Host $jobHeader
+
+                    Write-Verbose "Receiving job $($job.Name)."
+                    Receive-Job -Job $job -ErrorAction Continue | Write-Host
+
+                    Write-Verbose "Removing job $($job.Name)."
+                    Remove-Job -Job $job
+                    if( $job.State -eq 'Failed' )
+                    {
+                        $numFailed += 1
+                    }
+                }
+                elseif( $job.State -eq 'NotStarted' -or $job.State -eq 'Running' )
+                {
+                    $jobsStillRunning = $true
+                }
+                else
+                {
+                    throw "Found unknown job state $($job.State)."
+                }
+            }
+            catch
+            {
+                Write-Warning "Encountered error handling job $($job.Name)."
+                Write-Warning $_
+            }
+        }
+        
+     } while( $jobsStillRunning )
+     
+     return $numFailed
+}
+
 function Get-PowershellPath
 {
     <#
@@ -62,4 +147,30 @@ function Invoke-PowerShell
     }
     
     & (Get-PowerShellPath @params) -NoProfile -NoLogo -Command $command -Args $Args
+}
+
+function Test-PowerShellIs32Bit
+{
+    <#
+    .SYNOPSIS
+    Tests if the current powershell process is 32-bit.
+    #>
+    [CmdletBinding()]
+    param(
+    )
+    
+    return ($env:PROCESSOR_ARCHITECTURE -eq 'x86')
+}
+
+function Test-PowerShellIs64Bit
+{
+    <#
+    .SYNOPSIS
+    Tests if the current powershell process is 64-bit.
+    #>
+    [CmdletBinding()]
+    param(
+    )
+    
+    return ($env:PROCESSOR_ARCHITECTURE -eq 'AMD64')
 }
