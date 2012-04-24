55e1b8134a6fe9d4afad696b7487ce7c100ebffe
Aaron Jensen <ajensen@webmd.net>
2012-04-24 14:27:45 -0700

    * Created Carbon assembly for holding code and types that can't be expressed well in PowerShell.  * Created a compiled JunctionPoint helper class for creating, removing, and getting junction/reparse points.  * Updated the DirectoryInfo TargetPath script property to use the new JunctionPoint.GetTarget function.  * Updated New-Junction to use the new JunctionPoint.Create function.  * UPdated Remove-Junction to use the new JunctionPoint.Remove function.

--- Carbon/Carbon.psd1
+++ Carbon/Carbon.psd1
@@ -64,7 +64,7 @@ ProcessorArchitecture = ''
 RequiredModules = @()
 
 # Assemblies that must be loaded prior to importing this module
-RequiredAssemblies = @()
+RequiredAssemblies = @((Join-Path $PSScriptRoot bin\Carbon.dll))
 
 # Script files (.ps1) that are run in the caller's environment prior to importing this module
 ScriptsToProcess = @()
---
+++ Source/Win32.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Copyright 2012 Aaron Jensen
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Carbon
+{
+	public class Win32
+	{
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		public static extern uint GetLongPathName(
+			string shortPath,
+			StringBuilder sb,
+			int buffer);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern uint GetShortPathName(
+			string lpszLongPath,
+			StringBuilder lpszShortPath,
+			uint cchBuffer);
+
+		[DllImport("shlwapi.dll", CharSet = CharSet.Auto)]
+		public static extern bool PathRelativePathTo(
+			[Out] StringBuilder pszPath,
+			[In] string pszFrom,
+			[In] FileAttributes dwAttrFrom,
+			[In] string pszTo,
+			[In] FileAttributes dwAttrTo
+			);
+
+	}
+}
