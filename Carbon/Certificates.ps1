be72163f781768f9e954ac9d13040e5fd54a4031
Aaron Jensen <ajensen@webmd.net>
2012-04-28 19:54:21 -0700

    Adding documentation to Test-SslCertificateBinding.

--- Carbon/Certificates.ps1
+++ Carbon/Certificates.ps1
@@ -457,6 +457,19 @@ function Test-SslCertificateBinding
     <#
     .SYNOPSIS
     Tests if an SSL certificate binding exists.
+	
+	.DESCRIPTION
+	SSL certificates are bound to IP addresses and ports.  This function tests if one exists on a given IP/port.
+	
+	.EXAMPLE
+	Test-SslCertificateBinding 0.0.0.0:443
+	
+	Tests if there is a default SSL certificate bound to all a machine's IP addresses on port 443.
+	
+	.EXAMPLE
+	Test-SslCertificateBinding 10.0.1.1:443
+	
+	Tests if there is an SSL certificate bound to IP address 10.0.1.1 on port 443.
     #>
     param(
         [Parameter(Mandatory=$true)]
