343597dd84c5f7007889310c1fd4c08e2625ac6c
Aaron Jensen <ajensen@webmd.net>
2012-05-31 14:44:47 -0700

    Adding license notices to scripts.

--- Carbon/Import-Carbon.ps1
+++ Carbon/Import-Carbon.ps1
@@ -5,6 +5,21 @@ Imports the Carbon module.
 .DESCRIPTION
 Imports the Carbon module.  If the Carbon module is already loaded, it will remove it and then reloaded.  If Carbon is present as a sub-module of Carbon, Carbon can't be re-loaded so a warning is output instead.  To hide the warning, use the `-Quiet` parameter.
 #>
+
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 [CmdletBinding()]
 param(
     [Switch]
