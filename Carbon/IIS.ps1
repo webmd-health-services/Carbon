a62bebbcd78a9648a19368539cbd613fc2757eb8
Aaron Jensen <ajensen@webmd.net>
2012-06-27 22:10:11 -0700

    Updating Set-IisWindowsAuthentication documentation.

--- Carbon/IIS.ps1
+++ Carbon/IIS.ps1
@@ -940,13 +940,47 @@ function Set-IisWebsiteSslCertificate
 
 function Set-IisWindowsAuthentication
 {
+    <#
+    .SYNOPSIS
+    Enables or disables Windows authentication for all or part of a website.
+
+    .DESCRIPTION
+    By default, enables Windows authentication on a website.  You can enable Windows authentication at a specific path under a website by passing the virtual path (*not* the physical path) to that directory.
+
+    To disable Windows authentication, set the `Disabled` flag.
+
+    .LINK
+    http://blogs.msdn.com/b/webtopics/archive/2009/01/19/service-principal-name-spn-checklist-for-kerberos-authentication-with-iis-7-0.aspx
+
+    .EXAMPLE
+    Set-IisWindowsAuthentication -SiteName Peanuts
+
+    Turns on Windows authentication for the `Peanuts` website.
+
+    .EXAMPLE
+    Set-IisWindowsAuthentication -SiteName Peanuts Snoopy/DogHouse
+
+    Turns on Windows authentication for the `Snoopy/DogHouse` directory under the `Peanuts` website.
+
+    .EXAMPLE
+    Set-IisWindowsAuthentication -SiteName Peanuts -Disabled
+
+    Turns off Windows authentication for the `Peanuts` website.
+
+    .EXAMPLE
+    Set-IisWindowsAuthentication -SiteName Peanuts -UseKernelMode
+
+    Turns on Windows authentication for the `Peanuts` website.
+
+    #>
     [CmdletBinding(SupportsShouldProcess=$true)]
     param(
         [Parameter(Mandatory=$true)]
+        [string]
         # The site where Windows authentication should be set.
         $SiteName,
         
-        [Parameter()]
+        [string]
         # The optional path where Windows authentication should be set.
         $Path = '',
         
@@ -955,7 +989,7 @@ function Set-IisWindowsAuthentication
         $Disabled,
         
         [Switch]
-        # Turn on kernel mode.  Default is false.
+        # Turn on kernel mode.  Default is false.  [More information about Kerndel Mode authentication.](http://blogs.msdn.com/b/webtopics/archive/2009/01/19/service-principal-name-spn-checklist-for-kerberos-authentication-with-iis-7-0.aspx)
         $UseKernelMode
     )
     
