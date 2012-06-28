9f3be3b92426ddfef954597815da7703214bf0a7
Aaron Jensen <ajensen@webmd.net>
2012-06-28 16:42:43 -0700

    Updating Complete-Jobs documentation.

--- Carbon/PowerShell.ps1
+++ Carbon/PowerShell.ps1
@@ -14,6 +14,30 @@
 
 function Complete-Jobs
 {
+    <#
+    .SYNOPSIS
+    Waits for a set of PowerShell jobs to complete, receives each job as it finishes, and returns the number of jobs that didn't complete successfully.
+
+    .DESCRIPTION
+    Job management in PowerShell is a pain.  At a minimum, you have to wait for each job to complete, then receive its output.  This function manages all that for you.  It iterates over a list/array of jobs, as each job finishes, it receives its output and removes it.  If a job is blocked or stopped it receives and removes those jobs.  When its all done, it returns the number of jobs that failed, were blocked, or stopped.
+
+    By default, it sleeps for one second between status checks.  You can increase this interval using the `IntervalSeconds` parameter.
+
+    This function never times out.
+
+    .OUTPUTS
+    System.Int32.
+
+    .EXAMPLE
+    Complete-Jobs -Jobs $jobs
+
+    Sits and waits for all the jobs in `$jobs` to finish, block, or stop and returns the number that didn't succeed.  It waits one second between status checks.
+
+    .EXAMPLE
+    Complete-Jobs -Jobs $jobs -IntervalSeconds 60
+
+    Waits for all the jobs in `$jobs` to finish, waiting 60 seconds between status checks.
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
