8fe382e9379a894d7171cfa078f4106ce9b7fd5c
Aaron Jensen <ajensen@webmd.net>
2012-06-28 10:50:26 -0700

    Updating Unlock-IisConfigSection documentation.

--- Carbon/IIS.ps1
+++ Carbon/IIS.ps1
@@ -1134,14 +1134,24 @@ function Unlock-IisConfigSection
     <#
     .SYNOPSIS
     Unlocks a section in the IIS server configuration.
+
+    .DESCRIPTION
+    Some sections/areas are locked by IIS, so that websites can't enable those settings, or have their own custom configurations.  This function will unlocks those locked sections.  You have to know the path to the section.  You can see a list of locked sections by running:
+
+        C:\Windows\System32\inetsrv\appcmd.exe unlock config /section:?
+
+    .EXAMPLE
+    Unlock-IisConfigSection -Name 'system.webServer/cgi'
+
+    Unlocks the CGI section so that websites can configure their own CGI settings.
     #>
     [CmdletBinding(SupportsShouldProcess=$true)]
     param(
         [Parameter(Mandatory=$true)]
         [string]
-        # The name of the section to unlock.  For a list of sections, run
+        # The path to the section to unlock.  For a list of sections, run
         #
-        #    > C:\Windows\System32\inetsrv\appcmd.exe unlock config /section:?
+        #     C:\Windows\System32\inetsrv\appcmd.exe unlock config /section:?
         $Name
     )
     
