5b56378cbf87dd4a1f66de52f345f4a34d53f954
Aaron Jensen <ajensen@webmd.net>
2012-06-27 21:17:21 -0700

    Updating Set-IisHttpRedirect documentation.

--- Carbon/IIS.ps1
+++ Carbon/IIS.ps1
@@ -731,13 +731,39 @@ function Set-IisDirectoryBrowsing
 
 function Set-IisHttpRedirect
 {
+    <#
+    .SYNOPSIS
+    Turns on HTTP redirect for all or part of a website.
+
+    .DESCRIPTION
+    Configures all or part of a website to redirect all requests to another website/URL.  By default, it operates on a specific website.  To configure a directory under a website, set `Path` to the virtual path of that directory.
+
+    .LINK
+    http://technet.microsoft.com/en-us/library/cc732969(v=WS.10).aspx
+
+    .EXAMPLE
+    Set-IisHttpRedirect -SiteName Peanuts -Destination 'http://new.peanuts.com'
+
+    Redirects all requests to the `Peanuts` website to `http://new.peanuts.com`.
+
+    .EXAMPLE
+    Set-IisHttpRedirect -SiteName Peanuts -Path Snoopy/DogHouse -Destination 'http://new.peanuts.com'
+
+    Redirects all requests to the `/Snoopy/DogHouse` path on the `Peanuts` website to `http://new.peanuts.com`.
+
+    .EXAMPLE
+    Set-IisHttpRedirect -SiteName Peanuts -Destination 'http://new.peanuts.com' -StatusCode 'Temporary'
+
+    Redirects all requests to the `Peanuts` website to `http://new.peanuts.com` with a temporary HTTP status code.  You can also specify `Found` (HTTP 302), or `Permanent` (HTTP 301).
+    #>
     [CmdletBinding(SupportsShouldProcess=$true)]
     param(
         [Parameter(Mandatory=$true)]
+        [string]
         # The site where the redirection should be setup.
         $SiteName,
         
-        [Parameter()]
+        [string]
         # The optional path where redirection should be setup.
         $Path = '',
         
@@ -748,15 +774,15 @@ function Set-IisHttpRedirect
         
         [ValidateSet('Found','Permanent','Temporary')]
         [string]
-        # The HTTP status code to use.
+        # The HTTP status code to use.  Default is `Found`.  Should be one of `Found` (HTTP 302), `Permanent` (HTTP 301), or `Temporary` (HTTP 307).
         $StatusCode = 'Found',
         
         [Switch]
-        # Redirect all requests to exact destination (instead of relative to destination).
+        # Redirect all requests to exact destination (instead of relative to destination).  I have no idea what this means.  [Maybe TechNet can help.](http://technet.microsoft.com/en-us/library/cc732969(v=WS.10).aspx)
         $ExactDestination,
         
         [Switch]
-        # Only redirect requests to content in site and/or path, but nothing below it.
+        # Only redirect requests to content in site and/or path, but nothing below it.  I have no idea what this means.  [Maybe TechNet can help.](http://technet.microsoft.com/en-us/library/cc732969(v=WS.10).aspx)
         $ChildOnly
     )
     
