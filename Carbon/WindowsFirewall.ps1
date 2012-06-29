e7a761f642e09ba18929d316231e4196bf9787a0
Aaron Jensen <ajensen@webmd.net>
2012-06-29 13:24:59 -0700

    Updating Assert-FirewallConfigurable documentation.

--- Carbon/WindowsFirewall.ps1
+++ Carbon/WindowsFirewall.ps1
@@ -14,6 +14,24 @@
 
 function Assert-FirewallConfigurable
 {
+    <#
+    .SYNOPSIS
+    Asserts that the Windows firewall is configurable and writes an error if it isn't.
+
+    .DESCRIPTION
+    The Windows firewall can only be configured if it is running.  This function checks test if it is running.  If it isn't, it writes out an error and returns `False`.  If it is running, it returns `True`.
+
+    .OUTPUTS
+    System.Boolean.
+
+    .EXAMPLE
+    Assert-FirewallConfigurable
+
+    Returns `True` if the Windows firewall can be configured, `False` if it can't.
+    #>
+    [CmdletBinding()]
+    param(
+    )
     if( (Get-Service 'Windows Firewall').Status -ne 'Running' ) 
     {
         Write-Error "Unable to configure firewall: Windows Firewall service isn't running."
