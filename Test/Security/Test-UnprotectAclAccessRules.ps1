9d2fe259a78763a6edbdfe58789d221b9bf22a9e
Aaron Jensen <ajensen@webmd.net>
2012-09-01 07:02:09 -0700

    Bug fix: adding missing license notices and fixing a broken unit test.

--- Test/Security/Test-UnprotectAclAccessRules.ps1
+++ Test/Security/Test-UnprotectAclAccessRules.ps1
@@ -1,11 +1,27 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-$parentFSPath = New-TempDir
-$childFSPath = Join-Path $parentFSPath 'TestUnprotectAclAccessRules'
+$parentFSPath = $null 
+$childFSPath = $null
 
 function Setup
 {
     & (Join-Path $TestDir ..\..\Carbon\Import-Carbon.ps1 -Resolve)
-
+	
+	$parentFSPath = New-TempDir
+	Join-Path $parentFSPath 'TestUnprotectAclAccessRules'
+	
     $null = New-Item $childFSPath -ItemType Container
     Grant-Permissions -Identity Everyone -Permissions FullControl -Path $childFSPath
 }
