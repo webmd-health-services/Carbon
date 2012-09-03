e0f829038408ac8ac4acd52a62b07a5d9145e9b6
Aaron Jensen <ajensen@webmd.net>
2012-09-03 13:39:46 -0700

    Fix issue #33: Install-MsmqMessageQueue: Can wait forever for a queue to get created.  It now waits 10 seconds.   Also added WhatIf support.

--- Carbon/MSMQ.ps1
+++ Carbon/MSMQ.ps1
@@ -251,7 +251,7 @@ function Install-MsmqMessageQueue
 
     Installs a private, transactional `CriticsQueue` queue.
     #>
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess=$true)]
     param(
         [Parameter(Mandatory=$true)]
         [string]
@@ -270,34 +270,73 @@ function Install-MsmqMessageQueue
     $queueArgs = @{ Name = $Name ; Private = $Private }
     $path = Get-MsmqMessageQueuePath @queueArgs 
     
+    $cmdletArgs = @{ }
+    if( $PSBoundParameters.ContainsKey( 'WhatIf' ) )
+    {
+        $cmdletArgs.WhatIf = $true
+    }
+    
     $logMessage = "MSMQ message queue '$Name'."
     if( Test-MsmqMessageQueue @queueArgs )
     {
         Write-Host "Re-creating $logMessage"
-        Remove-MsmqMessageQueue @queueArgs
+        Remove-MsmqMessageQueue @queueArgs @cmdletArgs
     }
     else
     {
         Write-Host "Creating $logMessage"
     }
     
-    # If you remove a queue, sometimes you can't immediately re-create it.  So, we keep trying until we can.
-    do
+    $MaxWait = [TimeSpan]'0:00:10'
+    $endAt = (Get-Date) + $MaxWait
+    $created = $false
+    if( $pscmdlet.ShouldProcess( $path, 'install MSMQ queue' ) )
     {
-        try
+        # If you remove a queue, sometimes you can't immediately re-create it.  So, we keep trying until we can.
+        do
         {
-            # Capture the return object, otherwise it gets sent down the pipeline and causes an error
-            $queue = [Messaging.MessageQueue]::Create( $path, $Transactional )
-            break
+            try
+            {
+                # Capture the return object, otherwise it gets sent down the pipeline and causes an error
+                $queue = [Messaging.MessageQueue]::Create( $path, $Transactional )
+                $created = $true
+                break
+            }
+            catch 
+            { 
+                if( $_.Exception.Message -like '*A workgroup installation computer does not support the operation.*' )
+                {
+                    Write-Error ("Can't create MSMSQ queues on this computer.  {0}" -f $_.Exception.Message)
+                    return
+                }
+            }
+            Start-Sleep -Milliseconds 100
         }
-        catch { }
-        Start-Sleep -Milliseconds 100
-    }
-    while( $true )
-    
-    while( -not (Test-MsmqMessageQueue @queueArgs) )
-    {
-        Start-Sleep -Milliseconds 100
+        while( -not $created -and (Get-Date) -lt $endAt )
+        
+        if( -not $created )
+        {
+            Write-Error ('Unable to create MSMQ queue {0}.' -f $path)
+            return
+        }
+        
+        $endAt = (Get-Date) + $MaxWait
+        $exists = $false
+        do
+        {
+            Start-Sleep -Milliseconds 100
+            if( (Test-MsmqMessageQueue @queueArgs) )
+            {
+                $exists = $true
+                break
+            }
+        }
+        while( (Get-Date) -lt $endAt -and -not $exists )
+        
+        if( -not $exists )
+        {
+            Write-Warning ('MSMSQ queue {0} created, but can''t be found.  Please double-check that the queue was created.' -f $path)
+        }
     }
 }
 
