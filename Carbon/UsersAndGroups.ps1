2f71e64bf26cc8272bb306f3fa55f9ef4000780c
Aaron Jensen <ajensen@webmd.net>
2012-10-18 15:13:31 -0700

    Creating Test-Identity function for checking if a local/domain user/group exists.

--- Carbon/UsersAndGroups.ps1
+++ Carbon/UsersAndGroups.ps1
@@ -263,6 +263,71 @@ function Remove-User
     }
 }
 
+function Test-Identity
+{
+    <#
+    .SYNOPSIS
+    Tests that a name is a valid Windows local or domain user/group.
+    
+    .DESCRIPTION
+    Attempts to convert an identity name into a `System.Security.Principal.SecurityIdentifer` object.  If the conversion succeeds, the name belongs to a valid local or domain user/group.  If conversion fails, the user/group doesn't exist. You can also optionally return the applicable `SecurityIdentifier` object.
+    
+    If the identity testing is in another domain, and there is no trust relationship between the current domain the identity's domain, `$false` will be returned even though the account could exist.
+    
+    .EXAMPLE
+    Test-Identity -Name 'Administrators
+    
+    Tests that a user or group called `Administrators` exists on the local computer.
+    
+    .EXAMPLE
+    Test-Identity -Name 'CARBON\Testers'
+    
+    Tests that a group called `Testers` exists in the `CARBON` domain.
+    
+    .EXAMPLE
+    Test-Identity -Name 'Tester' -PassThru
+    
+    Tests that a user or group named `Tester` exists and returns a `System.Security.Principal.SecurityIdentifier` object if it does.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The name of the identity to test.
+        $Name,
+        
+        [Switch]
+        # Returns a `System.Security.Principal.SecurityIdentifier` object if the identity exists.
+        $PassThru
+    )
+    
+    $ntAccount = New-Object Security.Principal.NTAccount $Name
+    try
+    {
+        $sid = $ntAccount.Translate([Security.Principal.SecurityIdentifier])
+        if( $PassThru )
+        {
+            return $sid
+        }
+        else
+        {
+            return $true
+        }
+    }
+    catch
+    {   
+        if( $_.Exception -and $_.Exception.InnerException -and $_.Exception.InnerException -is [SystemException] )
+        {
+            # If a local account doesn't exist, it looks like Translate will start talking to domain controllers.  This may include untrusted domain controllers.
+            if( $Name -like '*\*' )
+            {
+                Write-Error ("Unable to determine if identity {0} exists.  Usually this happens if the user's domain doesn't exist or there isn't a trust relationship between the current domain and the user's domain." -f $Name)
+            }
+        }
+        return $false
+    }
+}
+
 function Test-User
 {
     <#
---
+++ Test/UsersAndGroups/Test-TestIdentity.ps1
@@ -0,0 +1,66 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Setup
+{
+    & (Join-Path $TestDir ..\..\Carbon\Import-Carbon.ps1 -Resolve)
+}
+
+function TearDown
+{
+}
+
+function Test-ShouldFindLocalGroup
+{
+    Assert-True (Test-Identity -Name 'Administrators')
+}
+
+
+function Test-ShouldFindLocalUser
+{
+    Assert-True (Test-Identity -Name 'Administrator')
+}
+
+function Test-ShouldFindDomainUser
+{
+    Assert-True (Test-Identity -Name ('{0}\Administrator' -f $env:USERDOMAIN))
+}
+
+function Test-ShouldReturnSecurityIdentifier
+{
+    $sid = Test-Identity -Name 'Administrator' -PassThru
+    Assert-NotNull $sid
+    Assert-True ($sid -is [System.Security.Principal.SecurityIdentifier])
+}
+
+function Test-ShouldNotFindMissingLocalUser
+{
+    $error.Clear()
+    Assert-False (Test-Identity -Name 'IDoNotExistIHope')
+    Assert-Equal 1 $error.Count
+}
+
+function Test-ShouldNotFindUserInBadDomain
+{
+    $error.Clear()
+    Assert-False (Test-Identity -Name 'MISSINGDOMAIN\IDoNotExistIHope' -ErrorAction SilentlyContinue)
+    Assert-Equal 2 $error.Count
+}
+
+function Test-ShouldNotFindUserInCurrentDomain
+{
+    $error.Clear()
+    Assert-False (Test-Identity -Name ('{0}\IDoNotExistIHope' -f $env:USERDOMAIN) -ErrorAction SilentlyContinue)
+    Assert-Equal 2 $error.Count
+}
