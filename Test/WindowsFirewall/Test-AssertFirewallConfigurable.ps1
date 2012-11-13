fc0069f838dcc04fc9d9cc9fc13e7f8fdd557ab3
Aaron Jensen <ajensen@webmd.net>
2012-11-13 10:04:32 -0800

    Adding test for Assert-FirewallConfigurable.

---
+++ Test/WindowsFirewall/Test-AssertFirewallConfigurable.ps1
@@ -0,0 +1,42 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Setup
+{
+    & (Join-Path $TestDir ..\..\Carbon\Import-Carbon.ps1 -Resolve)
+}
+
+function TearDown
+{
+    Remove-Module Carbon
+}
+
+function Test-ShouldBeRunnable
+{
+    $firewallSvc = Get-Service -Name 'Windows Firewall'
+    Assert-NotNull $firewallSvc
+    $error.Clear()
+    if( $firewallSvc.Status -eq 'Running' )
+    {
+        $result = Assert-FirewallConfigurable
+        Assert-True $result
+        Assert-Equal 0 $error.Count
+    }
+    else
+    {
+        $result = Assert-FirewallConfigurable -ErrorAction SilentlyContinue
+        Assert-False $result
+        Assert-Equal 1 $error.Count
+    }
+}
