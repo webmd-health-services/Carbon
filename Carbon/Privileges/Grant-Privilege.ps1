7ca2071aec248481b70e88938f1df607bc1c8e54
Aaron Jensen <ajensen@webmd.net>
2013-01-01 13:55:22 -0800

    Moving privilege-related functions to their own files.

---
+++ Carbon/Privileges/Grant-Privilege.ps1
@@ -0,0 +1,95 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Grant-Privilege
+{
+    <#
+    .SYNOPSIS
+    Grants an identity priveleges to perform system operations.
+    
+    .DESCRIPTION
+    The most current list of privileges can be found [on Microsoft's website](http://msdn.microsoft.com/en-us/library/windows/desktop/aa375728(v=vs.85).aspx). Here is the most current list, as of November 2012:
+
+     * SeAuditPrivilege
+     * SeBackupPrivilege
+     * SeBatchLogonRight
+     * SeChangeNotifyPrivilege
+     * SeCreateGlobalPrivilege
+     * SeCreatePagefilePrivilege
+     * SeCreatePermanentPrivilege
+     * SeDebugPrivilege
+     * SeDenyBatchLogonRight
+     * SeDenyInteractiveLogonRight
+     * SeDenyNetworkLogonRight
+     * SeDenyRemoteInteractiveLogonRight
+     * SeDenyServiceLogonRight
+     * SeEnableDelegationPrivilege
+     * SeImpersonatePrivilege
+     * SeIncreaseBasePriorityPrivilege
+     * SeIncreaseQuotaPrivilege
+     * SeInteractiveLogonRight
+     * SeLoadDriverPrivilege
+     * SeLockMemoryPrivilege
+     * SeMachineAccountPrivilege
+     * SeManageVolumePrivilege
+     * SeNetworkLogonRight
+     * SeProfileSingleProcessPrivilege
+     * SeRestorePrivilege
+     * SeRemoteInteractiveLogonRight
+     * SeRemoteShutdownPrivilege
+     * SeReserveProcessorPrivilege
+     * SeSecurityPrivilege
+     * SeServiceLogonRight
+     * SeShutdownPrivilege
+     * SeSyncAgentPrivilege
+     * SeSystemEnvironmentPrivilege
+     * SeSystemProfilePrivilege
+     * SeSystemtimePrivilege
+     * SeTakeOwnershipPrivilege
+     * SeTcbPrivilege
+     * SeTrustedCredManAccessPrivilege
+     * SeUndockPrivilege
+     * SeUnsolicitedInputPrivilege
+
+    .LINK
+    http://msdn.microsoft.com/en-us/library/windows/desktop/aa375728(v=vs.85).aspx
+    
+    .LINK
+    Get-Privileges
+    
+    .LINK
+    Revoke-Privilege
+    
+    .LINK
+    Test-Privilege
+    
+    .EXAMPLE
+    Grant-Privilege -Identity Batcomputer -Privilege SeServiceLogonRight
+    
+    Grants the Batcomputer account the ability to logon as a service.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The identity to grant a privilege.
+        $Identity,
+        
+        [Parameter(Mandatory=$true)]
+        [string[]]
+        # The privileges to grant.
+        $Privilege
+    )
+    [Carbon.Lsa]::GrantPrivileges( $Identity, $Privilege )
+}
---
+++ Carbon/Privileges/Test-Privilege.ps1
@@ -0,0 +1,45 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Test-Privilege
+{
+    <#
+    .SYNOPSIS
+    Tests if an identity has a given privilege.
+    
+    .DESCRIPTION
+    Returns `true` if an identity has a privilege.  `False` otherwise.
+    
+    .EXAMPLE
+    Test-Privilege -Identity Forrester -Privilege SeServiceLogonRight
+    
+    Tests if `Forrester` has the `SeServiceLogonRight` privilege.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The identity whose privileges to check.
+        $Identity,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The privilege to check.
+        $Privilege
+    )
+    
+    $matchingPrivilege = Get-Privileges -Identity $Identity |
+                            Where-Object { $_ -eq $Privilege }
+    return ($matchingPrivilege -ne $null)
+}
