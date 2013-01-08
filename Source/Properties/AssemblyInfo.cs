531e13bb3b70e6d8f26db654cabe0ee0d36ffa97
Aaron Jensen <ajensen@webmd.net>
2013-01-07 20:44:55 -0800

    Fixes issue #51: version and sign the Carbon assembly.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -15,18 +15,17 @@
  */
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Carbon")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Assembly for Carbon, a PowerShell DevOps setup/configuration automation module.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Carbon")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © 2012 - 2013 Aaron Jensen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +47,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
---
+++ Test/Test-CarbonVersion.ps1
@@ -0,0 +1,47 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$expectedVersion = $null
+
+function Setup()
+{
+    & (Join-Path $TestDir ..\Carbon\Import-Carbon.ps1 -Resolve)
+
+    $line = Get-Content -Path (Join-Path $TestDir '..\RELEASE NOTES.txt' -Resolve) -TotalCount 1
+    if( $line -notmatch '(\d+\.\d+\.\d+\.\d+)' )
+    {
+        Fail 'Unable to find version number in release notes.'
+    }
+    
+    $expectedVersion = [Version]$matches[1]
+}
+
+function TearDown()
+{
+    Remove-Module Carbon
+}
+
+function Test-CarbonModuleVersionIsCorrect
+{
+    $moduleInfo = Get-Module -Name Carbon
+    Assert-NotNull $moduleInfo
+    Assert-Equal $expectedVersion $moduleInfo.Version 'Carbon module version not correct.'
+}
+
+function Test-CarbonAssemblyVersionIsCorrect
+{
+    $assembly = [Reflection.Assembly]::LoadFrom( (Join-Path $TestDir '..\Carbon\bin\Carbon.dll' -Resolve) )
+    Assert-NotNull $assembly
+    Assert-Equal $expectedVersion $assembly.GetName().Version 'Carbon assembly version not correct.'
+}
