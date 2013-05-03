84bb6c6ea909fb5d3b7c64aa10886d0d750d3cd8
Aaron Jensen <ajensen@webmd.net>
2013-05-03 13:26:22 -0700

    Upgrading to version 1.2.

--- Source/Test/Properties/AssemblyInfo.cs
+++ Source/Test/Properties/AssemblyInfo.cs
@@ -37,16 +37,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("29cfcfdb-d12b-43f6-9c7c-876dc8087da1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
