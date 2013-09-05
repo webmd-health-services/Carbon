0ccdb1a8e6fd18c0df6abe92f74da9b732d5e7ad
Aaron Jensen <ajensen@webmd.net>
2013-09-05 15:35:10 -0700

    Creating Test-IPAddress function to detect if an IP address is in use on the local computer.

---
+++ Carbon/Network/Test-IPAddress.ps1
@@ -0,0 +1,41 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Test-IPAddress
+{
+    <#
+    .SYNOPSIS
+    Tests that an IP address is in use on the local computer.
+
+    .DESCRIPTION
+    Sometimes its useful to know if an IP address is being used on the local computer.  This function does just that.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [Net.IPAddress]
+        # The IP address to check.
+        $IPAddress
+    )
+
+    $ip = Get-IPAddress | Where-Object { $_ -eq $IPAddress }
+    if( $ip )
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+}
---
+++ Test/Network/Test-TestIPAddress.ps1
@@ -0,0 +1,63 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function Setup
+{
+    & (Join-Path -Path $TestDir -ChildPath ..\..\Carbon\Import-Carbon.ps1 -Resolve)
+}
+
+function TearDown
+{
+}
+
+function Test-ShouldTestExistingIPV4Address
+{
+    Get-IPAddress -V4 | ForEach-Object { Assert-True (Test-IPAddress -IPAddress $_) }
+}
+
+function Test-ShouldTestExistingIPV4String
+{
+    Get-IPAddress -V4 | ForEach-Object { Assert-True (Test-IPAddress -IPAddress $_.ToString()) }
+}
+
+function Test-ShouldTestNonExistentIPV4Address
+{
+    Assert-False (Test-IPAddress -IPAddress ([Net.IPAddress]::Parse('255.255.255.0')))
+}
+
+function Test-ShouldTestNonExistentIPV4String
+{
+    Assert-False (Test-IPAddress -IPAddress '255.255.255.0')
+}
+
+
+function Test-ShouldHandleExistentIPV6Address
+{
+    Get-IPAddress -V6 | ForEach-Object { Assert-True (Test-IPAddress -IPAddress $_ ) }
+}
+
+function Test-ShouldHandleExistentIPV6String
+{
+    Get-IPAddress -V6 | ForEach-Object { Assert-True (Test-IPAddress -IPAddress $_.ToString() ) }
+}
+
+function Test-ShouldHandleNonExistentIP6Address
+{
+    Assert-False (Test-IPAddress -IPAddress ([Net.IPAddress]::Parse('::1')))
+}
+
+function Test-ShouldHandleNonExistentIPV6String
+{
+    Assert-False (Test-IPAddress -IPAddress '::1')
+}
