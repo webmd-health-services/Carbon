134de4dfd4e35b54b5d248e497296710aabdb184
James Crowley <james.crowley@gmail.com>
2013-11-20 14:26:26 +0000

    Suppress warning from net user when password is longer than 14 characters
    --HG--
    branch : developerfusion/suppress-warning-from-net-user-when-pass-1384957636524

--- Carbon/UsersAndGroups/Install-User.ps1
+++ Carbon/UsersAndGroups/Install-User.ps1
@@ -64,7 +64,7 @@ function Install-User
     if( $pscmdlet.ShouldProcess( $Username, "$operation local user" ) )
     {
         Write-Host "$logAction local user $Username."
-        & (Resolve-NetPath) user $Username $Password $addArg /Comment:$Description /Fullname:$FullName
+        & (Resolve-NetPath) user $Username $Password $addArg /Comment:$Description /Fullname:$FullName /Y
         
         if( -not $userExists )
         {
