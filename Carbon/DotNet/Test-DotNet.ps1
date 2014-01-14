f95397130550f1aaebbb0f7d5edce191e8124a73
Aaron Jensen <ajensen@webmd.net>
2014-01-13 16:04:44 -0800

    Adding missing license notice to Test-DotNet.ps1.

--- Carbon/DotNet/Test-DotNet.ps1
+++ Carbon/DotNet/Test-DotNet.ps1
@@ -1,25 +1,39 @@
-<#
-.SYNOPSIS
-Tests if .NET is installed.
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-.DESCRIPTION
-Currently, this function only tests if .NET 2 or 4 is installed.  Perhaps some friendly people out there will extend it to perform further checks?
-
-.LINK
-http://msdn.microsoft.com/en-us/kb/kbarticle.aspx?id=318785
-
-.EXAMPLE
-Test-DotNet -v2
-
-Demonstrates how to test if .NET 2 is installed.
-
-.EXAMPLE
-Test-DotNet -v4 -Full
-
-Demonstrates how to test if the full .NET v4 is installed.
-#>
 function Test-DotNet
 {
+    <#
+    .SYNOPSIS
+    Tests if .NET is installed.
+
+    .DESCRIPTION
+    Currently, this function only tests if .NET 2 or 4 is installed.  Perhaps some friendly people out there will extend it to perform further checks?
+
+    .LINK
+    http://msdn.microsoft.com/en-us/kb/kbarticle.aspx?id=318785
+
+    .EXAMPLE
+    Test-DotNet -v2
+
+    Demonstrates how to test if .NET 2 is installed.
+
+    .EXAMPLE
+    Test-DotNet -v4 -Full
+
+    Demonstrates how to test if the full .NET v4 is installed.
+    #>
     param(
         [Parameter(Mandatory=$true,ParameterSetName='v2')]
         [Switch]
