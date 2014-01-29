8801b0fd7cba472df937c9fd41f6a251b10931b8
Aaron Jensen <ajensen@webmd.net>
2014-01-29 14:39:50 -0800

    Adding missing C# files used when resolving identities.

---
+++ Source/IdentityType.cs
@@ -0,0 +1,17 @@
+﻿namespace Carbon
+{
+    // http://msdn.microsoft.com/en-us/library/windows/desktop/aa379601.aspx
+    public enum IdentityType
+    {
+        User = 1,
+        Group,
+        Domain,
+        Alias,
+        WellKnownGroup,
+        DeletedAccount,
+        Invalid,
+        Unknown,
+        Computer,
+        Label
+    }
+}
---
+++ Source/Kernel32.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Carbon
+{
+    public sealed class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LocalFree(IntPtr hMem);
+    }
+}
