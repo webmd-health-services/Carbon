054a14a8b4dcb2a190c09521c0cb12b2a8590910
Aaron Jensen <ajensen@webmd.net>
2014-07-21 16:34:03 -0700

    Getting tests to pass under Windows 2012 R2: MSMQ tests can only run if computer and user running tests are on the same domain.
    --HG--
    branch : 1.0

--- Test/MSMQ/Test-TestMSMQMessageQueue.ps1
+++ Test/MSMQ/Test-TestMSMQMessageQueue.ps1
@@ -14,7 +14,9 @@
 
 
 # These tests should only run if MSMQ is not installed
-if( Get-Service -Name MSMQ -ErrorAction SilentlyContinue )
+$userDomain = $env:USERDNSDOMAIN
+$computerDomain = Get-WmiObject 'Win32_ComputerSystem' | Select-Object -ExpandProperty Domain
+if( Get-Service -Name MSMQ -ErrorAction SilentlyContinue -and $userDomain -eq $computerDomain )
 {
 
     $publicQueueName = 'CarbonTestGetQueue-Public'
@@ -58,5 +60,5 @@ if( Get-Service -Name MSMQ -ErrorAction SilentlyContinue )
 }
 else
 {
-    Write-Warning "Tests for Test-MSMQMessageQueue not run because MSMQ is not installed."
+    Write-Warning ("Tests for Get-MSMQMessageQueue not run because MSMQ is not installed or the current user's domain ({0}) and the computer's domain ({1}) are different." -f $userDomain,$computerDomain)
 }
