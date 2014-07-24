c8a4b2e068a266576d5fa8b5ce57402a83ffbd60
Aaron Jensen <ajensen@webmd.net>
2014-07-23 22:30:02 -0700

    DSC: creating FirewallRule resource for configuring firewall rules.

---
+++ Carbon/DscResources/Carbon_FirewallRule/Carbon_FirewallRule.psm1
@@ -0,0 +1,381 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+& (Join-Path -Path $PSScriptRoot -ChildPath '..\Initialize-CarbonDscResource.ps1' -Resolve)
+
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([Hashtable])]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Name,
+
+        [bool]
+        $Enabled = $true,
+
+        [ValidateSet('In','Out')]
+        [string]
+        $Direction,
+
+        [ValidateSet('Any','Domain','Private','Public')]
+        [string[]]
+        $Profile = @( 'Any' ),
+
+        [string]
+        $LocalIPAddress = 'Any',
+
+        [string]
+        $LocalPort,
+
+        [string]
+        $RemoteIPAddress = 'Any',
+
+        [string]
+        $RemotePort,
+
+        [string]
+        $Protocol = 'Any',
+
+        [ValidateSet('Yes', 'No', 'DeferUser','DeferApp')]
+        [string]
+        $EdgeTraversalPolicy = 'No',
+
+        [ValidateSet('Allow','Block','Bypass')]
+        [string]
+        $Action,
+
+        [ValidateSet('Any','Wireless','LAN','RAS')]
+        [string]
+        $InterfaceType = 'Any',
+
+        [ValidateSet('NotRequired','Authenticate','AuthEnc','AuthDynEnc','AuthNoEncap')]
+        [string]
+        $Security = 'NotRequired',
+
+        [string]
+        $Description,
+
+        [string]
+        $Program,
+
+        [string]
+        $Service,
+
+        [ValidateSet('Present','Absent')]
+        [string]
+        $Ensure = 'Present'
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $rule = Get-FirewallRule -LiteralName $Name
+    
+    $resource = @{ 
+                    'Action' = $Action;
+                    'Description' = $Description;
+                    'Direction' = $Direction;
+                    'EdgeTraversalPolicy' = $EdgeTraversalPolicy
+                    'Enabled' = $Enabled;
+                    'Ensure' = 'Absent';
+                    'InterfaceType' = $InterfaceType
+                    'LocalIPAddress' = $LocalIPAddress;
+                    'LocalPort' = $LocalPort;
+                    'Name' = $Name;
+                    'Profile' = $Profile;
+                    'Program' = $Program;
+                    'Protocol' = $Protocol;
+                    'RemoteIPAddress' = $RemoteIPAddress;
+                    'RemotePort' = $RemotePort;
+                    'Security' = $Security;
+                    'Service' = $Service;
+                }
+
+    if( $rule )
+    {
+        $propNames = $resource.Keys | ForEach-Object { $_ }
+        $propNames | 
+            Where-Object { $_ -ne 'Ensure' } |
+            ForEach-Object { 
+                $propName = $_
+                switch( $propName )
+                {
+                    'Profile' { $value = $rule.Profile.ToString() -split ',' }
+                    'Enabled' { $value = $rule.Enabled }
+                    default
+                    {
+                        $value = ($rule.$propName).ToString()
+                    }
+                }
+
+                $resource[$propName] = $value
+            }
+        $resource.Ensure = 'Present'
+    }
+
+    return $resource
+}
+ 
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Name,
+
+        [bool]
+        $Enabled = $true,
+
+        [ValidateSet('In','Out')]
+        [string]
+        $Direction,
+
+        [ValidateSet('Any','Domain','Private','Public')]
+        [string[]]
+        $Profile,
+
+        [string]
+        $LocalIPAddress,
+
+        [string]
+        $LocalPort,
+
+        [string]
+        $RemoteIPAddress,
+
+        [string]
+        $RemotePort,
+
+        [string]
+        $Protocol,
+
+        [ValidateSet('Yes', 'No', 'DeferUser','DeferApp')]
+        [string]
+        $EdgeTraversalPolicy,
+
+        [ValidateSet('Allow','Block','Bypass')]
+        [string]
+        $Action,
+
+        [ValidateSet('Any','Wireless','LAN','RAS')]
+        [string]
+        $InterfaceType,
+
+        [ValidateSet('NotRequired','Authenticate','AuthEnc','AuthDynEnc','AuthNoEncap')]
+        [string]
+        $Security,
+
+        [string]
+        $Description,
+
+        [string]
+        $Program,
+
+        [string]
+        $Service,
+
+        [ValidateSet('Present','Absent')]
+        [string]
+        $Ensure = 'Present'
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $resource = Get-TargetResource -Name $Name
+    if( $Ensure -eq 'Absent' -and $resource.Ensure -eq 'Present' )
+    {
+        Write-Verbose ('Deleting firewall rule ''{0}''' -f $Name)
+        $output = netsh advfirewall firewall delete rule name=$Name
+        if( $LASTEXITCODE )
+        {
+            Write-Error ($output -join ([Environment]::NewLine))
+            return
+        }
+        $output | Write-Verbose
+        return
+    }
+
+    $cmd = 'add'
+    $cmdDisplayName = 'Adding'
+    $newArg = ''
+    if( $Ensure -eq 'Present' -and $resource.Ensure -eq 'Present' )
+    {
+        $cmd = 'set'
+        $cmdDisplayName = 'Setting'
+        $newArg = 'new'
+    }
+    else
+    {
+        if( -not $Direction -and -not $Action )
+        {
+            Write-Error ('Parameters ''Direction'' and ''Action'' are required when adding a new firewall rule.')
+            return
+        }
+        elseif( -not $Direction )
+        {
+            Write-Error ('Parameter ''Direction'' is required when adding a new firewall rule.')
+            return
+        }
+        elseif( -not $Action )
+        {
+            Write-Error ('Parameter ''Action'' is required when adding a new firewall rule.')
+            return
+        }
+    }
+
+    $argMap = @{
+                    'Direction' = 'dir';
+                    'Enabled' = 'enable';
+                    'LocalIPAddress' = 'localip';
+                    'RemoteIPAddress' = 'remoteip';
+                    'EdgeTraversalPolicy' = 'edge';
+              }
+
+    $netshArgs = New-Object 'Collections.Generic.List[string]'
+    $resource.Keys |
+        Where-Object { $_ -ne 'Ensure' -and $_ -ne 'Name' } |
+        Where-Object { $PSBoundParameters.ContainsKey($_) } |
+        ForEach-Object {
+            $argName = $_.ToLowerInvariant()
+            $argValue = $PSBoundParameters[$argName]
+            if( $argValue -is [bool] )
+            {
+                $argValue = if( $argValue ) { 'yes' } else { 'no' }
+            }
+            if( $argMap.ContainsKey($argName) )
+            {
+                $argName = $argMap[$argName]
+            }
+
+            [void]$netshArgs.Add( ('{0}=' -f $argName) )
+            [void]$netshArgs.Add( $argValue )
+        }
+    
+    Write-Verbose ('{0} firewall rule ''{1}''' -f $cmdDisplayName,$Name)
+    $output = netsh advfirewall firewall $cmd rule name= $Name $newArg $netshArgs 
+    if( $LASTEXITCODE )
+    {
+        Write-Error ($output -join ([Environment]::NewLine))
+        return
+    }
+    $output | Write-Verbose
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Name,
+
+        [bool]
+        $Enabled,
+
+        [ValidateSet('In','Out')]
+        [string]
+        $Direction,
+
+        [ValidateSet('Any','Domain','Private','Public')]
+        [string[]]
+        $Profile,
+
+        [string]
+        $LocalIPAddress,
+
+        [string]
+        $LocalPort,
+
+        [string]
+        $RemoteIPAddress,
+
+        [string]
+        $RemotePort,
+
+        [string]
+        $Protocol,
+
+        [ValidateSet('Yes', 'No', 'DeferUser','DeferApp')]
+        [string]
+        $EdgeTraversalPolicy,
+
+        [ValidateSet('Allow','Block','Bypass')]
+        [string]
+        $Action,
+
+        [ValidateSet('Any','Wireless','LAN','RAS')]
+        [string]
+        $InterfaceType,
+
+        [ValidateSet('NotRequired','Authenticate','AuthEnc','AuthDynEnc','AuthNoEncap')]
+        [string]
+        $Security,
+
+        [string]
+        $Description,
+
+        [string]
+        $Program,
+
+        [string]
+        $Service,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('Present','Absent')]
+        [string]
+        $Ensure
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $resource = Get-TargetResource @PSBoundParameters
+    if( $Ensure -eq 'Absent' )
+    {
+        $result = ($resource.Ensure -eq 'Absent')
+        if( $result )
+        {
+            Write-Verbose ('Firewall rule ''{0}'' not found.' -f $Name)
+        }
+        else
+        {
+            Write-Verbose ('Firewall rule ''{0}'' found.' -f $Name)
+        }
+        return $result
+    }
+
+    if( $Ensure -eq 'Present' -and $resource.Ensure -eq 'Absent' )
+    {
+        Write-Verbose ('Firewall rule ''{0}'' not found.' -f $Name)
+        return $false
+    }
+
+    $notEqualProperties = $resource.Keys | 
+                            Where-Object { $_ -ne 'Ensure' } |  
+                            Where-Object { $PSBoundParameters.ContainsKey( $_ ) } |
+                            Where-Object { $resource.$_ -ne $PSBoundParameters[$_] }
+
+    if( $notEqualProperties )
+    {
+        Write-Verbose ('Firewall rule ''{0}'' has stale properties: ''{1}''' -f $Name,($notEqualProperties -join ''','''))
+        return $false
+    }
+
+    return $true
+
+}
+
+
---
+++ Test/DscResources/Test-FirewallRule.ps1
@@ -0,0 +1,333 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'CarbonDscTest.psm1' -Resolve) -Force
+$RuleName = 'CarbonDscFirewallRule'
+
+function Start-TestFixture
+{
+    Start-CarbonDscTestFixture 'FirewallRule'
+}
+
+function Start-Test
+{
+    Remove-FirewallRule
+}
+
+function Stop-Test
+{
+    Remove-FirewallRule
+}
+
+function Stop-TestFixture
+{
+    Stop-CarbonDscTestFixture
+}
+
+function Test-ShouldGetPresentTargetResource
+{
+    $rule = Get-FirewallRule | Select-Object -First 1
+    $resource = Get-TargetResource -Name $rule.Name -Direction $rule.Direction -Action $rule.Action
+    Assert-NotNull $resource
+    foreach( $propName in $resource.Keys )
+    {
+        if( $propName -eq 'Ensure' )
+        {
+            continue
+        }
+        Assert-Equal $rule.$propName $resource[$propName]
+    }
+    Assert-DscResourcePresent $resource
+}
+
+function Test-ShouldGetAbsentTargetResource
+{
+    $resource = Get-TargetResource -Name $RuleName -Direction 'In' -Action 'Allow'
+    Assert-NotNull $resource
+    Assert-Equal $RuleName $resource.Name
+    Assert-Equal 'In' $resource.Direction
+    Assert-Equal 'Allow' $resource.Action
+    Assert-Empty $resource.Description
+    Assert-Equal 'No' $resource.EdgeTraversalPolicy
+    Assert-True $resource.Enabled
+    Assert-Equal 'Any' $resource.InterfaceType
+    Assert-Equal 'Any' $resource.LocalIPAddress
+    Assert-Empty $resource.LocalPort
+    Assert-Equal 'Any' $resource.Profile
+    Assert-Empty $resource.Program
+    Assert-Equal 'Any' $resource.Protocol
+    Assert-Equal 'Any' $resource.RemoteIPAddress
+    Assert-Empty $resource.RemotePort
+    Assert-Equal 'NotRequired' $resource.Security
+    Assert-Empty $resource.Service
+    Assert-DscResourceAbsent $resource
+}
+
+function Test-ShouldGetAbsentTargetResourceUsesParamValues
+{
+    $resource = Get-TargetResource -Name 'Name' `
+                                   -Direction Out `
+                                   -Action Block `
+                                   -Enabled:$false `
+                                   -Profile Public `
+                                   -LocalIPAddress '10.1.2.3' `
+                                   -LocalPort '6500' `
+                                   -RemoteIPAddress '192.168.1.1' `
+                                   -RemotePort '5600' `
+                                   -Protocol 'tcp' `
+                                   -EdgeTraversalPolicy Yes `
+                                   -InterfaceType Wireless `
+                                   -Security AuthEnc `
+                                   -Description 'description' `
+                                   -Program 'C:\program.exe' `
+                                   -Service 'C:\service.exe' `
+                                   -Ensure Present
+    Assert-NotNull $resource
+    Assert-Equal 'Name' $resource.Name
+    Assert-Equal 'Out' $resource.Direction
+    Assert-Equal 'Block' $resource.Action
+    Assert-Equal 'Description' $resource.Description
+    Assert-Equal 'Yes' $resource.EdgeTraversalPolicy
+    Assert-False $resource.Enabled
+    Assert-Equal 'Wireless' $resource.InterfaceType
+    Assert-Equal '10.1.2.3' $resource.LocalIPAddress
+    Assert-Equal '6500' $resource.LocalPort
+    Assert-Equal 'Public' $resource.Profile
+    Assert-Equal 'C:\program.exe' $resource.Program
+    Assert-Equal 'tcp' $resource.Protocol
+    Assert-Equal '192.168.1.1' $resource.RemoteIPAddress
+    Assert-Equal '5600' $resource.RemotePort
+    Assert-Equal 'AuthEnc' $resource.Security
+    Assert-Equal 'C:\service.exe' $resource.Service
+    Assert-DscResourceAbsent $resource
+}
+
+function Test-ShouldTestTargetResource
+{
+    Assert-Null (Get-FirewallRule -Name $RuleName)
+    Assert-False (Test-TargetResource -Name $RuleName -Direction In -Action Allow -Ensure Present)
+    Assert-True (Test-TargetResource -Name $RuleName -Direction In -Action Allow -Ensure Absent)
+    $rule = Get-FirewallRule | select -First 1
+    Assert-NotNull $rule
+    Assert-True (Test-TargetResource -Name $rule.Name -Direction $rule.Direction -Action $rule.Action -Ensure Present)
+    Assert-False (Test-TargetResource -Name $rule.Name -Direction $rule.Direction -Action $rule.Action -Ensure Absent)
+}
+
+function Test-ShouldLookAtEachPropertyWhenTestingTargetResource
+{
+    netsh advfirewall firewall add rule name=$ruleName dir=in action=allow
+    $testParams = @{
+                        Name = $ruleName;
+                        Direction = 'In';
+                        Action = 'Allow';
+                        Ensure = 'Present';
+                    }
+    Assert-True (Test-TargetResource @testParams)
+    Assert-False (Test-TargetResource @testParams -Enabled:$false)
+    Assert-False (Test-TargetResource @testParams -Profile Private)
+    Assert-False (Test-TargetResource @testParams -LocalIPAddress '10.1.1.2')
+    Assert-False (Test-TargetResource @testParams -LocalPort '6500')
+    Assert-False (Test-TargetResource @testParams -RemoteIPAddress '10.2.2.3')
+    Assert-False (Test-TargetResource @testParams -RemotePort '8700')
+    Assert-False (Test-TargetResource @testParams -Protocol 'tcp')
+    Assert-False (Test-TargetResource @testParams -EdgeTraversalPolicy DeferApp)
+    Assert-False (Test-TargetResource @testParams -InterfaceType LAN)
+    Assert-False (Test-TargetResource @testParams -Security AuthDynEnc)
+    Assert-False (Test-TargetResource @testParams -Description 'description')
+    Assert-False (Test-TargetResource @testParams -Program 'C:\program.exe')
+    Assert-False (Test-TargetResource @testParams -Service $ruleName)
+}
+
+function Test-ShouldSetTargetResource
+{
+    $name = $RuleName
+
+    Set-TargetResource -Name $name `
+                        -Enabled:$false `
+                        -Direction Out `
+                        -Action Block `
+                        -Profile Private `
+                        -LocalIPAddress '10.1.2.3' `
+                        -LocalPort '60543' `
+                        -Protocol 'tcp' `
+                        -RemoteIPAddress '10.3.2.1' `
+                        -RemotePort '34556' `
+                        -InterfaceType LAN `
+                        -Description 'description' `
+                        -Program 'C:\program.exe' `
+                        -Service $name `
+                        -Ensure Present
+
+    $rule = Get-FirewallRule -Name $name
+    Assert-NotNull $rule
+    Assert-False $rule.Enabled
+    Assert-Equal 'Out' $rule.Direction
+    Assert-Equal 'Private' $rule.Profile
+    Assert-Equal '10.1.2.3/32' $rule.LocalIPAddress
+    Assert-Equal '60543' $rule.LocalPort
+    Assert-Equal '10.3.2.1/32' $rule.RemoteIPAddress
+    Assert-Equal '34556' $rule.RemotePort
+    Assert-Equal 'tcp' $rule.Protocol
+    Assert-Equal 'No' $rule.EdgeTraversalPolicy
+    Assert-Equal 'Block' $rule.Action
+    Assert-Equal 'LAN' $rule.InterfaceType
+    Assert-Equal 'NotRequired' $rule.Security
+    Assert-Equal 'description' $rule.Description
+    Assert-Equal 'C:\program.exe' $rule.Program
+    Assert-Equal $name $rule.Service
+
+    # Now, let's ensure we can change things
+    Set-TargetResource -Name $name `
+                        -Enabled:$true `
+                        -Direction In `
+                        -Action Allow `
+                        -Profile Public `
+                        -LocalIPAddress '10.4.5.6' `
+                        -LocalPort '59432' `
+                        -Protocol 'udp' `
+                        -RemoteIPAddress '10.6.5.4' `
+                        -RemotePort '23456' `
+                        -InterfaceType Wireless `
+                        -Description 'description 2' `
+                        -Program 'C:\program2.exe' `
+                        -Service ('{0}2' -f $name) `
+                        -Ensure Present
+
+    $rule = Get-FirewallRule -Name $name
+    Assert-NotNull $rule
+    Assert-True $rule.Enabled
+    Assert-Equal 'In' $rule.Direction
+    Assert-Equal 'Public' $rule.Profile
+    Assert-Equal '10.4.5.6/32' $rule.LocalIPAddress
+    Assert-Equal '59432' $rule.LocalPort
+    Assert-Equal '10.6.5.4/32' $rule.RemoteIPAddress
+    Assert-Equal '23456' $rule.RemotePort
+    Assert-Equal 'udp' $rule.Protocol
+    Assert-Equal 'No' $rule.EdgeTraversalPolicy
+    Assert-Equal 'Allow' $rule.Action
+    Assert-Equal 'Wireless' $rule.InterfaceType
+    Assert-Equal 'NotRequired' $rule.Security
+    Assert-Equal 'description 2' $rule.Description
+    Assert-Equal 'C:\program2.exe' $rule.Program
+    Assert-Equal ('{0}2' -f $name) $rule.Service
+
+    Set-TargetResource -Name $name -Ensure Absent
+    Assert-Null (Get-FirewallRule -Name $name)
+}
+
+function Test-ShouldSetWithDefaults
+{
+    Set-TargetResource -Name $RuleName -Direction In -Action Allow
+    $rule = Get-FirewallRule -Name $RuleName
+    Assert-NotNull $rule
+    Assert-Equal $RuleName $rule.Name
+    Assert-Equal 'In' $rule.Direction
+    Assert-Equal 'Allow' $rule.Action
+    Assert-Empty $rule.Description
+    Assert-Equal 'No' $rule.EdgeTraversalPolicy
+    Assert-True $rule.Enabled
+    Assert-Equal 'Any' $rule.InterfaceType
+    Assert-Equal 'Any' $rule.LocalIPAddress
+    Assert-Empty $rule.LocalPort
+    Assert-Equal 'Domain, Private, Public' $rule.Profile
+    Assert-Empty $rule.Program
+    Assert-Equal 'Any' $rule.Protocol
+    Assert-Equal 'Any' $rule.RemoteIPAddress
+    Assert-Empty $rule.RemotePort
+    Assert-Equal 'NotRequired' $rule.Security
+    Assert-Empty $rule.Service
+}
+
+
+function Test-ShouldSetSecurityAndEdge
+{
+    # Set a firewall rule with security and edge. Requires inbound rule.
+    Set-TargetResource -Name $RuleName -Direction In -Action Allow -Security AuthEnc -EdgeTraversalPolicy Yes -Ensure Present
+    $rule = Get-FirewallRule -Name $RuleName
+    Assert-NotNull $rule
+    Assert-Equal $RuleName $rule.Name
+    Assert-Equal 'In' $rule.Direction
+    Assert-Equal 'Allow' $rule.Action
+    Assert-Equal 'AuthEnc' $rule.Security
+    Assert-Equal 'Yes' $rule.EdgeTraversalPolicy
+
+    Set-TargetResource -Name $RuleName -Direction In -Action Allow -Security Authenticate -EdgeTraversalPolicy No -Ensure Present
+    $rule = Get-FirewallRule -Name $RuleName
+    Assert-NotNull $rule
+    Assert-Equal 'Authenticate' $rule.Security
+    Assert-Equal 'No' $rule.EdgeTraversalPolicy
+}
+
+function Test-ShouldRequireDirectionAndActionWhenAddingNewRule
+{
+    Set-TargetResource -Name $RuleName -ErrorAction SilentlyContinue
+    Assert-Error -Last -Regex '\bDirection\b.*\bAction\b'
+    Assert-Null (Get-FirewallRule -Name $RuleName)
+}
+
+configuration DscConfiguration
+{
+    param(
+        $Ensure
+    )
+
+    Set-StrictMode -Off
+
+    Import-DscResource -Name '*' -Module 'Carbon'
+
+    node 'localhost'
+    {
+        FirewallRule set
+        {
+            Name = $RuleName;
+            Direction = 'In';
+            Action = 'Allow';
+            Ensure = $Ensure;
+        }
+    }
+}
+
+function Test-ShouldRunThroughDsc
+{
+    & DscConfiguration -Ensure 'Present' -OutputPath $CarbonDscOutputRoot
+    
+    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot
+    Assert-NoError
+
+    $rule = Get-FirewallRule -Name $RuleName
+    Assert-NotNull $rule
+    Assert-Equal $RuleName $rule.Name
+    Assert-Equal 'In' $rule.Direction
+    Assert-Equal 'Allow' $rule.Action
+
+    & DscConfiguration -Ensure 'Absent' -OutputPath $CarbonDscOutputRoot 
+    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot
+    Assert-NoError
+
+    $rule = Get-FirewallRule -Name $RuleName
+    Assert-Null $rule
+
+}
+
+function Remove-FirewallRule
+{
+    param(
+        $Name = $RuleName
+    )
+
+    if( Get-FirewallRule -Name $Name )
+    {
+        netsh advfirewall firewall delete rule name=$Name
+    }
+}
