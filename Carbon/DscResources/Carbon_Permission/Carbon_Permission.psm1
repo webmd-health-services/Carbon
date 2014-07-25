cabc280c9167a4dfad46cd77b2d6afa8967f9922
Aaron Jensen <ajensen@webmd.net>
2014-07-25 14:38:03 -0700

    DSC: creating Permission resource for configuring file, directory, and registry key permissions.

---
+++ Carbon/DscResources/Carbon_Permission/Carbon_Permission.psm1
@@ -0,0 +1,226 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+& (Join-Path -Path $PSScriptRoot -ChildPath '..\Initialize-CarbonDscResource.ps1' -Resolve)
+
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([hashtable])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The path on which the permissions should be granted.  Can be a file system or registry path.
+        $Path,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The user or group getting the permissions.
+        $Identity,
+        
+        [string[]]
+        # The permission: e.g. FullControl, Read, etc.  For file system items, use values from [System.Security.AccessControl.FileSystemRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.filesystemrights.aspx).  For registry items, use values from [System.Security.AccessControl.RegistryRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.registryrights.aspx).
+        $Permission,
+        
+        [ValidateSet('Container','SubContainers','ContainerAndSubContainers','Leaves','ContainerAndLeaves','SubContainersAndLeaves','ContainerAndSubContainersAndLeaves','ChildContainers','ContainerAndChildContainers','ChildLeaves','ContainerAndChildLeaves','ChildContainersAndChildLeaves','ContainerAndChildContainersAndChildLeaves')]
+        [string]
+        # How to apply container permissions.  This controls the inheritance and propagation flags.  Default is full inheritance, e.g. `ContainersAndSubContainersAndLeaves`. This parameter is ignored if `Path` is to a leaf item.
+        $ApplyTo,
+        
+        [ValidateSet('Present','Absent')]
+        [string]
+        # Should the user exist or not exist?
+        $Ensure
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $resource = @{
+                    Path = $Path;
+                    Identity = $Identity;
+                    Permission = @();
+                    ApplyTo = $null;
+                    Ensure = 'Absent';
+                }
+
+    $perm = Get-Permission -Path $Path -Identity $Identity
+    if( $perm )
+    {
+        [string[]]$resource.Permission = $perm.FileSystemRights.ToString() -split ',' | ForEach-Object { $_.Trim() } | Where-Object { $_ -ne 'Synchronize' }
+        $resource.ApplyTo = ConvertTo-ContainerInheritanceFlags -InheritanceFlags $perm.InheritanceFlags -PropagationFlags $perm.PropagationFlags
+        $resource.Ensure = 'Present'
+    }
+
+    return $resource
+}
+
+
+function Set-TargetResource
+{
+    <#
+    .SYNOPSIS
+    Grants/revokes a user or group permission to a file, directory, or registry key.
+
+    .DESCRIPTION
+
+    ## Granting Permission
+
+    Permissions are granted when the `Ensure` property is set to `Present`.
+    
+    When granting permissions, you *must* supply a value for the `Permission` property. When granting permission to a file or directory, the values for the `Permission` property must be a valid [System.Security.AccessControl.FileSystemRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.filesystemrights.aspx) enumeration value. When granting permission to a registry key, `Permission` must be a valid [System.Security.AccessControl.RegistryRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.registryrights.aspx) enumeration value.
+    
+    The value of the `ApplyTo` property must be a valid `Carbon.Security.ContainerInheritanceFlags` enumeration value. For a list of values, run `[Enum]::GetValues([Carbon.Security.ContainerInheritanceFlags])`. For help on choosing a proper value, see the help for `Grant-Permission`.
+
+    ## Revoking Permission
+        
+    Permissions are revoked when the `Ensure` property is set to `Absent`. *All* a user or group's permissions are revoked. You can't revoke part of a user's access. If you want to revoke part of a user's access, set the `Ensure` property to `Present` and the `Permissions` property to the list of properties you want the user to have.
+
+    .LINK
+    Grant-Permission
+
+    .LINK
+    Revoke-Permission
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The path on which the permissions should be granted.  Can be a file system or registry path.
+        $Path,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The user or group getting the permissions.
+        $Identity,
+        
+        [string[]]
+        # The permission: e.g. FullControl, Read, etc. Mandatory when granting permission. For file system items, use values from [System.Security.AccessControl.FileSystemRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.filesystemrights.aspx).  For registry items, use values from [System.Security.AccessControl.RegistryRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.registryrights.aspx).
+        $Permission,
+        
+        [ValidateSet('Container','SubContainers','ContainerAndSubContainers','Leaves','ContainerAndLeaves','SubContainersAndLeaves','ContainerAndSubContainersAndLeaves','ChildContainers','ContainerAndChildContainers','ChildLeaves','ContainerAndChildLeaves','ChildContainersAndChildLeaves','ContainerAndChildContainersAndChildLeaves')]
+        [string]
+        # How to apply container permissions.  This controls the inheritance and propagation flags.  Default is full inheritance, e.g. `ContainersAndSubContainersAndLeaves`. This parameter is ignored if `Path` is to a file.
+        $ApplyTo,
+        
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('Present','Absent')]
+        [string]
+        # If set to `Present`, permissions are set. If `Absent`, all permissions to `$Path` removed.
+        $Ensure
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+
+    if( $PSBoundParameters.ContainsKey('Ensure') )
+    {
+        $PSBoundParameters.Remove('Ensure')
+    }
+    
+    if( $Ensure -eq 'Absent' )
+    {
+        Write-Verbose ('Revoking permission for ''{0}'' to ''{1}''' -f $Identity,$Path)
+        Revoke-Permission -Path $Path -Identity $Identity
+    }
+    else
+    {
+        if( -not $Permission )
+        {
+            Write-Error ('Permission parameter is mandatory when granting permissions.')
+            return
+        }
+
+        Write-Verbose ('Granting permission for ''{0}'' to ''{1}'': {2}' -f $Identity,$Path,($Permission -join ','))
+        Grant-Permission @PSBoundParameters
+    }
+}
+
+
+function Test-TargetResource
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The path on which the permissions should be granted.  Can be a file system or registry path.
+        $Path,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The user or group getting the permissions.
+        $Identity,
+        
+        [Parameter(Mandatory=$true)]
+        [string[]]
+        # The permission: e.g. FullControl, Read, etc.  For file system items, use values from [System.Security.AccessControl.FileSystemRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.filesystemrights.aspx).  For registry items, use values from [System.Security.AccessControl.RegistryRights](http://msdn.microsoft.com/en-us/library/system.security.accesscontrol.registryrights.aspx).
+        $Permission,
+        
+        [ValidateSet('Container','SubContainers','ContainerAndSubContainers','Leaves','ContainerAndLeaves','SubContainersAndLeaves','ContainerAndSubContainersAndLeaves','ChildContainers','ContainerAndChildContainers','ChildLeaves','ContainerAndChildLeaves','ChildContainersAndChildLeaves','ContainerAndChildContainersAndChildLeaves')]
+        [string]
+        # How to apply container permissions.  This controls the inheritance and propagation flags.  Default is full inheritance, e.g. `ContainersAndSubContainersAndLeaves`. This parameter is ignored if `Path` is to a leaf item.
+        $ApplyTo,
+        
+        [ValidateSet('Present','Absent')]
+        [string]
+        # Should the user exist or not exist?
+        $Ensure
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $providerName = Get-PathProvider -Path $Path
+    if( -not $providerName )
+    {
+        Write-Error ('Path ''{0}'' not supported. Only file system and registry paths are allowed.')
+        return
+    }
+    $providerName = $providerName.Name
+
+    $resource = Get-TargetResource -Identity $Identity -Path $Path
+    $desiredRights = $Permission -join ','
+    $currentRights = $resource.Permission -join ','
+    
+    if( $Ensure -eq 'Absent' )
+    {
+        if( $resource.Ensure -eq 'Absent' )
+        {
+            Write-Verbose ('Identity ''{0}'' has no permission to ''{1}''' -f $Identity,$Path)
+            return $true
+        }
+        
+        Write-Verbose ('Identity ''{0}'' has permission to ''{1}'': {2}' -f $Identity,$Path,$currentRights)
+        return $false
+    }
+
+    if( -not $currentRights )
+    {
+        Write-Verbose ('Identity ''{0} has no permission to ''{1}''' -f $Identity,$Path,$currentRights)
+        return $false
+    }
+
+    if( $desiredRights -ne $currentRights )
+    {
+        Write-Verbose ('Identity ''{0} has stale permission to ''{1}'': {2}' -f $Identity,$Path,$currentRights)
+        return $false
+    }
+
+    if( $ApplyTo -and $ApplyTo -ne $resource.ApplyTo )
+    {
+        Write-Verbose ('Identity ''{0}'' has stale inheritance/propagation flags to ''{1}'': {2}' -f $Identity,$Path,$resource.ApplyTo)
+        return $false
+    }
+
+    return $true
+}
---
+++ Test/DscResources/Test-Permission.ps1
@@ -0,0 +1,198 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'CarbonDscTest.psm1' -Resolve) -Force
+$UserName = 'CarbonDscTestUser'
+$Password = [Guid]::NewGuid().ToString()
+$tempDir = $null
+
+function Start-TestFixture
+{
+    Start-CarbonDscTestFixture 'Permission'
+    Install-User -UserName $UserName -Password $Password
+
+}
+
+function Start-Test
+{
+    $tempDir = 'Carbon+{0}+{1}' -f ((Split-Path -Leaf -Path $PSCommandPath),([IO.Path]::GetRandomFileName()))
+    $tempDir = Join-Path -Path $env:TEMP -ChildPath $tempDir
+    New-Item -Path $tempDir -ItemType 'Directory' | Out-Null
+}
+
+function Stop-Test
+{
+    if( (Test-Path -Path $Path -PathType Container) )
+    {
+        Remove-Item -Path $Path
+    }
+}
+
+function Stop-TestFixture
+{
+    Stop-CarbonDscTestFixture
+}
+
+function Test-ShouldGrantPermissionOnFileSystem
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -Ensure Present
+    Assert-NoError
+    Assert-True (Test-Permission -Identity $UserName -Path $tempDir -Permission FullControl -ApplyTo ContainerAndSubContainersAndLeaves -Exact)
+}
+
+function Test-ShouldGrantPermissionWithInheritenceOnFileSystem
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -ApplyTo Container -Ensure Present
+    Assert-NoError
+    Assert-True (Test-Permission -Identity $UserName -Path $tempDir -Permission FullControl -ApplyTo Container -Exact)
+}
+
+function Test-ShouldGrantPermissionOnRegistry
+{
+    $keyPath = 'hkcu:\{0}' -f (Split-Path -Leaf -Path $tempDir)
+    New-Item -Path $keyPath
+    try
+    {
+        Set-TargetResource -Identity $UserName -Path $keyPath -Permission ReadKey -ApplyTo Container -Ensure Present
+        Assert-NoError
+        Assert-True (Test-Permission -Identity $UserName -Path $keyPath -Permission ReadKey -ApplyTo Container -Exact)
+    }
+    finally
+    {
+        Remove-Item -Path $keyPath
+    }
+}
+
+function Test-ShouldChangePermission
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -Ensure Present
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Container -Ensure Present
+    Assert-True (Test-Permission -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Container -Exact)
+}
+
+function Test-ShouldRevokePermission
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -Ensure Present
+    Set-TargetResource -Identity $UserName -Path $tempDir -Ensure Absent
+    Assert-False (Test-Permission -Identity $UserName -Path $tempDir -Permission FullControl -Exact)
+}
+
+function Test-ShouldRequirePermissionWhenGranting
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Ensure Present -ErrorAction SilentlyContinue
+    Assert-Error -Last -Regex 'mandatory'
+    Assert-Null (Get-Permission -Path $tempDir -Identity $UserName)
+}
+
+function Test-ShouldGetNoPermission
+{
+    $resource = Get-TargetResource -Identity $UserName -Path $tempDir
+    Assert-NotNull $resource
+    Assert-Equal $UserName $resource.Identity
+    Assert-Equal $tempDir $resource.Path
+    Assert-Empty $resource.Permission
+    Assert-Null $resource.ApplyTo
+    Assert-DscResourceAbsent $resource
+}
+
+function Test-ShouldGetCurrentPermission
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -Ensure Present
+    $resource = Get-TargetResource -Identity $UserName -Path $tempDir
+    Assert-NotNull $resource
+    Assert-Equal $UserName $resource.Identity
+    Assert-Equal $tempDir $resource.Path
+    Assert-Equal 'FullControl' $resource.Permission
+    Assert-Equal 'ContainerAndSubContainersAndLeaves' $resource.ApplyTo
+    Assert-DscResourcePresent $resource
+}
+
+function Test-ShouldGetMultiplePermissions
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission Read,Write -Ensure Present
+    $resource = Get-TargetResource -Identity $UserName -Path $tempDir
+    Assert-Is $resource.Permission 'string[]'
+    Assert-Equal 'Write,Read' ($resource.Permission -join ',')
+}
+
+
+function Test-ShouldGetCurrentContainerInheritanceFlags
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -ApplyTo SubContainers -Ensure Present
+    $resource = Get-TargetResource -Identity $UserName -Path $tempDir
+    Assert-NotNull $resource
+    Assert-Equal 'SubContainers' $resource.ApplyTo
+}
+
+function Test-ShouldTestNoPermission
+{
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -Ensure Present)
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -ApplyTo ContainerAndSubContainersAndLeaves -Ensure Present)
+    Assert-True (Test-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -Ensure Absent)
+    Assert-True (Test-TargetResource -Identity $UserName -Path $tempDir -Permission FullControl -ApplyTo ContainerAndSubContainersAndLeaves -Ensure Absent)
+}
+
+function Test-ShouldTestExistingPermission
+{
+    Set-TargetResource -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Container -Ensure Present
+    Assert-True (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Read -Ensure Present -Verbose)
+    Assert-True (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Container -Ensure Present)
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Read -Ensure Absent)
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Container -Ensure Absent)
+
+    # Now, see what happens if permissions are wrong
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Write -Ensure Present)
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Leaves -Ensure Present)
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Write -Ensure Absent)
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission Read -ApplyTo Leaves -Ensure Absent)
+}
+
+
+configuration DscConfiguration
+{
+    param(
+        $Ensure
+    )
+
+    Set-StrictMode -Off
+
+    Import-DscResource -Name '*' -Module 'Carbon'
+
+    node 'localhost'
+    {
+        Permission set
+        {
+            Identity = $UserName;
+            Path = $tempDir;
+            Permission = 'Read';
+            ApplyTo = 'Container';
+            Ensure = $Ensure;
+        }
+    }
+}
+
+function Test-ShouldRunThroughDsc
+{
+    & DscConfiguration -Ensure 'Present' -OutputPath $CarbonDscOutputRoot
+    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot
+    Assert-NoError
+    Assert-True (Test-TargetResource -Identity $UserName -Path $tempDir -Permission 'Read' -ApplyTo 'Container' -Ensure 'Present')
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission 'Read' -ApplyTo 'Container' -Ensure 'Absent')
+
+    & DscConfiguration -Ensure 'Absent' -OutputPath $CarbonDscOutputRoot 
+    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot
+    Assert-NoError
+    Assert-False (Test-TargetResource -Identity $UserName -Path $tempDir -Permission 'Ready' -ApplyTo 'Container' -Ensure 'Present')
+    Assert-True (Test-TargetResource -Identity $UserName -Path $tempDir -Permission 'Ready' -ApplyTo 'Container' -Ensure 'Absent')
+}
