0e1ca21d36612491bcc94b4ae7dce47c5c221538
Aaron Jensen <aaron@splatteredbits.com>
2014-09-07 08:59:21 -0700

    Upgrading Silk: improving link to MSDN documentation.
    --HG--
    branch : 1.0

---
+++ Tools/Silk/Get-TypeDocumentationLink.ps1
@@ -0,0 +1,64 @@
+
+function Get-TypeDocumentationLink
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        # The name of the type whose documentation link to return.
+        $TypeName
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $displayName = $TypeName
+    if( $TypeName.EndsWith('[]') )
+    {
+        $TypeName = $TypeName -replace '\[\]',''
+    }
+
+    if( $TypeName -eq 'bool' )
+    {
+        $TypeName = 'boolean'
+    }
+
+    $type = $null
+    if( $loadedTypes.ContainsKey( $TypeName ) )
+    {
+        $type = [Type]$loadedTypes[$TypeName]
+    }
+    else
+    {
+        try
+        {
+            $type = [Type]$TypeName
+        }
+        catch
+        {
+            Write-Warning ("Type {0} not found." -f $TypeName)
+            return $displayName
+        }
+    }
+
+    $typeLink = $TypeName
+    $typeFullName = $type.FullName
+
+    $msdnUri = 'http://msdn.microsoft.com/en-us/library/{0}.aspx' -f $Type.FullName.ToLower()
+    if( $Type.FullName -notlike 'System.*' )
+    {
+        $result = $null
+        try
+        {
+            $result = Invoke-WebRequest -Uri $msdnUri -Method Head -ErrorAction Ignore
+        }
+        catch
+        {
+        }
+
+        if( -not $result )
+        {
+            return $displayName
+        }
+    }
+
+    return '<a href="{0}">{1}</a>' -f $msdnUri,$displayName
+}
