b00f582338736729b92239506c42c88cc54c6be7
Aaron Jensen <ajensen@webmd.net>
2014-11-07 15:50:53 -0800

    DSC: creating Carbon_ScheduledTask resource.

---
+++ Carbon/DscResources/Carbon_ScheduledTask/Carbon_ScheduledTask.psm1
@@ -0,0 +1,287 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+& (Join-Path -Path $PSScriptRoot -ChildPath '..\Initialize-CarbonDscResource.ps1' -Resolve)
+
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([hashtable])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateLength(1,238)]
+        [Alias('TaskName')]
+        [string]
+        # The name of the scheduled task to return. Wildcards supported. This must be the *full task name*, i.e. the task's path/location and its name.
+        $Name,
+
+        [xml]
+        # Install the task from this XML.
+        $TaskXml,
+
+        [Management.Automation.PSCredential]
+        # The principal the task should run as. Use `Principal` parameter to run as a built-in security principal. Required if `Interactive` or `NoPassword` switches are used.
+        $TaskCredential,
+
+        [ValidateSet('Present','Absent')]
+        [string]
+        # If `Present`, the service is installed/updated. If `Absent`, the service is removed.
+        $Ensure = 'Present'
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $resource = @{
+                    TaskName = $Name;
+                    TaskXml = '';
+                    RunAsUser = '';
+                    Ensure = 'Absent';
+                }
+
+    if( (Test-ScheduledTask -Name $Name) )
+    {
+        $task = Get-ScheduledTask -Name $Name
+        $resource.RunAsUser = $task.RunAsUser
+        $resource.TaskXml = schtasks.exe /query /xml /tn $Name | Where-Object { $_ }
+        $resource.TaskXml = $resource.TaskXml -join ([Environment]::NewLine)
+        $resource.Ensure = 'Present'
+    }
+
+    return $resource
+}
+
+
+function Set-TargetResource
+{
+    <#
+    .SYNOPSIS
+    DSC resource for configuring scheduled tasks.
+
+    .DESCRIPTION
+    The `Carbon_ScheduledTask` resource configures scheduled tasks using `schtasks.exe` with [Task Scheduler XML](http://msdn.microsoft.com/en-us/library/windows/desktop/aa383609.aspx).
+
+    The task is installed when the `Ensure` property is set to `Present`. If the task already exists, and the XML of the current task doesn't match the XML passed in, the task is deleted, and a new task is created in its place. The XML comparison is pretty dumb: it compares the XML document(s) as a giant string, not element by element. This means if your XML doesn't order elements in the same way as `schtasks.exe /query /xml`, then your task will always be deleted and re-created. This may or may not be a problem for you.
+
+    .LINK
+    Get-ScheduledTask
+
+    .LINK
+    Install-ScheduledTask
+
+    .LINK
+    Test-ScheduledTask
+
+    .LINK
+    Uninstall-ScheduledTask
+
+    .LINK
+    http://technet.microsoft.com/en-us/library/cc725744.aspx#BKMK_create
+    
+    .LINK
+    http://msdn.microsoft.com/en-us/library/windows/desktop/aa383609.aspx
+
+    .EXAMPLE
+    >
+    Demonstrates how to install a new or update an existing scheduled task that runs as a built-in user. The user's SID should be declared in the task XML file.
+
+        Carbon_ScheduledTask InstallScheduledTask
+        {
+            Name = 'ClearApplicationCache';
+            TaskXml = (Get-Content -Path $clearApplicationCacheTaskPath.xml -Raw);
+        }
+
+    .EXAMPLE
+    >
+    Demonstrates how to install a new or update an existing scheduled task that runs as a custom user. 
+
+        Carbon_ScheduledTask InstallScheduledTask
+        {
+            Name = 'ClearApplicationCache';
+            TaskXml = (Get-Content -Path $clearApplicationCacheTaskPath.xml -Raw);
+            TaskCredential = (Get-Credential 'runasuser');
+        }
+
+    .EXAMPLE
+    >
+    Demonstrates how to remove a scheduled task.
+
+        Carbon_ScheduledTask InstallScheduledTask
+        {
+            Name = 'ClearApplicationCache';
+            Ensure = 'Absent';
+        }
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateLength(1,238)]
+        [Alias('TaskName')]
+        [string]
+        # The name of the scheduled task to return. Wildcards supported. This must be the *full task name*, i.e. the task's path/location and its name.
+        $Name,
+
+        [xml]
+        # Install the task from this XML.
+        $TaskXml,
+
+        [Management.Automation.PSCredential]
+        # The principal the task should run as. Use `Principal` parameter to run as a built-in security principal. Required if `Interactive` or `NoPassword` switches are used.
+        $TaskCredential,
+
+        [ValidateSet('Present','Absent')]
+        [string]
+        # If `Present`, the service is installed/updated. If `Absent`, the service is removed.
+        $Ensure = 'Present'
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $PSBoundParameters.Remove('Ensure')
+
+    if( $Ensure -eq 'Present' )
+    {
+        if( (Test-ScheduledTask -Name $Name ) )
+        {
+            Write-Verbose ('[{0}] Re-installing' -f $Name)
+        }
+        else
+        {
+            Write-Verbose ('[{0}] Installing' -f $Name)
+        }
+        Install-ScheduledTask @PSBoundParameters
+    }
+    else
+    {
+        Write-Verbose ('[{0}] Uninstalling' -f $Name)
+        Uninstall-ScheduledTask -Name $Name
+    }
+
+}
+
+
+function Test-TargetResource
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateLength(1,238)]
+        [Alias('TaskName')]
+        [string]
+        # The name of the scheduled task to return. Wildcards supported. This must be the *full task name*, i.e. the task's path/location and its name.
+        $Name,
+
+        [xml]
+        # Install the task from this XML.
+        $TaskXml,
+
+        [Management.Automation.PSCredential]
+        # The principal the task should run as. Use `Principal` parameter to run as a built-in security principal. Required if `Interactive` or `NoPassword` switches are used.
+        $TaskCredential,
+
+        [ValidateSet('Present','Absent')]
+        [string]
+        # If `Present`, the task is installed/updated. If `Absent`, the task is removed.
+        $Ensure = 'Present'
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $resource = Get-TargetResource -Name $Name
+
+    if( $Ensure -eq 'Present' )
+    {
+        if( $resource.Ensure -eq 'Absent' )
+        {
+            Write-Verbose ('[{0}] Desired scheduled task not found' -f $Name)
+            return $false
+        }
+
+        if( -not $TaskXml )
+        {
+            Write-Error ('Property ''TaskXml'' is missing or empty. When creating a scheduled task, the ''TaskXml'' property is required.')
+            return $true
+        }
+
+        $resourceXml = [xml]$resource.TaskXml
+        $currentTaskXml = $resourceXml.OuterXml
+        $desiredTaskXml = $TaskXml.OuterXml
+        if( $currentTaskXml -ne $desiredTaskXml )
+        {
+            $differsAt = 0
+            for( $idx = 0; $idx -lt $desiredTaskXml.Length -and $idx -lt $currentTaskXml.Length; ++$idx )
+            {
+                $charEqual = $desiredTaskXml[$idx] -eq $currentTaskXml[$idx]
+
+                if( -not $charEqual )
+                {
+                    $differsAt = $idx
+                    break
+                }
+            }
+  
+            $nameLength = $Name.Length
+            $linePrefix = ' ' * ($nameLength + 2)
+            $msgFormat = @'
+'@        
+            Write-Verbose ('[{0}] Task XML differs:' -f $Name)
+            $startIdx = $differsAt - 35
+            if( $startIdx -lt 0 )
+            {
+                $startIdx = 0
+            }
+
+            $shortestLength = $currentTaskXml.Length
+            if( $desiredTaskXml.Length -lt $shortestLength )
+            {
+                $shortestLength = $desiredTaskXml.Length
+            }
+
+            $length = 70
+            if( $startIdx + $length -ge $shortestLength )
+            {
+                $length = $shortestLength - $startIdx
+            }
+
+            Write-Verbose ('{0} Current {1}' -f $linePrefix,$currentTaskXml.Substring($startIdx,$length))
+            Write-Verbose ('{0} Desired {1}' -f $linePrefix,$desiredTaskXml.Substring($startIdx,$length))
+            if( $length -eq 70 )
+            {
+                Write-Verbose ('{0}         -----------------------------------^' -f $linePrefix)
+            }
+            return $false
+        }
+
+        if( $TaskCredential -and $resource.RunAsUser -ne $TaskCredential.UserName )
+        {
+            Write-Verbose ('[{0}] [RunAsUser] {0} != {1}' -f $Name,$resource.RunAsUser,$TaskCredential.UserName)
+            return $false
+        }
+
+        return $true
+    }
+    else
+    {
+        if( $resource.Ensure -eq 'Present' )
+        {
+            Write-Verbose ('[{0}] Found' -f $Name)
+            return $false
+        }
+
+        Write-Verbose ('[{0}] Not found' -f $Name)
+        return $true
+    }
+}
+
+Export-ModuleMember -Function *
---
+++ Test/DscResources/Test-ScheduledTask.ps1
@@ -0,0 +1,164 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'CarbonDscTest.psm1' -Resolve) -Force
+$credential = New-Credential -User 'CarbonDscTestUser' -Password ([Guid]::NewGuid().ToString())
+$tempDir = $null
+$taskForUser = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\ScheduledTasks\task.xml' -Resolve) -Raw
+$taskForUser = $taskForUser -replace '<UserId>[^<]+</UserId>',('<UserId>{0}</UserId>' -f $credential.UserName)
+$taskForSystem = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\ScheduledTasks\task_with_principal.xml' -Resolve) -Raw
+$taskName = 'CarbonDscScheduledTask'
+
+function Start-TestFixture
+{
+    Start-CarbonDscTestFixture 'ScheduledTask'
+    Install-User -UserName $credential.UserName -Password $credential.GetNetworkCredential().Password
+}
+
+function Start-Test
+{
+}
+
+function Stop-Test
+{
+    Uninstall-ScheduledTask -Name $taskName
+}
+
+function Stop-TestFixture
+{
+    Stop-CarbonDscTestFixture
+}
+
+function Test-ShouldGetExistingTasks
+{
+    Get-ScheduledTask -Name '\Apple\AppleSoftwareUpdate' | ForEach-Object {
+        $expectedXml = schtasks /query /xml /tn $_.FullName | Where-Object { $_ }
+        $expectedXml = $expectedXml -join ([Environment]::NewLine) 
+
+        $resource = Get-TargetResource -Name $_.FullName
+        Assert-NoError
+        Assert-NotNull $resource
+        Assert-Equal $_.FullName $resource.TaskName
+        Assert-Equal $_.RunAsUser $resource.RunAsUser ('task {0}' -f $_.FullName)
+        Assert-Equal $expectedXml $resource.TaskXml
+        Assert-DscResourcePresent $resource
+    }
+}
+
+function Test-ShouldGetNonExistentTask
+{
+    $name = [Guid]::NewGuid().ToString()
+    $resource = Get-TargetResource -Name $name
+    Assert-NoError
+    Assert-NotNull $resource
+    Assert-Equal $name $resource.TaskName
+    Assert-Empty $resource.TaskXml
+    Assert-Empty $resource.RunAsUser
+    Assert-DscResourceAbsent $resource
+}
+    
+function Test-ShouldInstallTaskForUser
+{
+    Set-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential
+    Assert-NoError
+    $resource = Get-TargetResource -Name $taskName
+    Assert-NotNull $resource
+    Assert-Equal $taskName $resource.TaskName
+    Assert-Equal $taskForUser $resource.TaskXml
+    Assert-Equal $credential.UserName $resource.RunAsUser
+    Assert-DscResourcePresent $resource
+}
+
+function Test-ShouldInstallTaskForSystemPrincipal
+{
+    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+    Assert-NoError
+    $resource = Get-TargetResource -Name $taskName
+    Assert-NotNull $resource
+    Assert-Equal $taskName $resource.TaskName
+    Assert-Equal $taskForSystem $resource.TaskXml
+    Assert-Equal 'System' $resource.RunAsUser
+    Assert-DscResourcePresent $resource
+}
+
+function Test-ShouldUninstallTask
+{
+    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+    $resource = Get-TargetResource -Name $taskName
+    Assert-NotNull $resource
+    Assert-DscResourcePresent $resource
+    
+    Set-TargetResource -Name $taskName -Ensure Absent
+    $resource = Get-TargetResource -Name $taskName
+    Assert-DscResourceAbsent $resource
+}
+
+function Test-ShouldTestPresent
+{
+    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForSystem)
+    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+    Assert-True (Test-TargetResource -Name $taskName -TaskXml $taskForSystem)
+    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForUser)
+    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential)
+}
+
+function Test-ShouldWriteVerboseMessageCorrectly
+{
+    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+    Assert-False (Test-TargetResource -Name $taskName -TaskXml '<Task />')
+}
+
+function Test-ShouldTestAbsent
+{
+    Assert-True (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure Absent)
+    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+    Assert-False (Test-TargetResource -Name $taskName -Ensure Absent)
+}
+
+
+configuration DscConfiguration
+{
+    param(
+        $Ensure
+    )
+
+    Set-StrictMode -Off
+
+    Import-DscResource -Name '*' -Module 'Carbon'
+
+    node 'localhost'
+    {
+        Carbon_ScheduledTask set
+        {
+            Name = $taskName;
+            TaskXml = $taskForSystem;
+            Ensure = $Ensure;
+        }
+    }
+}
+
+function Test-ShouldRunThroughDsc
+{
+    & DscConfiguration -Ensure 'Present' -OutputPath $CarbonDscOutputRoot
+    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot
+    Assert-NoError
+    Assert-True (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure 'Present')
+    Assert-False (Test-TargetResource -Name $taskName -Ensure 'Absent')
+
+    & DscConfiguration -Ensure 'Absent' -OutputPath $CarbonDscOutputRoot 
+    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot
+    Assert-NoError
+    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure 'Present')
+    Assert-True (Test-TargetResource -Name $taskName -Ensure 'Absent')
+}
