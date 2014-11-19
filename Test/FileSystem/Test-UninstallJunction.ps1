c89060afe0bb3369b6b2111a106be4965d0d9bc8
Konstantin Ushenin (kostaNew) <none@none>
2014-11-20 01:30:43 +0600

    Test for Uninstall-Junction
    --HG--
    branch : Uninstall-Junction

---
+++ Test/FileSystem/Test-UninstallJunction.ps1
@@ -0,0 +1,92 @@
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$JunctionPath = $null
+
+function Start-TestFixture
+{
+    & (Join-Path -Path $PSScriptRoot '..\Import-CarbonForTest.ps1' -Resolve)
+}
+
+function Start-Test
+{
+    $JunctionPath = Join-Path $env:Temp ([IO.Path]::GetRandomFileName())
+    New-Junction $JunctionPath $TestDir
+}
+
+function Stop-Test
+{
+    if( Test-Path $JunctionPath -PathType Container )
+    {
+        cmd /c rmdir $JunctionPath
+    }
+}
+
+function Invoke-UninstallJunction($junction)
+{
+    Uninstall-Junction $junction
+}
+
+function Test-ShouldUninstallJunction
+{
+    Invoke-UninstallJunction $JunctionPath
+    Assert-DirectoryDoesNotExist $JunctionPath 'Failed to delete junction.'
+    Assert-DirectoryExists $TestDir
+}
+
+function Test-ShouldDoNothingIfJunctionActuallyADirectory
+{
+    $realDir = Join-Path $env:Temp ([IO.Path]::GetRandomFileName())
+    New-Item $realDir -ItemType 'Directory'
+    $error.Clear()
+    Invoke-UninstallJunction $realDir 2> $null
+    Assert-DirectoryExists $realDir 'Real directory was removed.'
+    Assert-Equal 0 $error.Count "Wrote out error."
+    Remove-Item $realDir
+}
+
+function Test-ShouldDoNothingIfJunctionActuallyAFile
+{
+    $path = [IO.Path]::GetTempFileName()
+    $error.Clear()
+    Invoke-UninstallJunction $path 2> $null
+    Assert-FileExists $path 'File was deleted'
+    Assert-Equal 0 $error.Count "Wrote out error."
+    Remove-Item $path
+}
+
+function Test-ShouldSupportWhatIf
+{
+    Uninstall-Junction -Path $JunctionPath -WhatIf
+    Assert-DirectoryExists $JunctionPath
+    Assert-FileExists (Join-Path $JunctionPath Test-RemoveJunction.ps1)
+    Assert-DirectoryExists $TestDir
+}
+
+function Test-ShouldRemoveJunctionWithRelativePath
+{
+    $parentDir = Split-Path -Parent -Path $JunctionPath
+    $junctionName = Split-Path -Leaf -Path $JunctionPath
+    Push-Location $parentDir
+    try
+    {
+        Uninstall-Junction -Path ".\$junctionName"
+        Assert-DirectoryDoesNotExist $JunctionPath 'Failed to delete junction with relative path.'
+        Assert-DirectoryExists $TestDir
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
