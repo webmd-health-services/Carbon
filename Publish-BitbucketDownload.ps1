7c81a8996b7855d7f0765f6d45b154f3ffb301e2
Aaron Jensen <ajensen@webmd.net>
2015-07-02 16:26:25 -0700

    Adding missing license notice.

--- Publish-BitbucketDownload.ps1
+++ Publish-BitbucketDownload.ps1
@@ -5,7 +5,19 @@ Publishes a file to Bitbucket so it is available on a project's download page.
 .LINK
 https://bitbucket.org/Swyter/bitbucket-curl-upload-to-repo-downloads
 #>
-[CmdletBinding()]
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.[CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
     [pscredential]
