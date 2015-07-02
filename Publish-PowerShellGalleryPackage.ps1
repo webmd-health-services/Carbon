393816c90abc0486bf0fb381f02cc8786dd2da8d
Aaron Jensen <ajensen@webmd.net>
2015-07-02 12:47:00 -0700

    Creating script to publish to PowerShell Gallery.

---
+++ Publish-PowerShellGalleryPackage.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+Publishes Carbon to the PowerShell gallery.
+#>
+# Copyright 2012 Aaron Jensen
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    # The API key for the PowerShell Gallery.
+    $ApiKey
+)
+
+Set-StrictMode -Version 'Latest'
+
+Publish-Module -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Carbon') -NuGetApiKey $ApiKey -LicenseUri 'http://www.apache.org/licenses/LICENSE-2.0'
