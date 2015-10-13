743c22eaf2c7a20c0f9b321e729a980ef0051244
Aaron Jensen <ajensen@webmd.net>
2015-10-13 14:45:40 -0700

    Upgrading Silk.

--- Tools/Silk/Functions/Set-ModuleVersion.ps1
+++ Tools/Silk/Functions/Set-ModuleVersion.ps1
@@ -12,19 +12,20 @@ function Set-ModuleVersion
         # The path to the module's manifest.
         $ManifestPath,
 
-        [Parameter(Mandatory=$true)]
         [string]
         # The path to the module's manifest.
         $SolutionPath,
 
-        [Parameter(Mandatory=$true)]
         [string]
         # Path to an C# file to update with the assembly version.
         $AssemblyInfoPath,
 
-        [Parameter(Mandatory=$true)]
+        [string]
+        # Path to a release notes file.
+        $ReleaseNotesPath,
+
         [Version]
-        # The version to build.
+        # The version to build. If not provided, pulled from the module's manifest.
         $Version,
 
         [string]
@@ -38,25 +39,37 @@ function Set-ModuleVersion
 
     Set-StrictMode -Version 'Latest'
 
-    if( $Version )
+    if( -not $Version )
     {
-        if( $Version.Build -lt 0 )
+        if( -not $Version )
         {
-            Write-Error ('Version number must have a build number, i.e. it must have three parts.' -f $Version)
-            return
+            $Version = Test-ModuleManifest -Path $ManifestPath | Select-Object -ExpandProperty 'Version'
+            if( -not $Version )
+            {
+                return
+            }
         }
+    }
 
-        if( $Version.Revision -ge 0 )
-        {
-            Write-Error ('Version number must not have a revision number, i.e. it must only have three parts.' -f $Version)
-            return
-        }
+    if( $Version.Build -lt 0 )
+    {
+        Write-Error ('Version number must have a build number, i.e. it must have three parts.' -f $Version)
+        return
+    }
 
-        $moduleVersionRegex = 'ModuleVersion\s*=\s*(''|")([^''"])+(''|")' 
-        $manifest = Get-Content -Raw -Path $manifestPath
-        $manifest = $manifest -replace $moduleVersionRegex,('ModuleVersion = ''{0}''' -f $version)
-        $manifest | Set-Content -Path $manifestPath
+    if( $Version.Revision -ge 0 )
+    {
+        Write-Error ('Version number must not have a revision number, i.e. it must only have three parts.' -f $Version)
+        return
+    }
 
+    $moduleVersionRegex = 'ModuleVersion\s*=\s*(''|")([^''"])+(''|")' 
+    $manifest = Get-Content -Raw -Path $manifestPath
+    $manifest = $manifest -replace $moduleVersionRegex,('ModuleVersion = ''{0}''' -f $version)
+    $manifest | Set-Content -Path $manifestPath
+
+    if( $AssemblyInfoPath )
+    {
         $assemblyVersionRegex = 'Assembly(File|Informational)?Version\("[^"]*"\)'
         $assemblyVersion = Get-Content -Path $AssemblyInfoPath |
                                 ForEach-Object {
@@ -81,12 +94,33 @@ function Set-ModuleVersion
         $assemblyVersion | Set-Content -Path $AssemblyInfoPath
     }
 
-    $msbuildRoot = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0 -Name 'MSBuildToolsPath' | Select-Object -ExpandProperty 'MSBuildToolsPath'
-    $msbuildExe = Join-Path -Path $msbuildRoot -ChildPath 'MSBuild.exe' -Resolve
-    if( -not $msbuildExe )
+    if( $ReleaseNotesPath )
     {
-        return
+        $newVersionHeader = "# {0}" -f $Version
+        $updatedVersion = $false
+        $releaseNotes = Get-Content -Path $releaseNotesPath |
+                            ForEach-Object {
+                                if( -not $updatedVersion -and $_ -match '^#\s+' )
+                                {
+                                    $updatedVersion = $true
+                                    return $newVersionHeader
+                                }
+
+                                return $_
+                            }
+        $releaseNotes | Set-Content -Path $releaseNotesPath
     }
 
-    & $msbuildExe /target:"clean;build" $SolutionPath /v:m /nologo
+    if( $SolutionPath )
+    {
+        $msbuildRoot = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0 -Name 'MSBuildToolsPath' | Select-Object -ExpandProperty 'MSBuildToolsPath'
+        $msbuildExe = Join-Path -Path $msbuildRoot -ChildPath 'MSBuild.exe' -Resolve
+        if( -not $msbuildExe )
+        {
+            return
+        }
+
+        & $msbuildExe /target:"clean;build" $SolutionPath /v:m /nologo
+    }
+
 }
