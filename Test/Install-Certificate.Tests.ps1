cd6e1409cb05bd63b970b4fe117008f6ae76bd32
Aaron Jensen <ajensen@webmd.net>
2015-12-31 13:29:13 -0800

    Migrating Install-Certificate tests to use Pester (part 2).

--- Test/Install-Certificate.Tests.ps1
+++ Test/Install-Certificate.Tests.ps1
@@ -10,119 +10,102 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$TestCertPath = JOin-Path -Path $PSScriptRoot -ChildPath 'CarbonTestCertificate.cer' -Resolve
-$TestCert = New-Object 'Security.Cryptography.X509Certificates.X509Certificate2' $TestCertPath
-$TestCertProtectedPath = Join-Path -Path $PSScriptRoot -ChildPath 'CarbonTestCertificateWithPassword.cer' -Resolve
-$TestCertProtected = New-Object 'Security.Cryptography.X509Certificates.X509Certificate2' $TestCertProtectedPath,'password'
+Describe "Install-Certificate" {
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Import-CarbonForTest.ps1' -Resolve)
-}
+    $TestCertPath = Join-Path -Path $PSScriptRoot -ChildPath 'Certificates\CarbonTestCertificate.cer' -Resolve
+    $TestCert = New-Object 'Security.Cryptography.X509Certificates.X509Certificate2' $TestCertPath
+    $TestCertProtectedPath = Join-Path -Path $PSScriptRoot -ChildPath 'Certificates\CarbonTestCertificateWithPassword.cer' -Resolve
+    $TestCertProtected = New-Object 'Security.Cryptography.X509Certificates.X509Certificate2' $TestCertProtectedPath,'password'
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
-function Start-Test
-{
-    if( (Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My) )
+    function Assert-CertificateInstalled
     {
-        Uninstall-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
+        param(
+            $StoreLocation, 
+            $StoreName,
+            $ExpectedCertificate = $TestCert
+        )
+        $cert = Get-Certificate -Thumbprint $ExpectedCertificate.Thumbprint -StoreLocation CurrentUser -StoreName My
+        $cert | Should Not BeNullOrEmpty | Out-Null
+        $cert.Thumbprint | Should Be $ExpectedCertificate.Thumbprint | Out-Null
+        return $cert
     }
 
-    if( (Get-Certificate -Thumbprint $TestCertProtected.Thumbprint -StoreLocation CurrentUser -StoreName My) )
-    {
+    BeforeEach {
+        $Global:Error.Clear()
+
+        if( (Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My) )
+        {
+            Uninstall-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
+        }
+
+        if( (Get-Certificate -Thumbprint $TestCertProtected.Thumbprint -StoreLocation CurrentUser -StoreName My) )
+        {
+            Uninstall-Certificate -Certificate $TestCertProtected -StoreLocation CurrentUser -StoreName My
+        }
+    }
+
+    AfterEach {
+        Uninstall-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
         Uninstall-Certificate -Certificate $TestCertProtected -StoreLocation CurrentUser -StoreName My
     }
-}
 
-function Stop-Test
-{
-    Uninstall-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
-    Uninstall-Certificate -Certificate $TestCertProtected -StoreLocation CurrentUser -StoreName My
-}
-
-function Test-ShouldInstallCertificateToLocalMachine
-{
-    $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -StoreName My
-    Assert-Equal $TestCert.Thumbprint $cert.Thumbprint
-    $cert = Assert-CertificateInstalled -StoreLocation LocalMachine -StoreName My 
-    $exportFailed = $false
-    try
-    {
-        $bytes = $cert.Export( [Security.Cryptography.X509Certificates.X509ContentType]::Pfx )
-    }
-    catch
-    {
-        $exportFailed = $true
-    }
-    Assert-True $exportFailed
-}
-
-function Test-ShouldInstallCertificateToLocalMachineWithRelativePath
-{
-    Push-Location -Path $PSScriptRoot
-    try
-    {
-        $path = '.\{0}' -f (Split-Path -Leaf -Path $TestCertPath)
-        $cert = Install-Certificate -Path $path -StoreLocation CurrentUser -StoreName My
-        Assert-Equal $TestCert.Thumbprint $cert.Thumbprint
+    It 'should install certificate to local machine' {
+        $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -StoreName My
+        $cert.Thumbprint | Should Be $TestCert.Thumbprint
         $cert = Assert-CertificateInstalled -StoreLocation LocalMachine -StoreName My 
+        {
+            $bytes = $cert.Export( [Security.Cryptography.X509Certificates.X509ContentType]::Pfx )
+        } | Should Throw
     }
-    finally
-    {
-        Pop-Location
+
+    It 'should install certificate to local machine with relative path' {
+        Push-Location -Path $PSScriptRoot
+        try
+        {
+            $path = '.\Certificates\{0}' -f (Split-Path -Leaf -Path $TestCertPath)
+            $cert = Install-Certificate -Path $path -StoreLocation CurrentUser -StoreName My
+            $cert.Thumbprint | Should Be $TestCert.Thumbprint
+            $cert = Assert-CertificateInstalled -StoreLocation LocalMachine -StoreName My 
+        }
+        finally
+        {
+            Pop-Location
+        }
+    }
+
+    It 'should install certificate to local machine as exportable' {
+        $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -StoreName My -Exportable
+        $cert.Thumbprint | Should Be $TestCert.Thumbprint
+        $cert = Assert-CertificateInstalled -StoreLocation LocalMachine -StoreName My 
+        $bytes = $cert.Export( [Security.Cryptography.X509Certificates.X509ContentType]::Pfx )
+        $bytes | Should Not BeNullOrEmpty
+    }
+
+    It 'should install certificate in custom store' {
+        $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -CustomStoreName 'SharePoint' 
+        $cert | Should Not BeNullOrEmpty
+        'cert:\CurrentUser\SharePoint' | Should Exist
+        ('cert:\CurrentUser\SharePoint\{0}' -f $cert.Thumbprint) | Should Exist
+    }
+
+    It 'should install certificate idempotently' {
+        Install-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
+        $Global:Error | Should BeNullOrEmpty
+        Install-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
+        $Global:Error | Should BeNullOrEmpty
+        Assert-CertificateInstalled CurrentUser My
+    }
+
+    It 'should install certificate' {
+        $cert = Install-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
+        $cert | Should Not BeNullOrEmpty
+        Assert-CertificateInstalled CurrentUser My
+    }
+
+    It 'should install password protected certificate' {
+        $cert = Install-Certificate -Certificate $TestCertProtected -StoreLocation CurrentUser -StoreName My
+        $cert | Should Not BeNullOrEmpty
+        Assert-CertificateInstalled CurrentUser My $TestCertProtected
     }
 }
-
-function Test-ShouldInstallCertificateToLocalMachineAsExportable
-{
-    $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -StoreName My -Exportable
-    Assert-Equal $TestCert.Thumbprint $cert.Thumbprint
-    $cert = Assert-CertificateInstalled -StoreLocation LocalMachine -StoreName My 
-    $bytes = $cert.Export( [Security.Cryptography.X509Certificates.X509ContentType]::Pfx )
-    Assert-NotNull $bytes
-    Assert-NotEqual 0 $bytes.Length
-}
-
-function Test-ShouldInstallCertificateInCustomStore
-{
-    $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -CustomStoreName 'SharePoint' 
-    Assert-NotNull $cert
-    Assert-True (Test-Path -Path 'cert:\CurrentUser\SharePoint' -PathType Container)
-    Assert-True (Test-Path -Path ('cert:\CurrentUser\SharePoint\{0}' -f $cert.Thumbprint) -PathType Leaf)
-}
-
-function Test-ShouldInstallCertificateIdempotently
-{
-    Install-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
-    Assert-NoError
-    Install-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
-    Assert-NoError
-    Assert-CertificateInstalled CurrentUser My
-}
-
-function Test-ShouldInstallCertificate
-{
-    $cert = Install-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
-    Assert-NotNull $cert
-    Assert-CertificateInstalled CurrentUser My
-}
-
-function Test-ShouldInstallPasswordProtectedCertificate
-{
-    $cert = Install-Certificate -Certificate $TestCertProtected -StoreLocation CurrentUser -StoreName My
-    Assert-NotNull $cert
-    Assert-CertificateInstalled CurrentUser My $TestCertProtected
-}
-
-function Assert-CertificateInstalled
-{
-    param(
-        $StoreLocation, 
-        $StoreName,
-        $ExpectedCertificate = $TestCert
-    )
-    $cert = Get-Certificate -Thumbprint $ExpectedCertificate.Thumbprint -StoreLocation CurrentUser -StoreName My
-    Assert-NotNull $cert
-    Assert-Equal $ExpectedCertificate.Thumbprint $cert.Thumbprint
-    return $cert
-}
-
