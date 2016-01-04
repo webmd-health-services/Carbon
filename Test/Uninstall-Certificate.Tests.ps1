c82cf0a184cd81a929c4079542f926de4014de37
Aaron Jensen <ajensen@webmd.net>
2016-01-04 14:40:34 -0800

    Converting Uninstall-Certificate tests to use Pester (part 2).

--- Test/Uninstall-Certificate.Tests.ps1
+++ Test/Uninstall-Certificate.Tests.ps1
@@ -10,60 +10,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$TestCertPath = JOin-Path $TestDir CarbonTestCertificate.cer -Resolve
+$TestCertPath = Join-Path -Path $PSScriptRoot -ChildPath 'Certificates\CarbonTestCertificate.cer' -Resolve
 $TestCert = New-Object Security.Cryptography.X509Certificates.X509Certificate2 $TestCertPath
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Import-CarbonForTest.ps1' -Resolve)
-}
+Describe 'Uninstall-Certificate' {
 
-function Start-Test
-{
-    if( -not (Test-Path Cert:\CurrentUser\My\$TestCert.Thumbprint -PathType Leaf) )
-    {
-        Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -StoreName My
-    }
-}
-
-function Test-ShouldRemoveCertificateByCertificate
-{
-    Uninstall-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
-    $cert = Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
-    Assert-Null $cert
-}
-
-function Test-ShouldRemoveCertificateByThumbprint
-{
-    Uninstall-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
-    $maxTries = 10
-    $tryNum = 0
-    do
-    {
-        $cert = Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
-        if( -not $cert )
+    BeforeEach {
+        if( -not (Test-Path Cert:\CurrentUser\My\$TestCert.Thumbprint -PathType Leaf) )
         {
-            break
+            Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -StoreName My
         }
-        Start-Sleep -Milliseconds 100
     }
-    while( $tryNum++ -lt $maxTries )
-    Assert-Null $cert
-}
 
-function Test-ShouldSupportWhatIf
-{
-    Uninstall-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My -WhatIf
-    $cert = Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
-    Assert-NotNull $cert
-}
+    It 'should remove certificate by certificate' {
+        Uninstall-Certificate -Certificate $TestCert -StoreLocation CurrentUser -StoreName My
+        $cert = Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
+        $cert | Should BeNullOrEmpty
+    }
 
-function Test-ShouldUninstallCertificateFromCustomStore
-{
-    $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -CustomStoreName 'Carbon'
-    Assert-NotNull $cert
-    $certPath = 'Cert:\CurrentUser\Carbon\{0}' -f $cert.Thumbprint
-    Assert-True (Test-Path -Path $certPath -PathType Leaf)
-    Uninstall-Certificate -Thumbprint $cert.Thumbprint -StoreLocation CurrentUser -CustomStoreName 'Carbon'
-}
+    It 'should remove certificate by thumbprint' {
+        Uninstall-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
+        $maxTries = 10
+        $tryNum = 0
+        do
+        {
+            $cert = Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
+            if( -not $cert )
+            {
+                break
+            }
+            Start-Sleep -Milliseconds 100
+        }
+        while( $tryNum++ -lt $maxTries )
+        $cert | Should BeNullOrEmpty
+    }
 
+    It 'should support WhatIf' {
+        Uninstall-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My -WhatIf
+        $cert = Get-Certificate -Thumbprint $TestCert.Thumbprint -StoreLocation CurrentUser -StoreName My
+        $cert | Should Not BeNullOrEmpty
+    }
+
+    It 'should uninstall certificate from custom store' {
+        $cert = Install-Certificate -Path $TestCertPath -StoreLocation CurrentUser -CustomStoreName 'Carbon'
+        $cert | Should Not BeNullOrEmpty
+        $certPath = 'Cert:\CurrentUser\Carbon\{0}' -f $cert.Thumbprint
+        $certPath | Should Exist
+        Uninstall-Certificate -Thumbprint $cert.Thumbprint -StoreLocation CurrentUser -CustomStoreName 'Carbon'
+        $certPath | Should Not Exist   
+    }
+}
