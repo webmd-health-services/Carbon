c499bc7b6b71f8869022c40dd9e246cf642aae1b
Aaron Jensen <ajensen@webmd.net>
2016-02-08 17:28:51 -0800

    Moving website tests to Pester (part 2).

--- Test/Website.Tests.ps1
+++ Test/Website.Tests.ps1
@@ -12,24 +12,25 @@
 
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
-function Test-AllFunctionShouldBeTagged
-{
-    $tags = Get-Content -Raw -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\tags.json' -Resolve) | ConvertFrom-Json
-    $taggedCommands = @{ }
-    $tags | ForEach-Object { $taggedCommands[$_.Name] = $_.Name }
+Describe "Carbon Website" {
+    It 'all function should be tagged' {
+        $tags = Get-Content -Raw -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\tags.json' -Resolve) | ConvertFrom-Json
+        $taggedCommands = @{ }
+        $tags | ForEach-Object { $taggedCommands[$_.Name] = $_.Name }
 
-    $missingCommandNames = Get-Command -Module 'Carbon' | 
-                                Where-Object { $_.CommandType -ne [Management.Automation.CommandTypes]::Alias } |
-                                Select-Object -ExpandProperty 'Name' | 
-                                Where-Object { -not $taggedCommands.ContainsKey($_) }
+        $missingCommandNames = Get-Command -Module 'Carbon' | 
+                                    Where-Object { $_.CommandType -ne [Management.Automation.CommandTypes]::Alias } |
+                                    Select-Object -ExpandProperty 'Name' | 
+                                    Where-Object { -not $taggedCommands.ContainsKey($_) }
 
-    if( $missingCommandNames )
-    {
-        Fail @"
+        if( $missingCommandNames )
+        {
+        @"
 The following commands are missing from tags.json:
 
  * $($missingCommandNames -join ('{0} * ' -f [Environment]::NewLine))
 
-"@
+"@ | Should BeNullOrEmpty
+        }
     }
 }
