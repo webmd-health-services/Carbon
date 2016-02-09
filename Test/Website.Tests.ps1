6e23802b7667d37b6f01a2bf08910db142d2e143
Aaron Jensen <ajensen@webmd.net>
2016-02-08 17:34:14 -0800

    Fixed: Carbon_Group missing from tags.

--- Test/Website.Tests.ps1
+++ Test/Website.Tests.ps1
@@ -13,10 +13,12 @@
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
 Describe "Carbon Website" {
-    It 'all function should be tagged' {
-        $tags = Get-Content -Raw -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\tags.json' -Resolve) | ConvertFrom-Json
-        $taggedCommands = @{ }
-        $tags | ForEach-Object { $taggedCommands[$_.Name] = $_.Name }
+
+    $tags = Get-Content -Raw -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\tags.json' -Resolve) | ConvertFrom-Json
+    $taggedCommands = @{ }
+    $tags | ForEach-Object { $taggedCommands[$_.Name] = $_.Name }
+
+    It 'should have tags for all functions' {
 
         $missingCommandNames = Get-Command -Module 'Carbon' | 
                                     Where-Object { $_.CommandType -ne [Management.Automation.CommandTypes]::Alias } |
@@ -32,5 +34,13 @@ The following commands are missing from tags.json:
 
 "@ | Should BeNullOrEmpty
         }
+    }
+
+    It 'should have tags for all DSC resources' {
+        $missingDscResources = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon\DscResources' -Resolve) -Directory |
+                            Select-Object -ExpandProperty 'Name' |
+                            Where-Object { -not $taggedCommands.ContainsKey($_) }
+
+        ,$missingDscResources | Should BeNullOrEmpty
     }
 }
