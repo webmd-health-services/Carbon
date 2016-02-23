c4279db0fca41397e2162576961e2d83490e501b
Aaron Jensen <ajensen@webmd.net>
2016-02-22 17:39:06 -0800

    Incrementing version to 2.1.1.

--- Source/Properties/AssemblyVersion.cs
+++ Source/Properties/AssemblyVersion.cs
@@ -21,8 +21,8 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyCopyright("Copyright 2011 - 2015 Aaron Jensen.")]
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+// [assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyCopyright("Copyright 2011 - 2016 Aaron Jensen.")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyInformationalVersion("2.1.1")]
