eb02383484e39684fe5283223cc40b9292577be3
Aaron Jensen <ajensen@webmd.net>
2016-05-05 14:34:11 -0700

    Start-Bootstrap.ps1 doesn't do any error handling.

--- Start-Bootstrap.ps1
+++ Start-Bootstrap.ps1
@@ -10,10 +10,29 @@ Invoke-Expression -Command "iex ((new-object net.webclient).DownloadString('http
 
 choco install tortoisehg -y
 
-mkdir C:\Projects
-$carbonDir = 'C:\Projects\Carbon'
+$projectsDir = 'C:\Projects'
+if( -not (Test-Path -Path $projectsDir) )
+{
+    mkdir C:\Projects
+}
+
+$carbonDir = Join-Path -Path $projectsDir -ChildPath 'Carbon'
 
 Set-Alias -Name 'hg' -Value 'C:\Program Files\TortoiseHg\hg.exe'
-hg clone 'https://bitbucket.org/splatteredbits/carbon' $carbonDir
+if( -not (Test-Path -Path $carbonDir -PathType Container) )
+{
+    hg clone 'https://bitbucket.org/splatteredbits/carbon' $carbonDir
+}
 
-& 'C:\Projects\Carbon\Initialize-Server.ps1'
+Push-Location -Path $carbonDir
+try
+{
+    hg pull
+    hg update
+
+    & 'C:\Projects\Carbon\Initialize-Server.ps1'
+}
+finally
+{
+    Pop-Location
+}
