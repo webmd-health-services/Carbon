17a046c54c3cb16ec9454c8dda8b6e39d9cd1b15
Aaron Jensen <ajensen@webmd.net>
2016-05-09 12:05:22 -0700

    Moving Carbon_ScheduledTask DSC resource tests to Pester (part 2).

--- Test/Carbon_ScheduledTask.Tests.ps1
+++ Test/Carbon_ScheduledTask.Tests.ps1
@@ -10,163 +10,154 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'CarbonDscTest.psm1' -Resolve) -Force
-$credential = New-Credential -User 'CarbonDscTestUser' -Password ([Guid]::NewGuid().ToString())
-$tempDir = $null
-$taskForUser = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\ScheduledTasks\task.xml' -Resolve) -Raw
-$taskForUser = $taskForUser -replace '<UserId>[^<]+</UserId>',('<UserId>{0}</UserId>' -f $credential.UserName)
-$taskForSystem = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '..\ScheduledTasks\task_with_principal.xml' -Resolve) -Raw
-$taskName = 'CarbonDscScheduledTask'
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'DscResources\CarbonDscTest.psm1' -Resolve) -Force
 
-function Start-TestFixture
-{
+Describe 'Carbon_ScheduledTask' {
+
+    $credential = New-Credential -User 'CarbonDscTestUser' -Password ([Guid]::NewGuid().ToString())
+    $tempDir = $null
+    $taskForUser = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'ScheduledTasks\task.xml' -Resolve) -Raw
+    $taskForUser = $taskForUser -replace '<UserId>[^<]+</UserId>',('<UserId>{0}</UserId>' -f $credential.UserName)
+    $taskForSystem = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'ScheduledTasks\task_with_principal.xml' -Resolve) -Raw
+    $taskName = 'CarbonDscScheduledTask'
+
     Start-CarbonDscTestFixture 'ScheduledTask'
     Install-User -UserName $credential.UserName -Password $credential.GetNetworkCredential().Password
-}
 
-function Start-Test
-{
-}
-
-function Stop-Test
-{
-    Uninstall-ScheduledTask -Name $taskName
-}
-
-function Stop-TestFixture
-{
-    Stop-CarbonDscTestFixture
-}
-
-function Test-ShouldGetExistingTasks
-{
-    Get-ScheduledTask | ForEach-Object {
-        $expectedXml = schtasks /query /xml /tn $_.FullName | Where-Object { $_ }
-        $expectedXml = $expectedXml -join ([Environment]::NewLine) 
-
-        $resource = Get-TargetResource -Name $_.FullName
-        Assert-NoError
-        Assert-NotNull $resource
-        Assert-Equal $_.FullName $resource.TaskName
-        Assert-Equal $_.RunAsUser $resource.RunAsUser ('task {0}' -f $_.FullName)
-        Assert-Equal $expectedXml $resource.TaskXml
-        Assert-DscResourcePresent $resource
-    }
-}
-
-function Test-ShouldGetNonExistentTask
-{
-    $name = [Guid]::NewGuid().ToString()
-    $resource = Get-TargetResource -Name $name
-    Assert-NoError
-    Assert-NotNull $resource
-    Assert-Equal $name $resource.TaskName
-    Assert-Empty $resource.TaskXml
-    Assert-Empty $resource.RunAsUser
-    Assert-DscResourceAbsent $resource
-}
-    
-function Test-ShouldInstallTaskForUser
-{
-    Set-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential
-    Assert-NoError
-    $resource = Get-TargetResource -Name $taskName
-    Assert-NotNull $resource
-    Assert-Equal $taskName $resource.TaskName
-    Assert-Equal $taskForUser $resource.TaskXml
-    Assert-Equal $credential.UserName $resource.RunAsUser
-    Assert-DscResourcePresent $resource
-}
-
-function Test-ShouldInstallTaskForSystemPrincipal
-{
-    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
-    Assert-NoError
-    $resource = Get-TargetResource -Name $taskName
-    Assert-NotNull $resource
-    Assert-Equal $taskName $resource.TaskName
-    Assert-Equal $taskForSystem $resource.TaskXml
-    Assert-Equal 'System' $resource.RunAsUser
-    Assert-DscResourcePresent $resource
-}
-
-function Test-ShouldReinstallTask
-{
-    Set-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential
-    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
-    $resource = Get-TargetResource -Name $taskName
-    Assert-DscResourcePresent $resource
-    Assert-Equal 'System' $resource.RunAsUser
-}
-
-function Test-ShouldUninstallTask
-{
-    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
-    $resource = Get-TargetResource -Name $taskName
-    Assert-NotNull $resource
-    Assert-DscResourcePresent $resource
-    
-    Set-TargetResource -Name $taskName -Ensure Absent
-    $resource = Get-TargetResource -Name $taskName
-    Assert-DscResourceAbsent $resource
-}
-
-function Test-ShouldTestPresent
-{
-    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForSystem)
-    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
-    Assert-True (Test-TargetResource -Name $taskName -TaskXml $taskForSystem)
-    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForUser)
-    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential)
-}
-
-function Test-ShouldWriteVerboseMessageCorrectly
-{
-    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
-    Assert-False (Test-TargetResource -Name $taskName -TaskXml '<Task />')
-}
-
-function Test-ShouldTestAbsent
-{
-    Assert-True (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure Absent)
-    Set-TargetResource -Name $taskName -TaskXml $taskForSystem
-    Assert-False (Test-TargetResource -Name $taskName -Ensure Absent)
-}
-
-
-configuration DscConfiguration
-{
-    param(
-        $Ensure
-    )
-
-    Set-StrictMode -Off
-
-    Import-DscResource -Name '*' -Module 'Carbon'
-
-    node 'localhost'
-    {
-        Carbon_ScheduledTask set
-        {
-            Name = $taskName;
-            TaskXml = $taskForSystem;
-            Ensure = $Ensure;
+    try
+    {    
+        BeforeEach {
         }
+    
+        AfterEach {
+            Uninstall-ScheduledTask -Name $taskName
+        }
+    
+        It 'should get existing tasks' {
+            Get-ScheduledTask | ForEach-Object {
+                $expectedXml = schtasks /query /xml /tn $_.FullName | Where-Object { $_ }
+                $expectedXml = $expectedXml -join ([Environment]::NewLine) 
+    
+                $resource = Get-TargetResource -Name $_.FullName
+                $Global:Error.Count | Should Be 0
+                $resource | Should Not BeNullOrEmpty
+                $resource.TaskName | Should Be $_.FullName
+                $resource.RunAsUser | Should Be $_.RunAsUser
+                $resource.TaskXml | Should Be $expectedXml
+                Assert-DscResourcePresent $resource
+            }
+        }
+    
+        It 'should get non existent task' {
+            $name = [Guid]::NewGuid().ToString()
+            $resource = Get-TargetResource -Name $name
+            $Global:Error.Count | Should Be 0
+            $resource | Should Not BeNullOrEmpty
+            $resource.TaskName | Should Be $name
+            $resource.TaskXml | Should BeNullOrEmpty
+            $resource.RunAsUser | Should BeNullOrEmpty
+            Assert-DscResourceAbsent $resource
+        }
+        
+        It 'should install task for user' {
+            Set-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential
+            $Global:Error.Count | Should Be 0
+            $resource = Get-TargetResource -Name $taskName
+            $resource | Should Not BeNullOrEmpty
+            $resource.TaskName | Should Be $taskName
+            $resource.TaskXml | Should Be $taskForUser
+            $resource.RunAsUser | Should Be $credential.UserName
+            Assert-DscResourcePresent $resource
+        }
+    
+        It 'should install task for system principal' {
+            Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+            $Global:Error.Count | Should Be 0
+            $resource = Get-TargetResource -Name $taskName
+            $resource | Should Not BeNullOrEmpty
+            $resource.TaskName | Should Be $taskName
+            $resource.TaskXml | Should Be $taskForSystem
+            $resource.RunAsUser | Should Be 'System'
+            Assert-DscResourcePresent $resource
+        }
+    
+        It 'should reinstall task' {
+            Set-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential
+            Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+            $resource = Get-TargetResource -Name $taskName
+            Assert-DscResourcePresent $resource
+            $resource.RunAsUser | Should Be 'System'
+        }
+    
+        It 'should uninstall task' {
+            Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+            $resource = Get-TargetResource -Name $taskName
+            $resource | Should Not BeNullOrEmpty
+            Assert-DscResourcePresent $resource
+        
+            Set-TargetResource -Name $taskName -Ensure Absent
+            $resource = Get-TargetResource -Name $taskName
+            Assert-DscResourceAbsent $resource
+        }
+    
+        It 'should test present' {
+            (Test-TargetResource -Name $taskName -TaskXml $taskForSystem) | Should Be $false
+            Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+            (Test-TargetResource -Name $taskName -TaskXml $taskForSystem) | Should Be $true
+            (Test-TargetResource -Name $taskName -TaskXml $taskForUser) | Should Be $false
+            (Test-TargetResource -Name $taskName -TaskXml $taskForUser -TaskCredential $credential) | Should Be $false
+        }
+    
+        It 'should write verbose message correctly' {
+            Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+            (Test-TargetResource -Name $taskName -TaskXml '<Task />') | Should Be $false
+        }
+    
+        It 'should test absent' {
+            (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure Absent) | Should Be $true
+            Set-TargetResource -Name $taskName -TaskXml $taskForSystem
+            (Test-TargetResource -Name $taskName -Ensure Absent) | Should Be $false
+        }
+    
+    
+        configuration DscConfiguration
+        {
+            param(
+                $Ensure
+            )
+    
+            Set-StrictMode -Off
+    
+            Import-DscResource -Name '*' -Module 'Carbon'
+    
+            node 'localhost'
+            {
+                Carbon_ScheduledTask set
+                {
+                    Name = $taskName;
+                    TaskXml = $taskForSystem;
+                    Ensure = $Ensure;
+                }
+            }
+        }
+    
+        It 'should run through dsc' {
+            & DscConfiguration -Ensure 'Present' -OutputPath $CarbonDscOutputRoot
+            Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot -Force
+            $Global:Error.Count | Should Be 0
+            (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure 'Present') | Should Be $true
+            (Test-TargetResource -Name $taskName -Ensure 'Absent') | Should Be $false
+    
+            & DscConfiguration -Ensure 'Absent' -OutputPath $CarbonDscOutputRoot 
+            Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot -Force
+            $Global:Error.Count | Should Be 0
+            (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure 'Present') | Should Be $false
+            (Test-TargetResource -Name $taskName -Ensure 'Absent') | Should Be $true
+        }
+    
+    }
+    finally
+    {
+        Stop-CarbonDscTestFixture
     }
 }
-
-function Test-ShouldRunThroughDsc
-{
-    & DscConfiguration -Ensure 'Present' -OutputPath $CarbonDscOutputRoot
-    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot -Force
-    Assert-NoError
-    Assert-True (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure 'Present')
-    Assert-False (Test-TargetResource -Name $taskName -Ensure 'Absent')
-
-    & DscConfiguration -Ensure 'Absent' -OutputPath $CarbonDscOutputRoot 
-    Start-DscConfiguration -Wait -ComputerName 'localhost' -Path $CarbonDscOutputRoot -Force
-    Assert-NoError
-    Assert-False (Test-TargetResource -Name $taskName -TaskXml $taskForSystem -Ensure 'Present')
-    Assert-True (Test-TargetResource -Name $taskName -Ensure 'Absent')
-}
-
