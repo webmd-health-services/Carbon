d707a55035e28d52b69af7ab6371b78569465ab6
Aaron Jensen <ajensen@webmd.net>
2016-09-15 16:08:18 -0700

    Migrating Install-IisAppPool tests to Pester (part 2).

--- Test/Install-IisAppPool.Tests.ps1
+++ Test/Install-IisAppPool.Tests.ps1
@@ -14,293 +14,277 @@ $appPoolName = 'CarbonInstallIisAppPool'
 $username = 'CarbonInstallIisAppP'
 $password = '!QAZ2wsx'
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot '..\Import-CarbonForTest.ps1' -Resolve)
-}
-
-function Start-Test
-{
-    Remove-AppPool
-    Install-User -Credential (New-Credential -Username $username -Password $password) -Description 'User for testing Carbon''s Install-IisAppPool function.'
-    Revoke-Privilege -Identity $username -Privilege SeBatchLogonRight
-}
-
-function Stop-Test
-{
-    Remove-AppPool
-    Uninstall-User -Username $username
-}
-
-function Remove-AppPool
-{
-    Uninstall-IisAppPool -Name $appPoolName
-}
-
-function Get-IISDefaultAppPoolIdentity
-{
-    $iisVersion = Get-IISVersion
-    if( $iisVersion -eq '7.0' )
-    {
-        return 'NetworkService'
-    }
-    return 'ApplicationPoolIdentity'
-}
-
-function Test-ShouldCreateNewAppPool
-{
-    $result = Install-IisAppPool -Name $appPoolName -PassThru
-    Assert-NotNull $result
-    Assert-AppPool $result
-}
-
-function Test-ShouldCreateNewAppPoolButNotREturnObject
-{
-    $result = Install-IisAppPool -Name $appPoolName
-    Assert-Null $result
-    $appPool = Get-IisAppPool -Name $appPoolName
-    Assert-NotNull $appPool
-    Assert-AppPool $appPool
-    
-}
-
-function Test-ShouldSetManagedRuntimeVersion
-{
-    $result = Install-IisAppPool -Name $appPoolName -ManagedRuntimeVersion 'v2.0'
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-ManagedRuntimeVersion 'v2.0'
-}
-
-function Test-ShouldSetManagedPipelineMode
-{
-    $result = Install-IisAppPool -Name $appPoolName -ClassicPipelineMode
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-ManagedPipelineMode 'Classic'
-}
-
-function Test-ShouldSetIdentityAsServiceAccount
-{
-    $result = Install-IisAppPool -Name $appPoolName -ServiceAccount 'NetworkService'
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-IdentityType 'NetworkService'
-}
-
-function Test-ShouldSetIdentityAsSpecificUser
-{
-    $warnings = @()
-    $result = Install-IisAppPool -Name $appPoolName -UserName $username -Password $password -WarningVariable 'warnings'
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-Identity $username $password
-    Assert-IdentityType 'SpecificUser'
-    Assert-Contains (Get-Privilege $username) 'SeBatchLogonRight' 'custom user not granted SeBatchLogonRight'
-    Assert-Equal 1 $warnings.Count
-    Assert-Like $warnings[0] '*obsolete*'
-}
-
-function Test-ShouldSetIdentityWithCredential
-{
-    $credential = New-Credential -UserName $username -Password $password
-    Assert-NotNull $credential
-    $result = Install-IisAppPool -Name $appPoolName -Credential $credential
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-Identity $credential.UserName $credential.GetNetworkCredential().Password
-    Assert-IdentityType 'Specificuser'
-    Assert-Contains (Get-Privilege $username) 'SeBatchLogonRight' 'custom user not granted SeBatchLogonRight'
-}
-
-function Test-ShouldSetIdleTimeout
-{
-    $result = Install-IisAppPool -Name $appPoolName -IdleTimeout 55
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-Idletimeout 55
-}
-
-function Test-ShouldEnable32bitApps
-{
-    $result = Install-IisAppPool -Name $appPoolName -Enable32BitApps
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-AppPool32BitEnabled $true
-}
-
-function Test-ShouldHandleAppPoolThatExists
-{
-    $result = Install-IisAppPool -Name $appPoolName
-    Assert-Null $result
-    $result = Install-IisAppPool -Name $appPoolName
-    Assert-Null $result
-}
+& (Join-Path -Path $PSScriptRoot 'Import-CarbonForTest.ps1' -Resolve)
 
 function Assert-AppPoolExists
 {
     $exists = Test-IisAppPool -Name $appPoolname
-    Assert-True $exists "App pool '$appPoolName' not created."
+    $exists | Should Be $true
 }
-
-function Test-ShouldChangeSettingsOnExistingAppPool
-{
-    $result = Install-IisAppPool -Name $appPoolName
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-ManagedRuntimeVersion 'v4.0'
-    Assert-ManagedPipelineMode 'Integrated'
-    Assert-IdentityType (Get-IISDefaultAppPoolIdentity)
-
-    Assert-AppPool32BitEnabled $false
-
-    $result = Install-IisAppPool -Name $appPoolName -ManagedRuntimeVersion 'v2.0' -ClassicPipeline -ServiceAccount 'LocalSystem' -Enable32BitApps
-    Assert-Null $result
-    Assert-AppPoolExists
-    Assert-ManagedRuntimeVersion 'v2.0'
-    Assert-ManagedPipelineMode 'Classic'
-    Assert-IdentityType 'LocalSystem'
-    Assert-AppPool32BitEnabled $true
-
-}
-
-function Test-ShouldAcceptSecureStringForAppPoolPassword
-{
-    $securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-    Install-IisAppPool -Name $appPoolName -Username $username -Password $securePassword
-    Assert-Identity $username $password
-}
-
-function Test-ShouldConvert32BitAppPoolto64Bit
-{
-    Install-IisAppPool -Name $appPoolName -ServiceAccount NetworkService -Enable32BitApps
-    Assert-AppPool32BitEnabled $true
-    Install-IisAppPool -Name $appPoolName -ServiceAccount NetworkService
-    Assert-AppPool32BitEnabled $false    
-}
-
-function Test-ShouldSwitchToAppPoolIdentityIfServiceAccountNotGiven
-{
-    Install-IisAppPool -Name $appPoolName -ServiceAccount NetworkService
-    Assert-IdentityType 'NetworkService'
-    Install-IisAppPool -Name $appPoolName
-    Assert-IdentityType (Get-IISDefaultAppPoolIdentity)
-}
-
-function Test-ShouldStartStoppedAppPool
-{
-    Install-IisAppPool -Name $appPoolName 
-    $appPool = Get-IisAppPool -Name $appPoolName
-    Assert-NotNull $appPool
-    if( $appPool.state -ne [Microsoft.Web.Administration.ObjectState]::Stopped )
-    { 
-        Start-Sleep -Seconds 1
-        $appPool.Stop()
-    }
     
-    Install-IisAppPool -Name $appPoolName
-    $appPool = Get-IisAppPool -Name $appPoolName
-    Assert-Equal ([Microsoft.Web.Administration.ObjectState]::Started) $appPool.state
-}
-
-function Test-ShouldFailIfIdentityDoesNotExist
-{
-    $error.Clear()
-    Install-IisAppPool -Name $appPoolName -Username 'IDoNotExist' -Password 'blahblah' -ErrorAction SilentlyContinue
-    Assert-True (Test-IisAppPool -Name $appPoolName)
-    Assert-True ($error.Count -ge 2)
-}
-
 function Assert-ManagedRuntimeVersion($Version)
 {
     $apppool = Get-IisAppPool -Name $appPoolName
-    Assert-Equal $Version $apppool.ManagedRuntimeVersion "App pool's managed runtime not at correct version."
+    $apppool.ManagedRuntimeVersion | Should Be $Version
 }
-
+    
 function Assert-ManagedPipelineMode($expectedMode)
 {
     $apppool = Get-IisAppPool -Name $appPoolName
-    Assert-Equal $expectedMode $apppool.ManagedPipelineMode "App pool's managed pipeline not in $expectedMode mode."
+    $apppool.ManagedPipelineMode | Should Be $expectedMode
 }
-
+    
 function Assert-IdentityType($expectedIdentityType)
 {
     $appPool = Get-IisAppPool -Name $appPoolName
-    Assert-Equal $expectedIdentityType $appPool.ProcessModel.IdentityType 'App pool identity type not set correctly'
+    $appPool.ProcessModel.IdentityType | Should Be $expectedIdentityType
 }
-
+    
 function Assert-IdleTimeout($expectedIdleTimeout)
 {
     $appPool = Get-IisAppPool -Name $appPoolName
     $expectedIdleTimeoutTimespan = New-TimeSpan -minutes $expectedIdleTimeout
-    Assert-Equal $expectedIdleTimeoutTimespan $appPool.ProcessModel.IdleTimeout 'App pool idle timeout not set correctly'
+    $appPool.ProcessModel.IdleTimeout | Should Be $expectedIdleTimeoutTimespan
 }
-
+    
 function Assert-Identity($expectedUsername, $expectedPassword)
 {
     $appPool = Get-IisAppPool -Name $appPoolName
-    Assert-Equal $expectedUsername $appPool.ProcessModel.UserName 'App pool username not set correctly'
-    Assert-Equal $expectedPassword $appPool.ProcessModel.Password 'App pool password not set correctly'
+    $appPool.ProcessModel.UserName | Should Be $expectedUsername
+    $appPool.ProcessModel.Password | Should Be $expectedPassword
 }
-
+    
 function Assert-AppPool32BitEnabled([bool]$expected32BitEnabled)
 {
     $appPool = Get-IisAppPool -Name $appPoolName
-    Assert-Equal $expected32BitEnabled $appPool.Enable32BitAppOnWin64 '32-bit apps enabled flag.'
+    $appPool.Enable32BitAppOnWin64 | Should Be $expected32BitEnabled
 }
-
+    
 function Assert-AppPool
 {
     param(
         [Parameter(Position=0)]
         $AppPool,
-
+    
         $ManangedRuntimeVersion = 'v4.0',
-
+    
         [Switch]
         $ClassicPipelineMode,
-
+    
         $IdentityType = (Get-IISDefaultAppPoolIdentity),
-
+    
         [Switch]
         $Enable32Bit,
-
+    
         [TimeSpan]
         $IdleTimeout = (New-TimeSpan -Seconds 0)
     )
-
+    
     Set-StrictMode -Version 'Latest'
-
+    
     Assert-AppPoolExists
-
+    
     if( -not $AppPool )
     {
         $AppPool = Get-IisAppPool -Name $appPoolName
     }
-
-    Assert-Equal $ManangedRuntimeVersion $AppPool.ManagedRuntimeVersion
+    
+    $AppPool.ManagedRuntimeVersion | Should Be $ManangedRuntimeVersion
     $pipelineMode = 'Integrated'
     if( $ClassicPipelineMode )
     {
         $pipelineMode = 'Classic'
     }
-    Assert-Equal $pipelineMode $AppPool.ManagedPipelineMode
-    Assert-Equal $IdentityType $AppPool.ProcessModel.IdentityType
-    Assert-Equal ([bool]$Enable32Bit) $AppPool.Enable32BitAppOnWin64
-    Assert-Equal $IdleTimeout $AppPool.ProcessModel.IdleTimeout
-
+    $AppPool.ManagedPipelineMode | Should Be $pipelineMode
+    $AppPool.ProcessModel.IdentityType | Should Be $IdentityType
+    $AppPool.Enable32BitAppOnWin64 | Should Be ([bool]$Enable32Bit)
+    $AppPool.ProcessModel.IdleTimeout | Should Be $IdleTimeout
+    
     $MAX_TRIES = 20
     for ( $idx = 0; $idx -lt $MAX_TRIES; ++$idx )
     {
         $AppPool = Get-IisAppPool -Name $appPoolName
-        Assert-NotNull $AppPool
+        $AppPool | Should Not BeNullOrEmpty
         if( $AppPool.State )
         {
-            Assert-Equal ([Microsoft.Web.Administration.ObjectState]::Started) $AppPool.State
+            $AppPool.State | Should Be ([Microsoft.Web.Administration.ObjectState]::Started)
             break
         }
         Start-Sleep -Milliseconds 1000
     }
 }
-
-
+    
+Describe 'Install-IisAppPool' {
+    BeforeAll {
+    }
+    
+    BeforeEach {
+        Remove-AppPool
+        Install-User -Credential (New-Credential -Username $username -Password $password) -Description 'User for testing Carbon''s Install-IisAppPool function.'
+        Revoke-Privilege -Identity $username -Privilege SeBatchLogonRight
+    }
+    
+    AfterEach {
+        Remove-AppPool
+        Uninstall-User -Username $username
+    }
+    
+    function Remove-AppPool
+    {
+        Uninstall-IisAppPool -Name $appPoolName
+    }
+    
+    function Get-IISDefaultAppPoolIdentity
+    {
+        $iisVersion = Get-IISVersion
+        if( $iisVersion -eq '7.0' )
+        {
+            return 'NetworkService'
+        }
+        return 'ApplicationPoolIdentity'
+    }
+    
+    It 'should create new app pool' {
+        $result = Install-IisAppPool -Name $appPoolName -PassThru
+        $result | Should Not BeNullOrEmpty
+        Assert-AppPool $result
+    }
+    
+    It 'should create new app pool but not r eturn object' {
+        $result = Install-IisAppPool -Name $appPoolName
+        $result | Should BeNullOrEmpty
+        $appPool = Get-IisAppPool -Name $appPoolName
+        $appPool | Should Not BeNullOrEmpty
+        Assert-AppPool $appPool
+        
+    }
+    
+    It 'should set managed runtime version' {
+        $result = Install-IisAppPool -Name $appPoolName -ManagedRuntimeVersion 'v2.0'
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-ManagedRuntimeVersion 'v2.0'
+    }
+    
+    It 'should set managed pipeline mode' {
+        $result = Install-IisAppPool -Name $appPoolName -ClassicPipelineMode
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-ManagedPipelineMode 'Classic'
+    }
+    
+    It 'should set identity as service account' {
+        $result = Install-IisAppPool -Name $appPoolName -ServiceAccount 'NetworkService'
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-IdentityType 'NetworkService'
+    }
+    
+    It 'should set identity as specific user' {
+        $warnings = @()
+        $result = Install-IisAppPool -Name $appPoolName -UserName $username -Password $password -WarningVariable 'warnings'
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-Identity $username $password
+        Assert-IdentityType 'SpecificUser'
+        Get-Privilege $username | Where-Object { $_ -eq 'SeBatchLogonRight' } | Should Not BeNullOrEmpty
+        $warnings.Count | Should Be 1
+        ($warnings[0] -like '*obsolete*') | Should Be $true
+    }
+    
+    It 'should set identity with credential' {
+        $credential = New-Credential -UserName $username -Password $password
+        $credential | Should Not BeNullOrEmpty
+        $result = Install-IisAppPool -Name $appPoolName -Credential $credential
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-Identity $credential.UserName $credential.GetNetworkCredential().Password
+        Assert-IdentityType 'Specificuser'
+        Get-Privilege $username | Where-Object { $_ -eq 'SeBatchLogonRight' } | Should Not BeNullOrEmpty
+    }
+    
+    It 'should set idle timeout' {
+        $result = Install-IisAppPool -Name $appPoolName -IdleTimeout 55
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-Idletimeout 55
+    }
+    
+    It 'should enable32bit apps' {
+        $result = Install-IisAppPool -Name $appPoolName -Enable32BitApps
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-AppPool32BitEnabled $true
+    }
+    
+    It 'should handle app pool that exists' {
+        $result = Install-IisAppPool -Name $appPoolName
+        $result | Should BeNullOrEmpty
+        $result = Install-IisAppPool -Name $appPoolName
+        $result | Should BeNullOrEmpty
+    }
+    
+    It 'should change settings on existing app pool' {
+        $result = Install-IisAppPool -Name $appPoolName
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-ManagedRuntimeVersion 'v4.0'
+        Assert-ManagedPipelineMode 'Integrated'
+        Assert-IdentityType (Get-IISDefaultAppPoolIdentity)
+    
+        Assert-AppPool32BitEnabled $false
+    
+        $result = Install-IisAppPool -Name $appPoolName -ManagedRuntimeVersion 'v2.0' -ClassicPipeline -ServiceAccount 'LocalSystem' -Enable32BitApps
+        $result | Should BeNullOrEmpty
+        Assert-AppPoolExists
+        Assert-ManagedRuntimeVersion 'v2.0'
+        Assert-ManagedPipelineMode 'Classic'
+        Assert-IdentityType 'LocalSystem'
+        Assert-AppPool32BitEnabled $true
+    
+    }
+    
+    It 'should accept secure string for app pool password' {
+        $securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+        Install-IisAppPool -Name $appPoolName -Username $username -Password $securePassword
+        Assert-Identity $username $password
+    }
+    
+    It 'should convert32 bit app poolto64 bit' {
+        Install-IisAppPool -Name $appPoolName -ServiceAccount NetworkService -Enable32BitApps
+        Assert-AppPool32BitEnabled $true
+        Install-IisAppPool -Name $appPoolName -ServiceAccount NetworkService
+        Assert-AppPool32BitEnabled $false    
+    }
+    
+    It 'should switch to app pool identity if service account not given' {
+        Install-IisAppPool -Name $appPoolName -ServiceAccount NetworkService
+        Assert-IdentityType 'NetworkService'
+        Install-IisAppPool -Name $appPoolName
+        Assert-IdentityType (Get-IISDefaultAppPoolIdentity)
+    }
+    
+    It 'should start stopped app pool' {
+        Install-IisAppPool -Name $appPoolName 
+        $appPool = Get-IisAppPool -Name $appPoolName
+        $appPool | Should Not BeNullOrEmpty
+        if( $appPool.state -ne [Microsoft.Web.Administration.ObjectState]::Stopped )
+        { 
+            Start-Sleep -Seconds 1
+            $appPool.Stop()
+        }
+        
+        Install-IisAppPool -Name $appPoolName
+        $appPool = Get-IisAppPool -Name $appPoolName
+        $appPool.state | Should Be ([Microsoft.Web.Administration.ObjectState]::Started)
+    }
+    
+    It 'should fail if identity does not exist' {
+        $error.Clear()
+        Install-IisAppPool -Name $appPoolName -Username 'IDoNotExist' -Password 'blahblah' -ErrorAction SilentlyContinue
+        (Test-IisAppPool -Name $appPoolName) | Should Be $true
+        ($error.Count -ge 2) | Should Be $true
+    }
+    
+    
+}
