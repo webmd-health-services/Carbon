acbd39aa98b57d31333d942943799dcda5021f61
Aaron Jensen <ajensen@webmd.net>
2016-09-15 14:58:53 -0700

    Migrating Set-RegistryKeyValue tests to use Pester (part 2).

--- Test/Set-RegistryKeyValue.Tests.ps1
+++ Test/Set-RegistryKeyValue.Tests.ps1
@@ -10,187 +10,205 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
+
 $rootKey = 'hklm:\Software\Carbon\Test\Test-SetRegistryKeyValue'
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Import-CarbonForTest.ps1' -Resolve)
-}
-
-function Start-Test
-{
-    if( -not (Test-Path $rootKey -PathType Container) )
-    {
-        New-Item $rootKey -ItemType RegistryKey -Force
+<#
+    BeforeEach {
+        if( -not (Test-Path $rootKey -PathType Container) )
+        {
+            New-Item $rootKey -ItemType RegistryKey -Force
+        }
     }
+    
+    AfterEach {
+        Remove-Item $rootKey -Recurse
+    }
+#>
+
+function Remove-RootKey
+{
+    if( (Test-Path -Path $rootKey) )
+    {
+        Remove-Item -Path $rootKey -Recurse
+    }
+        
 }
 
-function Stop-Test
-{
-    Remove-Item $rootKey -Recurse
-}
+Describe 'Set-RegistryKeyValue when the key doesn''t exist' {
 
-function Test-ShouldCreateNewKeyAndValue
-{
+    Remove-RootKey
+        
     $keyPath = Join-Path $rootKey 'ShouldCreateNewKeyAndValue'
     $name = 'Title'
     $value = 'This is Sparta!'
-    
-    Assert-False (Test-RegistryKeyValue -Path $keyPath -Name $name)
-    Set-RegistryKeyValue -Path $keyPath -Name $name -String $value
-    Assert-True (Test-RegistryKeyValue -Path $keyPath -Name $name)
-    
-    $actualValue = Get-RegistryKeyValue -Path $keyPath -Name $name
-    Assert-Equal $value $actualValue
-}
 
-function Test-ShouldChangeAnExistingValue
-{
-    $name = 'ShouldChangeAnExistingValue'
-    $value = 'foobar''ed'
-    Set-RegistryKeyValue -Path $rootKey -Name $name -String $value
-    Assert-Equal $value (Get-RegistryKeyValue -Path $rootKey -Name $name)
-    
-    $newValue = 'Ok'
-    Set-RegistryKeyValue -Path $rootKey -Name $name -String $newValue
-    Assert-Equal $newValue (Get-RegistryKeyValue -Path $rootKey -Name $name)
-}
-
-function Test-ShouldSetBinaryValue
-{
-    Set-RegistryKeyValue -Path $rootKey -Name 'Binary' -Binary ([byte[]]@( 1, 2, 3, 4))
-    $value = Get-RegistryKeyValue -Path $rootKey -Name 'Binary'
-    Assert-NotNull $value
-    Assert-Equal 'System.Object[]' $value.GetType()
-    Assert-Equal 1 $value[0]
-    Assert-Equal 2 $value[1]
-    Assert-Equal 3 $value[2]
-    Assert-Equal 4 $value[3]
-}
-
-function Test-ShouldSetDwordValue
-{
-    $number = [Int32]::MaxValue
-    Set-RegistryKeyValue -Path $rootKey -Name 'DWord' -DWord $number
-    $value = Get-RegistryKeyValue -Path $rootKey -Name 'DWord'
-    Assert-NotNull $value
-    Assert-Equal 'int' $value.GetType()
-    Assert-Equal $number $value
-}
-
-function Test-ShouldSetQwordValue
-{
-    $number = [Int64]::MaxValue
-    Set-RegistryKeyValue -Path $rootKey -Name 'QWord' -QWord $number
-    $value = Get-RegistryKeyValue -Path $rootKey -Name 'QWord'
-    Assert-NotNull $value
-    Assert-Equal 'long' $value.GetType()
-    Assert-Equal $number $value
-}
-
-function Test-ShouldSetMultiStringValue
-{
-    $strings = @( 'Foo', 'Bar' )
-    Set-RegistryKeyValue -Path $rootKey -Name 'Strings' -Strings $strings
-    $value = Get-RegistryKeyValue -Path $rootKey -Name 'Strings'
-    Assert-NotNull $value
-    Assert-Equal $strings.Length $value.Length
-    Assert-Equal $strings[0] $value[0]
-    Assert-Equal $strings[1] $value[1]
-}
-
-function Test-ShouldSetExpandingStringValue
-{
-    Set-RegistryKeyValue -Path $rootKey -Name 'Expandable' -String '%ComputerName%' -Expand
-    $value = Get-RegistryKeyValue -Path $rootKey -Name 'Expandable'
-    Assert-NotNull $value
-    Assert-Equal $env:ComputerName $value
-}
-
-function Test-ShouldRemoveAndRecreateValue
-{
-    $name = 'ShouldChangeAnExistingValue'
-    $value = 'foobar''ed'
-    Set-RegistryKeyValue -Path $rootKey -Name $name -String $value
-    Assert-Equal $value (Get-RegistryKeyValue -Path $rootKey -Name $name)
-    
-    $newValue = 8439
-    Set-RegistryKeyValue -Path $rootKey -Name $name -DWord $newValue -Force
-    $newActualValue = Get-RegistryKeyValue -Path $rootKey -Name $name
-    Assert-Equal $newValue $newActualValue
-    Assert-Equal 'int' $newActualValue.GetType()
-}
-
-function Test-ShouldCreateNewValueEvenWithForce
-{
-    $name = 'NewWithForce'
-    $value = 8439
-    Assert-False (Test-REgistryKeyValue -Path $rootKey -Name $name)
-    Set-RegistryKeyValue -Path $rootKey -Name $name -DWord $value -Force
-    $actualValue = Get-RegistryKeyValue -Path $rootKey -Name $name
-    Assert-Equal $value $actualValue
-    Assert-Equal 'int' $actualValue.GetType()
-}
-
-function Test-ShouldSupportWhatIfWhenCReatingNewValue
-{
-    $name = 'newwithwhatif'
-    $value = 'value'
-    Assert-False (Test-RegistryKeyValue -Path $rootKey -Name $name)
-    Set-REgistryKeyValue -Path $rootKey -Name $name -String $value -WhatIf
-    Assert-False (Test-RegistryKeyValue -Path $rootKey -Name $name)
-}
-
-function Test-ShouldSupportWhatIfWhenUpdatingValue
-{
-    $name = 'newwithwhatif'
-    $value = 'value'
-    $newValue = 'newvalue'
-    Set-REgistryKeyValue -Path $rootKey -Name $name -String $value
-    Assert-True (Test-RegistryKeyValue -Path $rootKey -Name $name)
-    
-    Set-REgistryKeyValue -Path $rootKey -Name $name -String $newValue -WhatIf
-    Assert-True (Test-RegistryKeyValue -Path $rootKey -Name $name)
-    Assert-Equal $value (Get-REgistryKeyValue -Path $rootKey -Name $name)
-
-    Set-REgistryKeyValue -Path $rootKey -Name $name -String $newValue -WhatIf -Force
-    Assert-True (Test-RegistryKeyValue -Path $rootKey -Name $name)
-    Assert-Equal $value (Get-REgistryKeyValue -Path $rootKey -Name $name)
-}
-
-function Test-ShouldSetBitConvertedInt32ToUInt32
-{
-    $name = 'maxvalue'
-    foreach( $value in @( [int32]::MaxValue, 0, -1, [int32]::MinValue, [uint32]::MaxValue, [uint32]::MinValue ) )
-    {
-        Write-Debug -Message ('T {0} -is {1}' -f $value,$value.GetType())
-        $bytes = [BitConverter]::GetBytes( $value )
-        $int32 = [BitConverter]::ToInt32( $bytes, 0 )
-        Write-Debug -Message ('T {0} -is {1}' -f $value,$value.GetType())
-        Set-RegistryKeyValue -Path $rootKey -Name $name -DWord $int32
-        $setValue = Get-RegistryKeyValue -Path $rootKey -Name $name
-        Write-Debug -Message ('T {0} -is {1}' -f $setValue,$setValue.GetType())
-        Write-Debug -Message '-----'
-        $uint32 = [BitConverter]::ToUInt32( $bytes, 0 )
-        Assert-Equal $uint32 $setValue
+    It 'should create the registry key' {
+        (Test-RegistryKeyValue -Path $keyPath -Name $name) | Should Be $false
+        Set-RegistryKeyValue -Path $keyPath -Name $name -String $value
+        (Test-RegistryKeyValue -Path $keyPath -Name $name) | Should Be $true
+    }
+        
+    It 'should set the registry key''s value' {        
+        $actualValue = Get-RegistryKeyValue -Path $keyPath -Name $name
+        $actualValue | Should Be $value
     }
 }
 
-function Test-ShouldSetToUnsignedInt64
-{
-    $name = 'uint64maxvalue'
-    $value = [uint64]::MaxValue
-    Set-RegistryKeyValue -Path $rootKey -Name $name -UQWord $value
-    $setValue = Get-RegistryKeyValue -Path $rootKey -Name $name
-    Assert-Equal $value $setValue
+Describe 'Set-RegistryKeyValue when the key exists and has a value' {
+    $name = 'ShouldChangeAnExistingValue'
+    $value = 'foobar''ed'
+
+    It 'should set the initial value' {
+        Set-RegistryKeyValue -Path $rootKey -Name $name -String $value
+        (Get-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $value
+    }
+
+    It 'should change the value' {
+        $newValue = 'Ok'
+        Set-RegistryKeyValue -Path $rootKey -Name $name -String $newValue
+        (Get-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $newValue
+    }
+}
+    
+Describe 'Set-RegistryKeyValue when setting values of different types' {
+    It 'should set binary value' {
+        Set-RegistryKeyValue -Path $rootKey -Name 'Binary' -Binary ([byte[]]@( 1, 2, 3, 4))
+        $value = Get-RegistryKeyValue -Path $rootKey -Name 'Binary'
+        $value | Should Not BeNullOrEmpty
+        $value.GetType() | Should Be 'System.Object[]'
+        $value[0] | Should Be 1
+        $value[1] | Should Be 2
+        $value[2] | Should Be 3
+        $value[3] | Should Be 4
+    }
+
+    It 'should set dword value' {
+        $number = [Int32]::MaxValue
+        Set-RegistryKeyValue -Path $rootKey -Name 'DWord' -DWord $number
+        $value = Get-RegistryKeyValue -Path $rootKey -Name 'DWord'
+        $value | Should Not BeNullOrEmpty
+        $value.GetType() | Should Be 'int'
+        $value | Should Be $number
+    }
+
+    It 'should set qword value' {
+        $number = [Int64]::MaxValue
+        Set-RegistryKeyValue -Path $rootKey -Name 'QWord' -QWord $number
+        $value = Get-RegistryKeyValue -Path $rootKey -Name 'QWord'
+        $value | Should Not BeNullOrEmpty
+        $value.GetType() | Should Be 'long'
+        $value | Should Be $number
+    }
+    
+    It 'should set multi string value' {
+        $strings = @( 'Foo', 'Bar' )
+        Set-RegistryKeyValue -Path $rootKey -Name 'Strings' -Strings $strings
+        $value = Get-RegistryKeyValue -Path $rootKey -Name 'Strings'
+        $value | Should Not BeNullOrEmpty
+        $value.Length | Should Be $strings.Length
+        $value[0] | Should Be $strings[0]
+        $value[1] | Should Be $strings[1]
+    }
+    
+    It 'should set expanding string value' {
+        Set-RegistryKeyValue -Path $rootKey -Name 'Expandable' -String '%ComputerName%' -Expand
+        $value = Get-RegistryKeyValue -Path $rootKey -Name 'Expandable'
+        $value | Should Not BeNullOrEmpty
+        $value | Should Be $env:ComputerName
+    }
+
+    It 'should set to unsigned int64' {
+        $name = 'uint64maxvalue'
+        $value = [uint64]::MaxValue
+        Set-RegistryKeyValue -Path $rootKey -Name $name -UQWord $value
+        $setValue = Get-RegistryKeyValue -Path $rootKey -Name $name
+        $setValue | Should Be $value
+    }
+    
+    It 'should set to unsigned int32' {
+        $name = 'uint32maxvalue'
+        $value = [uint32]::MaxValue
+        Set-RegistryKeyValue -Path $rootKey -Name $name -UDWord $value
+        $setValue = Get-RegistryKeyValue -Path $rootKey -Name $name
+        $setValue | Should Be $value
+    }
+    
 }
 
-function Test-ShouldSetToUnsignedInt32
-{
-    $name = 'uint32maxvalue'
-    $value = [uint32]::MaxValue
-    Set-RegistryKeyValue -Path $rootKey -Name $name -UDWord $value
-    $setValue = Get-RegistryKeyValue -Path $rootKey -Name $name
-    Assert-Equal $value $setValue
+Describe 'Set-RegistryKeyValue when user needs to change the value''s type' {
+    It 'should remove and recreate value' {
+        $name = 'ShouldChangeAnExistingValue'
+        $value = 'foobar''ed'
+        Set-RegistryKeyValue -Path $rootKey -Name $name -String $value
+        (Get-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $value
+        
+        $newValue = 8439
+        Set-RegistryKeyValue -Path $rootKey -Name $name -DWord $newValue -Force
+        $newActualValue = Get-RegistryKeyValue -Path $rootKey -Name $name
+        $newActualValue | Should Be $newValue
+        $newActualValue.GetType() | Should Be 'int'
+    }
+ }
+
+ Describe 'Set-RegistryKeyValue when user uses -Force and the value doesn''t exist' {
+    Remove-RootKey
+
+    It 'should still create new value' {
+        $name = 'NewWithForce'
+        $value = 8439
+        (Test-REgistryKeyValue -Path $rootKey -Name $name) | Should Be $false
+        Set-RegistryKeyValue -Path $rootKey -Name $name -DWord $value -Force
+        $actualValue = Get-RegistryKeyValue -Path $rootKey -Name $name
+        $actualValue | Should Be $value
+        $actualValue.GetType() | Should Be 'int'
+    }
+    
 }
 
+Describe 'Set-RegistryKeyValue when using -WhatIf switch' {    
+    It 'should not create a new value' {
+        $name = 'newwithwhatif'
+        $value = 'value'
+        (Test-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $false
+        Set-REgistryKeyValue -Path $rootKey -Name $name -String $value -WhatIf
+        (Test-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $false
+    }
+
+    It 'should not update an existing value' {
+        $name = 'newwithwhatif'
+        $value = 'value'
+        $newValue = 'newvalue'
+        Set-RegistryKeyValue -Path $rootKey -Name $name -String $value
+        (Test-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $true
+        
+        Set-RegistryKeyValue -Path $rootKey -Name $name -String $newValue -WhatIf
+        (Test-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $true
+        (Get-REgistryKeyValue -Path $rootKey -Name $name) | Should Be $value
+    
+        Set-RegistryKeyValue -Path $rootKey -Name $name -String $newValue -WhatIf -Force
+        (Test-RegistryKeyValue -Path $rootKey -Name $name) | Should Be $true
+        (Get-REgistryKeyValue -Path $rootKey -Name $name) | Should Be $value
+    }
+}
+
+Describe 'Set-RegistryKeyValue when DWord value is an int32' {
+
+    $name = 'maxvalue'
+    foreach( $value in @( [int32]::MaxValue, 0, -1, [int32]::MinValue, [uint32]::MaxValue, [uint32]::MinValue ) )
+    {
+        It ('should set int32 value {0} as a uint32' -f $value) {
+            Write-Debug -Message ('T {0} -is {1}' -f $value,$value.GetType())
+            $bytes = [BitConverter]::GetBytes( $value )
+            $int32 = [BitConverter]::ToInt32( $bytes, 0 )
+            Write-Debug -Message ('T {0} -is {1}' -f $value,$value.GetType())
+            Set-RegistryKeyValue -Path $rootKey -Name $name -DWord $int32
+            $setValue = Get-RegistryKeyValue -Path $rootKey -Name $name
+            Write-Debug -Message ('T {0} -is {1}' -f $setValue,$setValue.GetType())
+            Write-Debug -Message '-----'
+            $uint32 = [BitConverter]::ToUInt32( $bytes, 0 )
+            $setValue | Should Be $uint32
+        }
+    }
+}
