b73ca0f45a9ef2236b37eb9548410b02a0c683ec
Aaron Jensen <ajensen@webmd.net>
2016-09-16 12:06:28 -0700

    Migrating Remove-HostsEntry tests to Pester (part 2).

--- Test/Remove-HostsEntry.Tests.ps1
+++ Test/Remove-HostsEntry.Tests.ps1
@@ -10,108 +10,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
+
 $hostsFile = $null
 $hostname = 'example.com'
-
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot '..\Import-CarbonForTest.ps1' -Resolve)
-}
-
-function Start-Test
-{
-    $hostsFile = Join-Path -Path $env:TEMP -ChildPath ('Carbon+Test-RemoveHostsEntry+{0}' -f [IO.Path]::GetRandomFileName())
-
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname -Path $hostsFile
-}
-
-function Stop-Test
-{
-    if( (Test-Path -Path $hostsFile -PathType Leaf) )
-    {
-        Remove-Item $hostsFile
-    }
-}
-
-function Test-ShouldRemoveHostsEntry
-{
-    Remove-HostsEntry -HostName $hostname -Path $hostsFile
-    Assert-NoHostsEntry -HostName $hostname
-}
-
-function Test-ShouldIgnoreSimilarHostsEntryWithDifferentTLD
-{
-    $dup = '{0}m' -f $hostname
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Path $hostsFile
-    Remove-HostsEntry $hostname -Path $hostsFile
-    Assert-NoHostsEntry -HostName $hostname
-    Assert-HostsEntry -HostName $dup
-}
-
-function Test-ShouldIgnoreSimilarHostsEntryWithSimilarTld
-{
-    $dup = 'example.co'
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Path $hostsFile
-    Remove-HostsEntry $hostname -Path $hostsFile
-    Assert-NoHostsEntry -HostName $hostname
-    Assert-HostsEntry -HostName $dup
-}
-
-function Test-ShouldIgnoreSimilarHostsEntryWithExtraSubDomain
-{
-    $dup = 'www.{0}' -f $hostname
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Path $hostsFile
-    Remove-HostsEntry $hostname -Path $hostsFile
-    Assert-NoHostsEntry -HostName $hostname
-    Assert-HostsEntry -HostName $dup
-}
-
-function Test-ShouldIgnoreCommentsWhenRemoving
-{
-    $dup = 'www.{0}' -f $hostname
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Description $hostname -Path $hostsFile
-    Remove-HostsEntry $hostname -Path $hostsFile
-    Assert-NoHostsEntry -HostName $hostname
-    Assert-HostsEntry -HostName $dup
-}
-
-function Test-ShouldRemoveMultipleHostsEntries
-{
-    $hostname2 = 'www.{0}' -f $hostname
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname2 -Path $hostsFile
-    Remove-HostsEntry -HostName $hostname,$hostname2 -Path $hostsFile
-    Assert-NoHostsEntry $hostname
-    Assert-NoHostsEntry $hostname2
-}
-
-function Test-ShouldRemoveMultipleHostsEntriesFromPipeline
-{
-    $hostname2 = 'www.{0}' -f $hostname
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname2 -Path $hostsFile
-    ($hostname,$hostname2) | Remove-HostsEntry -Path $hostsFile
-    Assert-NoHostsEntry $hostname
-    Assert-NoHostsEntry $hostname2
-}
-
-function Test-ShouldOperateOnWindowsHostsFile
-{
-    Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname
-    Remove-HostsEntry -HostName $hostname
-    Assert-NoHostsEntry -HostName $hostname -Path 'C:\Windows\system32\drivers\etc\hosts'
-}
-
-function Test-ShouldSupportWhatIf
-{
-    Remove-HostsEntry -HostName $hostname -WhatIf -Path $hostsFile
-    Assert-HostsEntry -HostName $hostname
-}
 
 function Get-HostsEntry
 {
     param(
         [string]
         $HostName,
-
+    
         [string]
         $Path = $hostsFile
     )
@@ -119,30 +28,109 @@ function Get-HostsEntry
     Get-Content -Path $Path |
         Where-Object { $_ -match ('^[0-9a-f.:]+\s+\b{0}\b' -f $HostName) }
 }
-
+    
 function Assert-HostsEntry
 {
     param(
         [string]
         $HostName,
-
+    
         [string]
         $Path = $hostsFile
     )
-
-    Assert-NotNull (Get-HostsEntry -HostName $HostName -Path $Path)
+    
+    (Get-HostsEntry -HostName $HostName -Path $Path) | Should Not BeNullOrEmpty
 }
-
+    
 function Assert-NoHostsEntry
 {
     param(
         [string]
         $HostName,
-
+    
         [string]
         $Path = $hostsFile
     )
-
-    Assert-Null (Get-HostsEntry -HostName $HostName -Path $Path)
+    
+    (Get-HostsEntry -HostName $HostName -Path $Path) | Should BeNullOrEmpty
 }
-
+    
+Describe 'Remove-HostsEntry' {
+    BeforeEach {
+        $hostsFile = Join-Path -Path $env:TEMP -ChildPath ('Carbon+Test-RemoveHostsEntry+{0}' -f [IO.Path]::GetRandomFileName())
+    
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname -Path $hostsFile
+    }
+    
+    AfterEach {
+        if( (Test-Path -Path $hostsFile -PathType Leaf) )
+        {
+            Remove-Item $hostsFile
+        }
+    }
+    
+    It 'should remove hosts entry' {
+        Remove-HostsEntry -HostName $hostname -Path $hostsFile
+        Assert-NoHostsEntry -HostName $hostname
+    }
+    
+    It 'should ignore similar hosts entry with different t l d' {
+        $dup = '{0}m' -f $hostname
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Path $hostsFile
+        Remove-HostsEntry $hostname -Path $hostsFile
+        Assert-NoHostsEntry -HostName $hostname
+        Assert-HostsEntry -HostName $dup
+    }
+    
+    It 'should ignore similar hosts entry with similar tld' {
+        $dup = 'example.co'
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Path $hostsFile
+        Remove-HostsEntry $hostname -Path $hostsFile
+        Assert-NoHostsEntry -HostName $hostname
+        Assert-HostsEntry -HostName $dup
+    }
+    
+    It 'should ignore similar hosts entry with extra sub domain' {
+        $dup = 'www.{0}' -f $hostname
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Path $hostsFile
+        Remove-HostsEntry $hostname -Path $hostsFile
+        Assert-NoHostsEntry -HostName $hostname
+        Assert-HostsEntry -HostName $dup
+    }
+    
+    It 'should ignore comments when removing' {
+        $dup = 'www.{0}' -f $hostname
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $dup -Description $hostname -Path $hostsFile
+        Remove-HostsEntry $hostname -Path $hostsFile
+        Assert-NoHostsEntry -HostName $hostname
+        Assert-HostsEntry -HostName $dup
+    }
+    
+    It 'should remove multiple hosts entries' {
+        $hostname2 = 'www.{0}' -f $hostname
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname2 -Path $hostsFile
+        Remove-HostsEntry -HostName $hostname,$hostname2 -Path $hostsFile
+        Assert-NoHostsEntry $hostname
+        Assert-NoHostsEntry $hostname2
+    }
+    
+    It 'should remove multiple hosts entries from pipeline' {
+        $hostname2 = 'www.{0}' -f $hostname
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname2 -Path $hostsFile
+        ($hostname,$hostname2) | Remove-HostsEntry -Path $hostsFile
+        Assert-NoHostsEntry $hostname
+        Assert-NoHostsEntry $hostname2
+    }
+    
+    It 'should operate on windows hosts file' {
+        Set-HostsEntry -IPAddress '1.2.3.4' -HostName $hostname
+        Remove-HostsEntry -HostName $hostname
+        Assert-NoHostsEntry -HostName $hostname -Path 'C:\Windows\system32\drivers\etc\hosts'
+    }
+    
+    It 'should support what if' {
+        Remove-HostsEntry -HostName $hostname -WhatIf -Path $hostsFile
+        Assert-HostsEntry -HostName $hostname
+    }
+    
+}
