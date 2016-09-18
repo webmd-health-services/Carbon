dbe77e7902d1ab2b34bf7d2567d7f202a45b0701
Aaron Jensen <ajensen@webmd.net>
2016-09-18 10:05:15 -0700

    Excluding scheduled tasks whose time changes everytime you inspect it.

--- Test/Get-ScheduledTask.Tests.ps1
+++ Test/Get-ScheduledTask.Tests.ps1
@@ -40,6 +40,7 @@ Describe 'Get-ScheduledTask' {
                                         '\Microsoft\Windows\Windows Activation Technologies\ValidationTaskDeadline' = $true;
                                         '\Microsoft\Windows\Customer Experience Improvement Program\Server\ServerRoleCollector' = $true;
                                         '\Microsoft\Windows\Customer Experience Improvement Program\Server\ServerRoleUsageCollector' = $true;
+                                        '\Microsoft\Windows\WS\WSRefreshBannedAppsListTask' = $true;
                                     }
         $scheduleProps = @(
                                'Last Result',
