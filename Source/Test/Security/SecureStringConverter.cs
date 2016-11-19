4b86e16f484c6e164833743d08895eb1d41996ce
Aaron Jensen <ajensen@webmd.net>
2016-11-18 16:32:10 -0800

    Adding missing license notice.

--- Source/Test/Security/SecureStringConverter.cs
+++ Source/Test/Security/SecureStringConverter.cs
@@ -1,8 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//    http://www.apache.org/licenses/LICENSE-2.0
+//   
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using NUnit.Framework;
 
 namespace Carbon.Test.Security
@@ -13,8 +20,7 @@ namespace Carbon.Test.Security
         [Test]
         public void ShouldConvertSecureString()
         {
-            var password = "waffles";
-            var passwordChars = password.ToCharArray();
+            const string password = "waffles";
             var securePassword = new System.Security.SecureString();
             foreach( var passwordChar in password.ToCharArray() )
             {
