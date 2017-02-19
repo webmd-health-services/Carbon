7185c18dcbbea0ea141d61a7876c27458c5c65fd
Aaron Jensen <ajensen@webmd.net>
2017-02-19 10:12:47 -0800

    Migrating Get-FirewallRule tests to Pester (part 2).

--- Test/Get-FirewallRule.Tests.ps1
+++ Test/Get-FirewallRule.Tests.ps1
@@ -10,108 +10,108 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot '..\Import-CarbonForTest.ps1' -Resolve)
-}
+& (Join-Path -Path $PSScriptRoot 'Import-CarbonForTest.ps1' -Resolve)
 
-function Test-ShouldGetFirewallRules
-{
-    [Carbon.Firewall.Rule[]]$rules = Get-FirewallRule
-    Assert-NotNull $rules
-    Assert-GreaterThan $rules.Count 0 
+Describe 'Get-FirewallRule.when getting all rules' {
+    It 'should get firewall rules' {
+        [Carbon.Firewall.Rule[]]$rules = Get-FirewallRule
+        $rules | Should Not BeNullOrEmpty
+        $rules.Count | Should BeGreaterThan 0
+        
+        $expectedRules = netsh advfirewall firewall show rule name=all verbose
+        
+        $fieldMaps =    @{
+                            'Rule Name' = 'Name';
+                            'Edge traversal' = 'EdgeTraversal';
+                            'InterfaceTypes' = 'InterfaceType';
+                            'Rule source' = 'Source';
+                        }
     
-    $expectedRules = netsh advfirewall firewall show rule name=all verbose
-    
-    $fieldMaps =    @{
-                        'Rule Name' = 'Name';
-                        'Edge traversal' = 'EdgeTraversal';
-                        'InterfaceTypes' = 'InterfaceType';
-                        'Rule source' = 'Source';
-                    }
-
-    $ruleIdx = 0
-    $actualRule = $null
-    $format = "{0,-38}{1}"
-    for( $idx = 0; $idx -lt $expectedRules.Count; ++$idx )
-    {
-        $line = $expectedRules[$idx]
-        if( -not $line -or $line -match '^\-+$' -or $line -eq 'Ok.' )
+        $ruleIdx = 0
+        $actualRule = $null
+        $format = "{0,-38}{1}"
+        for( $idx = 0; $idx -lt $expectedRules.Count; ++$idx )
         {
-            continue
-        }
-
-        if( $line -match '^ +Type +Code *$' )
-        {
-            $line = $expectedRules[++$idx]
-            if( $line -notmatch '^ +?([^ ]+) +?([^ ]+) *$' )
+            $line = $expectedRules[$idx]
+            if( -not $line -or $line -match '^\-+$' -or $line -eq 'Ok.' )
             {
-                Fail ('Failed to parse protocol details on line {0}: {1}' -f $idx,$line)
+                continue
             }
-            Assert-Like $actualRule.Protocol ('*:{0},{1}' -f $Matches[1],$Matches[2]) ('[{0}] {1}' -f $ruleIdx,$actualRule.Name)
-            continue
+    
+            if( $line -match '^ +Type +Code *$' )
+            {
+                $line = $expectedRules[++$idx]
+                if( $line -notmatch '^ +?([^ ]+) +?([^ ]+) *$' )
+                {
+                    Fail ('Failed to parse protocol details on line {0}: {1}' -f $idx,$line)
+                }
+                ($actualRule.Protocol -like ('*:{0},{1}' -f $Matches[1],$Matches[2])) | Should Be $true
+                continue
+            }
+    
+            if( $line -notmatch ('^(.+?):' ) )
+            {
+                Fail ('Found misshappen line {0}' -f $line)
+            }
+    
+            $fieldName = $Matches[1]
+            $propName = $fieldName
+            if( $fieldMaps.ContainsKey($propName) )
+            {
+                $propName = $fieldMaps[$fieldName]
+            }
+    
+            if( $propName -eq 'Name' )
+            {
+                $actualRule = $rules[$ruleIdx++]
+            }
+    
+            ($actualRule | Get-Member $propName) | Should Not BeNullOrEmpty
+            $value = $actualRule.$propName
+            if( $value -is [bool] )
+            {
+                $value = if( $value ) { 'Yes' } else { 'No' }
+            }
+    
+            if( $propName -eq 'Protocol' )
+            {
+                $value = $value -replace ':.*$',''
+            }
+    
+            ($format -f ('{0}:' -f $fieldName),$value) | Should Be $line
         }
+    }
+ }
 
-        if( $line -notmatch ('^(.+?):' ) )
-        {
-            Fail ('Found misshappen line {0}' -f $line)
+ Describe 'Get-FirewallRule.when getting a specific rule' {
+    It 'should get firewall rule' {
+        Get-FirewallRule | ForEach-Object {
+            $rule = $_
+            $actualRule = Get-FirewallRule -Name $rule.Name | ForEach-Object {
+                $actualRule = $_
+    
+                $actualRule | Should Not BeNullOrEmpty
+                $actualRule.Name | Should Be $rule.Name
+            }
         }
-
-        $fieldName = $Matches[1]
-        $propName = $fieldName
-        if( $fieldMaps.ContainsKey($propName) )
-        {
-            $propName = $fieldMaps[$fieldName]
-        }
-
-        if( $propName -eq 'Name' )
-        {
-            $actualRule = $rules[$ruleIdx++]
-        }
-
-        Assert-NotNull ($actualRule | Get-Member $propName) ('property {0} not found on {1} object [{2}] {3}' -f $propName,$actualRule.GetType().FullName,$ruleIdx,$actualRule.Name)
-        $value = $actualRule.$propName
-        if( $value -is [bool] )
-        {
-            $value = if( $value ) { 'Yes' } else { 'No' }
-        }
-
-        if( $propName -eq 'Protocol' )
-        {
-            $value = $value -replace ':.*$',''
-        }
-
-        Assert-Equal $line ($format -f ('{0}:' -f $fieldName),$value) $ruleIdx
     }
 }
 
-function Test-ShouldGetFirewallRule
-{
-    Get-FirewallRule | ForEach-Object {
-        $rule = $_
-        $actualRule = Get-FirewallRule -Name $rule.Name | ForEach-Object {
-            $actualRule = $_
+Describe 'Get-FirewallRule.when getting a specific rule with a wildcard pattern' {
+    It 'should support wildcard firewall rule' {
+        [Carbon.Firewall.Rule[]]$allRules = Get-FirewallRule
+        $allRules | Should Not BeNullOrEmpty
+        [Carbon.Firewall.Rule[]]$rules = Get-FirewallRule -Name '*HTTP*' 
+        $rules | Should Not BeNullOrEmpty
+        $rules.Length | Should BeLessThan $allRules.Length
+        $expectedCount = netsh advfirewall firewall show rule name=all | Where-Object { $_ -like 'Rule Name*HTTP*' } | Measure-Object | Select-Object -ExpandProperty 'Count'
+        $rules.Length | Should Be $expectedCount
+    }
+ }
 
-            Assert-NotNull $actualRule
-            Assert-Equal $rule.Name $actualRule.Name
-        }
+ Describe 'Get-FirewallRule.when getting a specific rule with a literal name' {
+    It 'should support literal name' {
+        $rules = Get-FirewallRule -LiteralName '*HTTP*'
+        $rules | Should BeNullOrEmpty
     }
 }
-
-function Test-ShouldSupportWildcardFirewallRule
-{
-    [Carbon.Firewall.Rule[]]$allRules = Get-FirewallRule
-    Assert-NotNull $allRules
-    [Carbon.Firewall.Rule[]]$rules = Get-FirewallRule -Name '*HTTP*' 
-    Assert-NotNull $rules
-    Assert-LessThan $rules.Length $allRules.Length
-    $expectedCount = netsh advfirewall firewall show rule name=all | Where-Object { $_ -like 'Rule Name*HTTP*' } | Measure-Object | Select-Object -ExpandProperty 'Count'
-    Assert-Equal $expectedCount $rules.Length
-}
-
-function Test-ShouldSupportLiteralName
-{
-    $rules = Get-FirewallRule -LiteralName '*HTTP*'
-    Assert-Null $rules
-}
-
