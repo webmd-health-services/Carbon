635123f77c5010c809f63ffb57c4934ae34a3b9a
Aaron Jensen <ajensen@webmd.net>
2017-02-20 11:28:05 -0800

    Migrating Test-Identity functions to Pester (part 2).

--- Test/Test-Identity.Tests.ps1
+++ Test/Test-Identity.Tests.ps1
@@ -11,88 +11,75 @@
 # limitations under the License.
 
 $user1 = $null
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
+$user1 = Install-User -Credential (New-Credential -UserName 'CarbonTestUser1' -Password 'P@ssw0rd!') -PassThru
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Import-CarbonForTest.ps1' -Resolve)
-    $user1 = Install-User -Credential (New-Credential -UserName 'CarbonTestUser1' -Password 'P@ssw0rd!') -PassThru
-}
-
-function Test-ShouldFindLocalGroup
-{
-    Assert-True (Test-Identity -Name 'Administrators')
-}
-
-
-function Test-ShouldFindLocalUser
-{
-    Assert-True (Test-Identity -Name $user1.SamAccountName)
-}
-
-if( (Get-WmiObject -Class 'Win32_ComputerSystem').Domain -ne 'WORKGROUP' )
-{
-    function Test-ShouldFindDomainUser
-    {
-        Assert-True (Test-Identity -Name ('{0}\Administrator' -f $env:USERDOMAIN))
+Describe 'Test-Identity' {
+    It 'should find local group' {
+        (Test-Identity -Name 'Administrators') | Should Be $true
     }
-}
-
-function Test-ShouldReturnSecurityIdentifier
-{
-    $sid = Test-Identity -Name $user1.SamAccountName -PassThru
-    Assert-NotNull $sid
-    Assert-True ($sid -is [Carbon.Identity])
-}
-
-function Test-ShouldNotFindMissingLocalUser
-{
-    $error.Clear()
-    Assert-False (Test-Identity -Name 'IDoNotExistIHope')
-    Assert-Equal 0 $error.Count
-}
-
-function Test-ShouldNotFindMissingLocalUserWithComputerForDomain
-{
-    $error.Clear()
-    Assert-False (Test-Identity -Name ('{0}\IDoNotExistIHope' -f $env:COMPUTERNAME))
-    Assert-Equal 0 $error.Count
-}
-
-function Test-ShouldNotFindUserInBadDomain
-{
-    $error.Clear()
-    Assert-False (Test-Identity -Name 'MISSINGDOMAIN\IDoNotExistIHope' -ErrorAction SilentlyContinue)
-    Assert-Equal 0 $error.Count
-}
-
-function Test-ShouldNotFindUserInCurrentDomain
-{
-    $error.Clear()
-    Assert-False (Test-Identity -Name ('{0}\IDoNotExistIHope' -f $env:USERDOMAIN) -ErrorAction SilentlyContinue)
-    Assert-Equal 0 $error.Count
-}
-
-function Test-ShouldFindUserWithDotDomain
-{
-    $users = Get-User
-    Assert-NotNull $users
-    try
+    
+    It 'should find local user' {
+        (Test-Identity -Name $user1.SamAccountName) | Should Be $true
+    }
+    
+    if( (Get-WmiObject -Class 'Win32_ComputerSystem').Domain -ne 'WORKGROUP' )
     {
-        $foundAUser = $false
-        foreach( $user in $users )
-        {
-            Assert-True (Test-Identity -Name ('.\{0}' -f $user.SamAccountName))
-            $foundAUser = $true
+        It 'should find domain user' {
+            (Test-Identity -Name ('{0}\Administrator' -f $env:USERDOMAIN)) | Should Be $true
         }
-        Assert-True $foundAUser
     }
-    finally
-    {
-        $users | ForEach-Object { $_.Dispose() }
+    
+    It 'should return security identifier' {
+        $sid = Test-Identity -Name $user1.SamAccountName -PassThru
+        $sid | Should Not BeNullOrEmpty
+        ($sid -is [Carbon.Identity]) | Should Be $true
     }
-}
-
-function Test-ShouldFindLocalSystem
-{
-    Assert-True (Test-Identity -Name 'LocalSystem')
+    
+    It 'should not find missing local user' {
+        $error.Clear()
+        (Test-Identity -Name 'IDoNotExistIHope') | Should Be $false
+        $error.Count | Should Be 0
+    }
+    
+    It 'should not find missing local user with computer for domain' {
+        $error.Clear()
+        (Test-Identity -Name ('{0}\IDoNotExistIHope' -f $env:COMPUTERNAME)) | Should Be $false
+        $error.Count | Should Be 0
+    }
+    
+    It 'should not find user in bad domain' {
+        $error.Clear()
+        (Test-Identity -Name 'MISSINGDOMAIN\IDoNotExistIHope' -ErrorAction SilentlyContinue) | Should Be $false
+        $error.Count | Should Be 0
+    }
+    
+    It 'should not find user in current domain' {
+        $error.Clear()
+        (Test-Identity -Name ('{0}\IDoNotExistIHope' -f $env:USERDOMAIN) -ErrorAction SilentlyContinue) | Should Be $false
+        $error.Count | Should Be 0
+    }
+    
+    It 'should find user with dot domain' {
+        $users = Get-User
+        $users | Should Not BeNullOrEmpty
+        try
+        {
+            $foundAUser = $false
+            foreach( $user in $users )
+            {
+                (Test-Identity -Name ('.\{0}' -f $user.SamAccountName)) | Should Be $true
+                $foundAUser = $true
+            }
+            $foundAUser | Should Be $true
+        }
+        finally
+        {
+            $users | ForEach-Object { $_.Dispose() }
+        }
+    }
+    
+    It 'should find local system' {
+        (Test-Identity -Name 'LocalSystem') | Should Be $true
+    }
 }
