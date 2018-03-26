07bb2ebd132c5f0fb286c3fee53d21d835902ef0
Joseph Larionov <jlarionov@webmd.net>
2018-03-26 08:49:19 -0700

    Fixed: Assert-FirewallConfigurable fails on Windows 10 due to firewall service name change

--- Carbon/Functions/Assert-FirewallConfigurable.ps1
+++ Carbon/Functions/Assert-FirewallConfigurable.ps1
@@ -35,7 +35,7 @@ function Assert-FirewallConfigurable
 
     Use-CallerPreference -Cmdlet $PSCmdlet -Session $ExecutionContext.SessionState
 
-    if( (Get-Service 'Windows Firewall').Status -ne 'Running' ) 
+    if( (Get-Service -Name 'MpsSvc').Status -ne 'Running' )
     {
         Write-Error "Unable to configure firewall: Windows Firewall service isn't running."
         return $false
