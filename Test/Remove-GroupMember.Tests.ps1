61a1dbf8fb16a7cce9a21ca8de24fc18f7c402dc
Aaron Jensen <ajensen@webmd.net>
2018-06-01 10:18:13 -0700

    Remove-GroupMember tests take 10 minutes. Hopefully, this changes makes them run faster.

--- Test/Remove-GroupMember.Tests.ps1
+++ Test/Remove-GroupMember.Tests.ps1
@@ -10,111 +10,159 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$GroupName = 'CarbonRemoveGroupMember'
-
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
-function Get-Member
+function Init
 {
-    Get-User | 
-        Where-Object { $_.SamAccountName -ne $env:COMPUTERNAME }
 }
 
-Describe 'Remove-GroupMember' {
-    BeforeEach {
-        $Global:Error.Clear()
-        $users = Get-Member
-        try
-        {
-            Remove-Group
-            Install-Group -Name $GroupName -Description "Group for testing the Remvoe-GroupMember Carbon function." -Member $users
-            $group = Get-Group -Name $GroupName
-            try
-            {
-                $group.Members.Count | Should Be $users.Count
-            }
-            finally
-            {
-                $group.Dispose()
-            }
-        }
-        finally
-        {
-            $users | ForEach-Object { $_.Dispose() }
-        }
-    }
-    
-    AfterEach {
-        Remove-Group
-    }
-    
-    function Remove-Group
+function GivenGroup
+{
+    param(
+        $Name,
+        $WithMember
+    )
+
+    GivenUser -UserName $WithMember
+
+    $WithMember = $WithMember | ForEach-Object { Resolve-IdentityName -Name $_ }
+
+    Install-Group -Name $Name -Description ('Carbon.{0} test group.' -f ($PSCommandPath | Split-Path -Leaf))
+    $group = Get-Group -Name $Name
+    $membersToRemove = $group.Members |
+                        Where-Object {
+                                        $currentMemberName = Resolve-Identity -SID $_.Sid
+                                        return ($currentMemberName -notin $WithMember )
+                                    }
+
+    foreach( $memberToRemove in $membersToRemove )
     {
-        $group = Get-Group -Name $GroupName -ErrorAction Ignore
-        try
-        {
-            if( $group -ne $null )
-            {
-                net localgroup `"$GroupName`" /delete
-            }
-        }
-        finally
-        {
-            if( $group )
-            {
-                $group.Dispose()
-            }
-        }
+        $group.Members.Remove($memberToRemove)
     }
-    
-    It 'should remove individual members' {
-        Get-Member | ForEach-Object { Remove-GroupMember -Name $GroupName -Member $_.SamAccountName ; $_.Dispose() }
-        $Global:Error.Count | Should Be 0
-        $group = Get-Group -Name $GroupName
-        try
-        {
-            $group.Members.Count | Should Be 0
-        }
-        finally
-        {
-            $group.Dispose()
-        }
+
+    $group.Save()
+    $group.Dispose()
+
+    Add-GroupMember -Name $Name -Member $WithMember
+}
+
+function GivenUser
+{
+    param(
+        $UserName
+    )
+
+    foreach( $member in $UserName )
+    {
+        Install-User -Credential (New-Credential -UserName $member -Password 'UVjh9DXN8YqD') -Description ('Carbon.{0} test user.' -f ($PSCommandPath | Split-Path -Leaf))
     }
-    
-    It 'should remove bulk members' {
-        $users = Get-Member
-        try
-        {
-            Remove-GroupMember -Name $GroupName -Member $users
-            $Global:Error.Count | Should Be 0
-        }
-        finally
-        {
-            $users | ForEach-Object { $_.Dispose() }
-        }
-    
-        $group = Get-Group -Name $GroupName
-        try
-        {
-            $group.Members.Count | Should Be 0
-        }
-        finally
-        {
-            $group.Dispose()
-        }
+}
+
+function ThenError
+{
+    param(
+        $Matches
+    )
+
+    It ('should write no errors') {
+        $Global:Error | Should Match $Matches
     }
-    
-    It 'should support what if' {
-        $users = Get-Member
-        $users | ForEach-Object { Remove-GroupMember -Name $GroupName -Member $_.SamAccountName -WhatIf; $_.Dispose() }
-        $group = Get-Group -Name $GroupName
-        try
-        {
-            $group.Members.Count | Should Be $users.Count
-        }
-        finally
-        {
-            $group.Dispose()
-        }
+}
+
+function ThenNoError
+{
+    param(
+    )
+
+    It ('should write no errors') {
+        $Global:Error | Should BeNullOrEmpty
     }
+}
+
+function ThenGroup
+{
+    param(
+        $Name,
+        [string[]]
+        $HasMember
+    )
+
+    $HasMember = $HasMember | ForEach-Object { Resolve-IdentityName -Name $_ }
+
+    $group = Get-Group -Name $Name
+    It ('should remove members') {
+      
+        $group.Members.Count | Should Be $HasMember.Count
+        foreach( $currentMember in $group.Members )
+        {
+            $currentMemberName = Resolve-IdentityName -SID $currentMember.Sid
+            $currentMemberName -in $HasMember | Should Be $true
+        }
+        $group.Save()
+        $group.Dispose()
+    }
+}
+
+function WhenRemoving
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    param(
+        $Member,
+        $FromGroup
+    )
+
+    $Global:Error.Clear()
+    Remove-GroupMember -Name $FromGroup -Member $Member
+}
+
+Describe 'Remove-GroupMember.when removing single member' {
+    Init
+    GivenGroup 'FubarSnafu' -WithMember 'one','two'
+    WhenRemoving 'one' -FromGroup 'FubarSnafu'
+    ThenGroup 'FubarSnafu' -HasMember 'two'
+}
+
+Describe 'Remove-GroupMember.when removing multiple members' {
+    Init
+    GivenGroup 'FubarSnafu' -WithMember 'one','two','three'
+    WhenRemoving 'one','two' -FromGroup 'FubarSnafu'
+    ThenGroup 'FubarSnafu' -HasMember 'three'
+}
+
+Describe 'Remove-GroupMember.when removing all members' {
+    Init
+    GivenGroup 'FubarSnafu' -WithMember 'one','two','three'
+    WhenRemoving 'one','two','three' -FromGroup 'FubarSnafu'
+    ThenGroup 'FubarSnafu' -HasMember @()
+}
+
+Describe 'Remove-GroupMember.when removing user not in group' {
+    Init
+    GivenGroup 'FubarSnafu' -WithMember 'one'
+    GivenUser 'two'
+    WhenRemoving 'two' -FromGroup 'FubarSnafu'
+    ThenGroup 'FubarSnafu' -HasMember 'one'
+    ThenNoError
+}
+
+Describe 'Remove-GroupMember.when removing user that does not exist' {
+    Init
+    GivenGroup 'FubarSnafu' -WithMember 'one'
+    WhenRemoving 'fdfsadfdsf' -FromGroup 'FubarSnafu' -ErrorAction SilentlyContinue
+    ThenGroup 'FubarSnafu' -HasMember 'one'
+    ThenError -Matches 'Identity\ ''fdfsadfdsf'' not found\.'
+}
+
+Describe 'Remove-GroupMember.when group does not exist' {
+    Init
+    WhenRemoving 'fdfsadfdsf' -FromGroup 'jkfdsjfldsf' -ErrorAction SilentlyContinue
+    ThenError -Matches 'Local\ group\ ''jkfdsjfldsf'' not found\.'
+}
+
+Describe 'Remove-GroupMember.when using -WhatIf switch' {
+    Init
+    GivenGroup 'FubarSnafu' -WithMember 'one','two'
+    WhenRemoving 'one' -FromGroup 'FubarSnafu' -WhatIf
+    ThenNoError
+    ThenGroup 'FubarSnafu' -HasMember 'one','two'
 }
