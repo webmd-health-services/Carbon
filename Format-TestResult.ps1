0d491f9dcb00bbfa60efb9532b1da2b3d81a15ba
Aaron Jensen <ajensen@webmd.net>
2018-06-01 12:37:09 -0700

    Adding missing license notice.

--- Format-TestResult.ps1
+++ Format-TestResult.ps1
@@ -1,4 +1,22 @@
-﻿param(
+﻿<#
+.SYNOPSIS
+Displays a test result report.
+
+.DESCRIPTION
+The `Format-TestResult.ps1` script parses test result XML files and displays a report on all test fixtures that ran, ordered by longest to shortest.
+#>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+param(
     [Parameter(Mandatory=$true)]
     [string]
     # Directory where the test output files are.
