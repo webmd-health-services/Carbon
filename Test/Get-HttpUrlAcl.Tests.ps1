c5dd61eeba74e97142130be0a8df96780f6b5b52
Aaron Jensen <ajensen@webmd.net>
2018-07-06 15:26:48 -0700

    Migrating Get-HttpUrlAcl test fixture to Pester (part 2).

--- Test/Get-HttpUrlAcl.Tests.ps1
+++ Test/Get-HttpUrlAcl.Tests.ps1
@@ -13,116 +13,109 @@
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 $user = $null
 $url = 'http://test-gethttpurlacl:10939/'
-
-function Start-Test
-{
-    $user = Install-User -Credential (New-Credential -UserName 'CarbonTestUser' -Password 'Password1') -PassThru
-    netsh http add urlacl ('url={0}' -f $url)('user={0}\{1}' -f $env:COMPUTERNAME,$user.SamAccountName) | Write-Debug
-}
-
-function Stop-Test
-{
-    netsh http delete urlacl ('url={0}' -f $url)
-}
-
-function Test-ShouldGetAllUrlAcls
-{
-    [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl
-    Assert-NotNull $acls
-    $urlacl = netsh http show urlacl
-    $urlacl = $urlacl -join [Environment]::NewLine
-
-    foreach( $acl in $acls )
-    {
-        $header = '' -f $acl.Url
-
-        if( $acl.Access.Count -eq 1 )
-        {
-            $rule = $acl.Access[0]
-            $identity = $rule.IdentityReference.ToString()
-            if( $identity -eq 'Everyone' )
-            {
-                $identity = '\Everyone'
-            }
-
-            $listen = 'No'
-            if( $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::Listen -or $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::ListenAndDelegate )
-            {
-                $listen = 'Yes'
-            }
-
-            $delegate = 'No'
-            if( $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::Delegate -or $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::ListenAndDelegate )
-            {
-                $delegate = 'Yes'
-            }
-
-            Assert-That $urlacl -Contains (@'
-    Reserved URL            : {0} 
-        User: {1}
-            Listen: {2}
-            Delegate: {3}
-'@ -f $acl.Url,$identity,$listen,$delegate)
-
-        }
-    }
-}
-
-function Test-ShouldGetSpecificUrl
-{
-    [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl -LiteralUrl $url
-    Assert-TestUrl $acls
-}
-
-
-function Test-ShouldIgnoreWildcardsWithLiteralUrlParameter
-{
-    [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl -LiteralUrl 'http://*:10939/' -ErrorAction SilentlyContinue
-    Assert-Null $acls
-    Assert-Error -Last -Regex 'not found'
-}
-
-function Test-ShouldFindWithWildcard
-{
-    [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl -Url 'http://*:10939/'
-    Assert-TestUrl $acls
-}
-
-function Test-ShouldWriteErrorIfLiteralUrlNotFound
-{
-    $acl = Get-HttpUrlAcl -LiteralUrl 'fubar' -ErrorAction SilentlyContinue
-    Assert-Null $acl
-    Assert-Error -Last -Regex 'not found'
-}
-
-function Test-ShouldFailIfUrlWithNoWildcardsNotFound
-{
-    $acl = Get-HttpUrlAcl -Url 'fubar' -ErrorAction SilentlyContinue
-    Assert-Null $acl
-    Assert-Error -Last -Regex 'not found'
-}
-
-function Test-ShouldNotFailIfUrlWithWildcardsNotFound
-{
-    $acl = Get-HttpUrlAcl -Url 'fubar*' 
-    Assert-Null $acl
-    Assert-NoError
-}
-
+    
 function Assert-TestUrl
 {
     param(
         [Carbon.Security.HttpUrlSecurity[]]
         $Acls 
     )
-
-    Assert-NoError
-
-    Assert-NotNull $Acls
-    Assert-Equal 1 $Acls.Count
-
+    
+    $Global:Error.Count | Should -Be 0
+    
+    $Acls | Should -Not -BeNullOrEmpty
+    $Acls.Count | Should -Be 1
+    
     $acl = $Acls[0]
-    Assert-Equal 1 $acl.Access.Count 
+    $acl.Access.Count | Should -Be 1
     $rule = $acl.Access[0]
-    Assert-Equal ('{0}\{1}' -f $env:COMPUTERNAME,$user.SamAccountName) $rule.IdentityReference
+    $rule.IdentityReference | Should -Be ('{0}\{1}' -f $env:COMPUTERNAME,$user.SamAccountName)
+}
+
+Describe 'Get-HttpUrlAcl' {
+    BeforeEach {
+        $Global:Error.Clear()
+        $user = Install-User -Credential (New-Credential -UserName 'CarbonTestUser' -Password 'Password1') -PassThru
+        netsh http add urlacl ('url={0}' -f $url)('user={0}\{1}' -f $env:COMPUTERNAME,$user.SamAccountName) | Write-Debug
+    }
+    
+    AfterEach {
+        netsh http delete urlacl ('url={0}' -f $url)
+    }
+    
+    It 'should get all url acls' {
+        [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl
+        $acls | Should -Not -BeNullOrEmpty
+        $urlacl = netsh http show urlacl
+        $urlacl = $urlacl -join [Environment]::NewLine
+    
+        foreach( $acl in $acls )
+        {
+            $header = '' -f $acl.Url
+    
+            if( $acl.Access.Count -eq 1 )
+            {
+                $rule = $acl.Access[0]
+                $identity = $rule.IdentityReference.ToString()
+                if( $identity -eq 'Everyone' )
+                {
+                    $identity = '\Everyone'
+                }
+    
+                $listen = 'No'
+                if( $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::Listen -or $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::ListenAndDelegate )
+                {
+                    $listen = 'Yes'
+                }
+    
+                $delegate = 'No'
+                if( $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::Delegate -or $rule.HttpUrlAccessRights -eq [Carbon.Security.HttpUrlAccessRights]::ListenAndDelegate )
+                {
+                    $delegate = 'Yes'
+                }
+    
+                $regex = 'Reserved\ URL\s+:\s+{0}\s+User:\s+{1}\s+Listen:\s+{2}\s+Delegate:\s+{3}' -f [regex]::Escape($acl.Url),[regex]::escape($identity),[regex]::Escape($listen),[regex]::Escape($delegate)
+                $urlacl | Should -Match $regex
+    
+            }
+        }
+    }
+    
+    It 'should get specific url' {
+        [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl -LiteralUrl $url
+        Assert-TestUrl $acls
+    }
+    
+    
+    It 'should ignore wildcards with literal url parameter' {
+        [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl -LiteralUrl 'http://*:10939/' -ErrorAction SilentlyContinue
+        $acls | Should -BeNullOrEmpty
+        $Global:Error.Count | Should -BeGreaterThan 0
+        $Global:Error[0] | Should -Match 'not found'
+    }
+    
+    It 'should find with wildcard' {
+        [Carbon.Security.HttpUrlSecurity[]]$acls = Get-HttpUrlAcl -Url 'http://*:10939/'
+        Assert-TestUrl $acls
+    }
+    
+    It 'should write error if literal url not found' {
+        $acl = Get-HttpUrlAcl -LiteralUrl 'fubar' -ErrorAction SilentlyContinue
+        $acl | Should -BeNullOrEmpty
+        $Global:Error.Count | Should -BeGreaterThan 0
+        $Global:Error[0] | Should -Match 'not found'
+    }
+    
+    It 'should fail if url with no wildcards not found' {
+        $acl = Get-HttpUrlAcl -Url 'fubar' -ErrorAction SilentlyContinue
+        $acl | Should -BeNullOrEmpty
+        $Global:Error.Count | Should -BeGreaterThan 0
+        $Global:Error[0] | Should -Match 'not found'
+    }
+    
+    It 'should not fail if url with wildcards not found' {
+        $acl = Get-HttpUrlAcl -Url 'fubar*' 
+        $acl | Should -BeNullOrEmpty
+        $Global:Error.Count | Should Be 0
+    }
 }
