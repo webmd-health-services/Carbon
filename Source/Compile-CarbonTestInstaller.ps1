919a1ba9d6427534ea2a0670418c65d66df8ba27
Aaron Jensen <ajensen@webmd.net>
2019-02-14 12:56:11 -0800

    Creating a script to build the Carbon installers.

---
+++ Source/Compile-CarbonTestInstaller.ps1
@@ -0,0 +1,30 @@
+<#
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [string]
+    $Configuration
+)
+
+Set-StrictMode -Version 'Latest'
+#Requires -Version 5.1
+
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\PSModules\VSSetup' -Resolve)
+
+$instances = Get-VSSetupInstance
+$instances | Format-List | Out-String | Write-Verbose
+$instance = $instances | Sort-Object -Descending -Property InstallationVersion
+
+$idePath = Join-Path -Path $instance.InstallationPath -ChildPath 'Common7\IDE' -Resolve
+Write-Verbose -Message ('Using {0} {1} found at "{2}".' -f $instance.DisplayName,$instance.InstallationVersion,$idePath)
+$installerSlnPath = Join-Path -Path $PSScriptRoot -ChildPath 'Carbon.Installer.sln' -Resolve
+
+$env:PATH = '{0}{1}{2}' -f $env:PATH,[IO.Path]::PathSeparator,$idePath
+
+Write-Verbose ('devenv "{0}" /build "{1}"' -f $installerSlnPath,$Configuration)
+devenv $installerSlnPath /build $Configuration
+if( $LASTEXITCODE )
+{
+    Write-Error -Message ('Failed to build Carbon test installers. Check the output above for details. If the build failed because of this error: "ERROR: An error occurred while validating. HRESULT = ''8000000A''", open a command prompt, move into the "{0}" directory, and run ".\Common7\IDE\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild\DisableOutOfProcBuild.exe".' -f $instance.InstallationPath) -ErrorAction Stop
+}
