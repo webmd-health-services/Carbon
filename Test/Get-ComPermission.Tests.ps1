235b72949f86d2e2acb6929c61c78651a9753a6b
Aaron Jensen <ajensen@webmd.net>
2019-02-21 16:00:10 -0800

    Migrating Get-ComPermission tests to Pester (part 2).

--- Test/Get-ComPermission.Tests.ps1
+++ Test/Get-ComPermission.Tests.ps1
@@ -10,90 +10,83 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Import-CarbonForTest.ps1' -Resolve)
-}
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
-function Test-ShouldGetComAccessPermissions
-{
-    $rules = Get-ComPermission -Access -Default
-    Assert-NotNull $rules
-    Assert-GreaterThan $rules.Count 1
-    $rules | ForEach-Object { 
-        Assert-NotNull $_.IdentityReference
-        Assert-NotNull $_.ComAccessRights
-        Assert-NotNull $_.AccessControlType
-        Assert-False $_.IsInherited
-        Assert-Equal 'None' $_.InheritanceFlags
-        Assert-Equal 'None' $_.PropagationFlags
-     }
-}
-
-function Test-ShouldGetPermissionsForSpecificUser
-{
-    $rules = Get-ComPermission -Access -Default
-    Assert-GreaterThan $rules.Count 1
-    $rule = Get-ComPermission -Access -Default -Identity $rules[0].IdentityReference.Value
-    Assert-NotNull $rule
-    Assert-Equal $rule.IdentityReference $rules[0].IdentityReference
-}
-
-function Test-ShouldGetSecurityLimits
-{
-    $defaultRules = Get-ComPermission -Access -Default
-    Assert-NotNull $defaultRules
+Describe 'Get-ComPermission' {
+    It 'should get com access permissions' {
+        $rules = Get-ComPermission -Access -Default
+        $rules | Should -Not -BeNullOrEmpty
+        $rules.Count | Should -BeGreaterThan 1
+        $rules | ForEach-Object { 
+            $_.IdentityReference | Should -Not -BeNullOrEmpty
+            $_.ComAccessRights | Should -Not -BeNullOrEmpty
+            $_.AccessControlType | Should -Not -BeNullOrEmpty
+            $_.IsInherited | Should -Be $false
+            $_.InheritanceFlags | Should -Be 'None'
+            $_.PropagationFlags | Should -Be 'None'
+         }
+    }
     
-    $limitRules = Get-ComPermission -Access -Limits
-    Assert-NotNull $limitRules
+    It 'should get permissions for specific user' {
+        $rules = Get-ComPermission -Access -Default
+        $rules.Count | Should -BeGreaterThan 1
+        $rule = Get-ComPermission -Access -Default -Identity $rules[0].IdentityReference.Value
+        $rule | Should -Not -BeNullOrEmpty
+        $rules[0].IdentityReference | Should -Be $rule.IdentityReference
+    }
     
-    if( $defaultRules.Count -eq $limitRules.Count )
-    {
-        for( $idx = 0; $idx -lt $limitRules.Count; $idx++ )
+    It 'should get security limits' {
+        $defaultRules = Get-ComPermission -Access -Default
+        $defaultRules | Should -Not -BeNullOrEmpty
+        
+        $limitRules = Get-ComPermission -Access -Limits
+        $limitRules | Should -Not -BeNullOrEmpty
+        
+        if( $defaultRules.Count -eq $limitRules.Count )
         {
-            Assert-NotEqual $defaultRules[$idx] $limitRules[$idx]
-        }
-    }    
-}
-
-function Test-ShouldGetComLaunchAndActivationPermissions
-{
-    $rules = Get-ComPermission -LaunchAndActivation -Default
-    Assert-NotNull $rules
-    Assert-True ($rules.Count -gt 1)
-    $rules | ForEach-Object { 
-        Assert-NotNull $_.IdentityReference
-        Assert-NotNull $_.ComAccessRights
-        Assert-NotNull $_.AccessControlType
-        Assert-False $_.IsInherited
-        Assert-Equal 'None' $_.InheritanceFlags
-        Assert-Equal 'None' $_.PropagationFlags
-     }
-}
-
-function Test-ShouldGetLaunchAndActivationRuleForSpecificUser
-{
-    $rules = Get-ComPermission -LaunchAndActivation -Default
-    Assert-GreaterThan $rules.Count 1
-    $rule = Get-ComPermission -LaunchAndActivation -Default -Identity $rules[0].IdentityReference.Value
-    Assert-NotNull $rule
-    Assert-Equal $rule.IdentityReference $rules[0].IdentityReference
-}
-
-function Test-ShouldGetLaunchAndActivationSecurityLimits
-{
-    $defaultRules = Get-ComPermission -LaunchAndActivation -Default
-    Assert-NotNull $defaultRules
+            for( $idx = 0; $idx -lt $limitRules.Count; $idx++ )
+            {
+                $limitRules[$idx] | Should -Not -Be $defaultRules[$idx]
+            }
+        }    
+    }
     
-    $limitRules = Get-ComPermission -LaunchAndActivation -Limits
-    Assert-NotNull $limitRules
+    It 'should get com launch and activation permissions' {
+        $rules = Get-ComPermission -LaunchAndActivation -Default
+        $rules | Should -Not -BeNullOrEmpty
+        ($rules.Count -gt 1) | Should -Be $true
+        $rules | ForEach-Object { 
+            $_.IdentityReference | Should -Not -BeNullOrEmpty
+            $_.ComAccessRights | Should -Not -BeNullOrEmpty
+            $_.AccessControlType | Should -Not -BeNullOrEmpty
+            $_.IsInherited | Should -Be $false
+            $_.InheritanceFlags | Should -Be 'None'
+            $_.PropagationFlags | Should -Be 'None'
+         }
+    }
     
-    if( $defaultRules.Count -eq $limitRules.Count )
-    {
-        for( $idx = 0; $idx -lt $limitRules.Count; $idx++ )
+    It 'should get launch and activation rule for specific user' {
+        $rules = Get-ComPermission -LaunchAndActivation -Default
+        $rules.Count | Should -BeGreaterThan 1
+        $rule = Get-ComPermission -LaunchAndActivation -Default -Identity $rules[0].IdentityReference.Value
+        $rule | Should -Not -BeNullOrEmpty
+        $rules[0].IdentityReference | Should -Be $rule.IdentityReference
+    }
+    
+    It 'should get launch and activation security limits' {
+        $defaultRules = Get-ComPermission -LaunchAndActivation -Default
+        $defaultRules | Should -Not -BeNullOrEmpty
+        
+        $limitRules = Get-ComPermission -LaunchAndActivation -Limits
+        $limitRules | Should -Not -BeNullOrEmpty
+        
+        if( $defaultRules.Count -eq $limitRules.Count )
         {
-            Assert-NotEqual $defaultRules[$idx] $limitRules[$idx]
-        }
-    }    
+            for( $idx = 0; $idx -lt $limitRules.Count; $idx++ )
+            {
+                $limitRules[$idx] | Should -Not -Be $defaultRules[$idx]
+            }
+        }    
+    }
+    
 }
-
