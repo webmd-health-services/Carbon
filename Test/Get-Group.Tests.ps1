3f9c6004d978c390271d3941e8330e87059674d4
Aaron Jensen <ajensen@webmd.net>
2019-09-10 16:47:24 -0700

    Migrating Get-CUser and Get-CGroup tests to Pester (part 2).

--- Test/Get-Group.Tests.ps1
+++ Test/Get-Group.Tests.ps1
@@ -10,62 +10,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function Start-Test
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\..\Carbon\Import-Carbon.ps1' -Resolve)
-}
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Import-CarbonForTest.ps1' -Resolve)
 
-function Stop-Test
-{
-}
-
-function Test-ShouldGetAllGroups
-{
-    $groups = Get-Group
-    try
-    {
-        Assert-NotNull $groups
-        Assert-GreaterThan $groups.Length 0
-        $groups | ForEach-Object { Assert-Is $_ ([DirectoryServices.AccountManagement.GroupPrincipal]) }
+Describe 'Get-Group' {
+    It 'should get all groups' {
+        $groups = Get-Group
+        try
+        {
+            $groups | Should -Not -BeNullOrEmpty
+            $groups.Length | Should -BeGreaterThan 0
+            
+        }
+        finally
+        {
+            $groups | ForEach-Object { $_.Dispose() }
+        }
     }
-    finally
-    {
-        $groups | ForEach-Object { $_.Dispose() }
-    }
-}
-
-function Test-ShouldGetOneGroup
-{
-    Get-Group |
-        ForEach-Object { 
-            $expectedGroup = $_
-            try
-            {
-                $group = Get-Group -Name $expectedGroup.Name
+    
+    It 'should get one group' {
+        Get-Group |
+            ForEach-Object { 
+                $expectedGroup = $_
                 try
                 {
-                    Assert-Equal $expectedGroup.Sid $group.Sid
+                    $group = Get-Group -Name $expectedGroup.Name
+                    try
+                    {
+                        $group.Sid | Should -Be $expectedGroup.Sid
+                    }
+                    finally
+                    {
+                        if( $group )
+                        {
+                            $group.Dispose()
+                        }
+                    }
                 }
                 finally
                 {
-                    if( $group )
-                    {
-                        $group.Dispose()
-                    }
+                    $expectedGroup.Dispose()
                 }
             }
-            finally
-            {
-                $expectedGroup.Dispose()
-            }
-        }
-}
-
-function Test-ShouldErrorIfGroupNotFound
-{
-    $Error.Clear()
-    $group = Get-Group -Name 'fjksdjfkldj' -ErrorAction SilentlyContinue
-    Assert-Null $group
-    Assert-Equal 1 $Error.Count
-    Assert-Like $Error[0].Exception.Message '*not found*'
+    }
+    
+    It 'should error if group not found' {
+        $Error.Clear()
+        $group = Get-Group -Name 'fjksdjfkldj' -ErrorAction SilentlyContinue
+        $group | Should -BeNullOrEmpty
+        $Error.Count | Should -Be 1
+        Assert-Like $Error[0].Exception.Message '*not found*'
+    }
 }
