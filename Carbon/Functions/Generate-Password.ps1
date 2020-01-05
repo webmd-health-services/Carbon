efdb7c444bdde7759e68493c16d81136c3a6453f
oliwex <oliwexminio@gmail.com>
2020-01-05 20:36:43 +0100

    Adding function: Generate-Password which generates hard to guess password.

---
+++ Carbon/Functions/Generate-Password.ps1
@@ -0,0 +1,33 @@
+﻿
+function Generate-Password
+{
+    <#
+    .SYNOPSIS
+    Creates hard password.
+
+    .DESCRIPTION
+    Removes a *local* user account.  If the account doesn't exist, nothing happens.
+
+    .EXAMPLE
+    Generate-Password -maxValue 9
+
+    #>
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [ValidateRange(8,99)]
+        [Int]
+        $maxValue
+    ) 
+        $newPass = '' 
+
+    1..$maxValue | ForEach-Object { 
+        $newPass += [char](Get-Random -Minimum 48 -Maximum 122) 
+    }  
+    $txt = "Za!" 
+    $passnew = $newPass+$txt 
+    return $passnew 
+}
+Generate-Password -maxValue 9
+
+
