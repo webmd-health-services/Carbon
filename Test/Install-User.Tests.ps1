0e2a8647947d85c4b3c2c9493c426928934cc94d
Aaron Jensen <ajensen@webmd.net>
2021-03-11 16:56:29 -0800

    Converting Install-User tests to Pester (2 of 2).

--- Test/Install-User.Tests.ps1
+++ Test/Install-User.Tests.ps1
@@ -10,275 +10,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-CarbonTest.ps1' -Resolve)
+
 $username = 'CarbonInstallUser'
 $password = 'IM33tRequ!rem$'
-
-function Start-TestFixture
-{
-    & (Join-Path -Path $PSScriptRoot -ChildPath '..\Initialize-CarbonTest.ps1' -Resolve)
-}
-
-function Start-Test
-{
-    Remove-TestUser
-}
-
-function Stop-Test
-{
-    Remove-TestUser
-}
-
-function Remove-TestUser
-{
-    Uninstall-User -Username $username
-}
-
-function Test-ShouldCreateNewUser
-{
-    $warnings = @()
-    $fullName = 'Carbon Install User'
-    $description = "Test user for testing the Carbon Install-User function."
-    $user = Install-CUser -UserName $username -Password $password -Description $description -FullName $fullName -PassThru -WarningVariable 'warnings'
-    Assert-NotNull $user
-    try
-    {
-        Assert-Is $user ([DirectoryServices.AccountManagement.UserPrincipal])
-        Assert-True (Test-User -Username $username)
-    }
-    finally
-    {
-        $user.Dispose()
-    }
-
-    [DirectoryServices.AccountManagement.UserPrincipal]$user = Get-User -Username $username
-    Assert-NotNull $user
-    try
-    {
-        Assert-Equal $description $user.Description
-        Assert-True $user.PasswordNeverExpires 
-        Assert-True $user.Enabled
-        Assert-Equal $username $user.SamAccountName
-        Assert-False $user.UserCannotChangePassword
-        Assert-Equal $fullName $user.DisplayName
-        Assert-Credential -Password $password
-        Assert-Equal 1 $warnings.Count
-        Assert-Like $warnings[0] '*obsolete*'
-    }
-    finally
-    {
-        $user.Dispose()
-    }
-}
-
-
-function Test-ShouldCreateNewUserWithCredential
-{
-    $fullName = 'Carbon Install User'
-    $description = "Test user for testing the Carbon Install-User function."
-    $c = New-Credential -UserName $username -Password $password
-    $user = Install-User -Credential $c -Description $description -FullName $fullName -PassThru
-    Assert-NotNull $user
-    try
-    {
-        Assert-Is $user ([DirectoryServices.AccountManagement.UserPrincipal])
-        Assert-True (Test-User -Username $username)
-    }
-    finally
-    {
-        $user.Dispose()
-    }
-
-    [DirectoryServices.AccountManagement.UserPrincipal]$user = Get-User -Username $username
-    Assert-NotNull $user
-    try
-    {
-        Assert-Equal $description $user.Description
-        Assert-True $user.PasswordNeverExpires 
-        Assert-True $user.Enabled
-        Assert-Equal $username $user.SamAccountName
-        Assert-False $user.UserCannotChangePassword
-        Assert-Equal $fullName $user.DisplayName
-        Assert-Credential -Password $password
-    }
-    finally
-    {
-        $user.Dispose()
-    }
-}
-
-function Test-ShouldUpdateExistingUsersProperties
-{
-    $fullName = 'Carbon Install User'
-    $result = Install-User -Username $username -Password $password -Description "Original description" -FullName $fullName
-    Assert-Null $result
-
-    $originalUser = Get-User -Username $username
-    Assert-NotNull $originalUser
-    try
-    {
-    
-        $newFullName = 'New {0}' -f $fullName
-        $newDescription = "New description"
-        $newPassword = 'IM33tRequ!re$2'
-        $result = Install-User -Username $username `
-                               -Password $newPassword `
-                               -Description $newDescription `
-                               -FullName $newFullName `
-                               -UserCannotChangePassword `
-                               -PasswordExpires 
-        try
-        {
-            Assert-Null $result
-        }
-        finally
-        {
-            if( $result )
-            {
-                $result.Dispose()
-            }
-        }
-
-        [DirectoryServices.AccountManagement.UserPrincipal]$newUser = Get-User -Username $username
-        Assert-NotNull $newUser
-        try
-        {
-            Assert-Equal $originalUser.SID $newUser.SID
-            Assert-Equal $newDescription $newUser.Description
-            Assert-Equal $newFullName $newUser.DisplayName
-            Assert-False $newUser.PasswordNeverExpires
-            Assert-True $newUser.UserCannotChangePassword
-            Assert-Credential -Password $newPassword
-        }
-        finally
-        {
-            $newUser.Dispose()
-        }
-    }
-    finally
-    {
-        $originalUser.Dispose()
-    }
-}
-
-function Test-ShouldUpdateExistingUsersPropertiesWithCredential
-{
-    $fullName = 'Carbon Install User'
-    $credential = New-Credential -Username $username -Password $password
-    $result = Install-User -Credential $credential -Description "Original description" -FullName $fullName
-    try
-    {
-        Assert-Null $result
-    }
-    finally
-    {
-        if( $result )
-        {
-            $result.Dispose()
-        }
-    }
-
-    $originalUser = Get-User -Username $username
-    Assert-NotNull $originalUser
-    try
-    {
-    
-        $newFullName = 'New {0}' -f $fullName
-        $newDescription = "New description"
-        $newPassword = [Guid]::NewGuid().ToString().Substring(0,14)
-        $credential = New-Credential -UserName $username -Password $newPassword
-        
-        $result = Install-User -Credential $credential `
-                               -Description $newDescription `
-                               -FullName $newFullName `
-                               -UserCannotChangePassword `
-                               -PasswordExpires 
-        try
-        {
-            Assert-Null $result
-        }
-        finally
-        {
-            if( $result )
-            {
-                $result.Dispose()
-            }
-        }
-
-        [DirectoryServices.AccountManagement.UserPrincipal]$newUser = Get-User -Username $username
-        Assert-NotNull $newUser
-        try
-        {
-            Assert-Equal $originalUser.SID $newUser.SID
-            Assert-Equal $newDescription $newUser.Description
-            Assert-Equal $newFullName $newUser.DisplayName
-            Assert-False $newUser.PasswordNeverExpires
-            Assert-True $newUser.UserCannotChangePassword
-            Assert-Credential -Password $newPassword
-        }
-        finally
-        {
-            $newUser.Dispose()
-        }
-    }
-    finally
-    {
-        $originalUser.Dispose()
-    }
-}
-
-function Test-ShouldAllowOptionalFullName
-{
-    $fullName = 'Carbon Install User'
-    $description = "Test user for testing the Carbon Install-User function."
-    $result = Install-User -Username $username -Password $password -Description $description
-    try
-    {
-        Assert-Null $result
-    }
-    finally
-    {
-        if( $result )
-        {
-            $result.Dispose()
-        }
-    }
-
-    $user = Get-User -Username $Username
-    try
-    {
-        Assert-Null $user.DisplayName
-    }
-    finally
-    {
-        $user.Dispose()
-    }
-}
-
-function Test-ShouldSupportWhatIf
-{
-    $user = Install-User -Username $username -Password $password -WhatIf -PassThru
-    try
-    {
-        Assert-NotNull $user
-    }
-    finally
-    {
-        $user.Dispose()
-    }
-
-    $user = Get-User -Username $username -ErrorAction SilentlyContinue
-    try
-    {
-        Assert-Null $user
-    }
-    finally
-    {
-        if( $user )
-        {
-            $user.Dispose()
-        }
-    }
-}
 
 function Assert-Credential
 {
@@ -290,7 +25,7 @@ function Assert-Credential
     {
         $ctx = [DirectoryServices.AccountManagement.ContextType]::Machine
         $px = New-Object 'DirectoryServices.AccountManagement.PrincipalContext' $ctx,$env:COMPUTERNAME
-        Assert-True ($px.ValidateCredentials( $username, $password ))
+        ($px.ValidateCredentials( $username, $password )) | Should -BeTrue
     }
     finally
     {
@@ -298,3 +33,258 @@ function Assert-Credential
     }
 }
 
+Describe 'Install-CUser' {
+    BeforeEach {
+        Remove-TestUser
+    }
+    
+    AfterEach {
+        Remove-TestUser
+    }
+    
+    function Remove-TestUser
+    {
+        Uninstall-CUser -Username $username
+    }
+    
+    It 'should create new user' {
+        $warnings = @()
+        $fullName = 'Carbon Install User'
+        $description = "Test user for testing the Carbon Install-CUser function."
+        $user = Install-CUser -UserName $username -Password $password -Description $description -FullName $fullName -PassThru -WarningVariable 'warnings'
+        $user | Should -Not -BeNullOrEmpty
+        try
+        {
+            $user | Should -BeOfType ([DirectoryServices.AccountManagement.UserPrincipal])
+            (Test-CUser -Username $username) | Should -BeTrue
+        }
+        finally
+        {
+            $user.Dispose()
+        }
+    
+        [DirectoryServices.AccountManagement.UserPrincipal]$user = Get-CUser -Username $username
+        $user | Should -Not -BeNullOrEmpty
+        try
+        {
+            $user.Description | Should -Be $description
+            $user.PasswordNeverExpires | Should -BeTrue
+            $user.Enabled | Should -BeTrue
+            $user.SamAccountName | Should -Be $username
+            $user.UserCannotChangePassword | Should -BeFalse
+            $user.DisplayName | Should -Be $fullName
+            Assert-Credential -Password $password
+            $warnings.Count | Should -Be 1
+            $warnings[0] | Should -BeLike '*obsolete*'
+        }
+        finally
+        {
+            $user.Dispose()
+        }
+    }
+    
+    
+    It 'should create new user with credential' {
+        $fullName = 'Carbon Install User'
+        $description = "Test user for testing the Carbon Install-CUser function."
+        $c = New-Credential -UserName $username -Password $password
+        $user = Install-CUser -Credential $c -Description $description -FullName $fullName -PassThru
+        $user | Should -Not -BeNullOrEmpty
+        try
+        {
+            $user | Should -BeOfType ([DirectoryServices.AccountManagement.UserPrincipal])
+            (Test-CUser -Username $username) | Should -BeTrue
+        }
+        finally
+        {
+            $user.Dispose()
+        }
+    
+        [DirectoryServices.AccountManagement.UserPrincipal]$user = Get-CUser -Username $username
+        $user | Should -Not -BeNullOrEmpty
+        try
+        {
+            $user.Description | Should -Be $description
+            $user.PasswordNeverExpires | Should -BeTrue
+            $user.Enabled | Should -BeTrue
+            $user.SamAccountName | Should -Be $username
+            $user.UserCannotChangePassword | Should -BeFalse
+            $user.DisplayName | Should -Be $fullName
+            Assert-Credential -Password $password
+        }
+        finally
+        {
+            $user.Dispose()
+        }
+    }
+    
+    It 'should update existing users properties' {
+        $fullName = 'Carbon Install User'
+        $result = Install-CUser -Username $username -Password $password -Description "Original description" -FullName $fullName
+        $result | Should -BeNullOrEmpty
+    
+        $originalUser = Get-CUser -Username $username
+        $originalUser | Should -Not -BeNullOrEmpty
+        try
+        {
+        
+            $newFullName = 'New {0}' -f $fullName
+            $newDescription = "New description"
+            $newPassword = 'IM33tRequ!re$2'
+            $result = Install-CUser -Username $username `
+                                   -Password $newPassword `
+                                   -Description $newDescription `
+                                   -FullName $newFullName `
+                                   -UserCannotChangePassword `
+                                   -PasswordExpires 
+            try
+            {
+                $result | Should -BeNullOrEmpty
+            }
+            finally
+            {
+                if( $result )
+                {
+                    $result.Dispose()
+                }
+            }
+    
+            [DirectoryServices.AccountManagement.UserPrincipal]$newUser = Get-CUser -Username $username
+            $newUser | Should -Not -BeNullOrEmpty
+            try
+            {
+                $newUser.SID | Should -Be $originalUser.SID
+                $newUser.Description | Should -Be $newDescription
+                $newUser.DisplayName | Should -Be $newFullName
+                $newUser.PasswordNeverExpires | Should -BeFalse
+                $newUser.UserCannotChangePassword | Should -BeTrue
+                Assert-Credential -Password $newPassword
+            }
+            finally
+            {
+                $newUser.Dispose()
+            }
+        }
+        finally
+        {
+            $originalUser.Dispose()
+        }
+    }
+    
+    It 'should update existing users properties with credential' {
+        $fullName = 'Carbon Install User'
+        $credential = New-Credential -Username $username -Password $password
+        $result = Install-CUser -Credential $credential -Description "Original description" -FullName $fullName
+        try
+        {
+            $result | Should -BeNullOrEmpty
+        }
+        finally
+        {
+            if( $result )
+            {
+                $result.Dispose()
+            }
+        }
+    
+        $originalUser = Get-CUser -Username $username
+        $originalUser | Should -Not -BeNullOrEmpty
+        try
+        {
+        
+            $newFullName = 'New {0}' -f $fullName
+            $newDescription = "New description"
+            $newPassword = [Guid]::NewGuid().ToString().Substring(0,14)
+            $credential = New-Credential -UserName $username -Password $newPassword
+            
+            $result = Install-CUser -Credential $credential `
+                                   -Description $newDescription `
+                                   -FullName $newFullName `
+                                   -UserCannotChangePassword `
+                                   -PasswordExpires 
+            try
+            {
+                $result | Should -BeNullOrEmpty
+            }
+            finally
+            {
+                if( $result )
+                {
+                    $result.Dispose()
+                }
+            }
+    
+            [DirectoryServices.AccountManagement.UserPrincipal]$newUser = Get-CUser -Username $username
+            $newUser | Should -Not -BeNullOrEmpty
+            try
+            {
+                $newUser.SID | Should -Be $originalUser.SID
+                $newUser.Description | Should -Be $newDescription
+                $newUser.DisplayName | Should -Be $newFullName
+                $newUser.PasswordNeverExpires | Should -BeFalse
+                $newUser.UserCannotChangePassword | Should -BeTrue
+                Assert-Credential -Password $newPassword
+            }
+            finally
+            {
+                $newUser.Dispose()
+            }
+        }
+        finally
+        {
+            $originalUser.Dispose()
+        }
+    }
+    
+    It 'should allow optional full name' {
+        $fullName = 'Carbon Install User'
+        $description = "Test user for testing the Carbon Install-CUser function."
+        $result = Install-CUser -Username $username -Password $password -FullName $fullName -Description $description
+        try
+        {
+            $result | Should -BeNullOrEmpty
+        }
+        finally
+        {
+            if( $result )
+            {
+                $result.Dispose()
+            }
+        }
+    
+        $user = Get-CUser -Username $Username
+        try
+        {
+            $user.DisplayName | Should -Be $fullName
+        }
+        finally
+        {
+            $user.Dispose()
+        }
+    }
+    
+    It 'should support what if' {
+        $user = Install-CUser -Username $username -Password $password -WhatIf -PassThru
+        try
+        {
+            $user | Should -Not -BeNullOrEmpty
+        }
+        finally
+        {
+            $user.Dispose()
+        }
+    
+        $user = Get-CUser -Username $username -ErrorAction SilentlyContinue
+        try
+        {
+            $user | Should -BeNullOrEmpty
+        }
+        finally
+        {
+            if( $user )
+            {
+                $user.Dispose()
+            }
+        }
+    }
+}
